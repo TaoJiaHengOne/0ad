TaoJiaHengOne/0ad

commit 8d0a7170f695dbe3f8785a70e99b82620805c2ca

# Slightly tidied up string code.
CStr: Indented comments more consistently. Made some parameters
pass-by-reference, made some others not. Removed some useless methods -
Length (use length or empty), GetSubstring (use substr), LCase/UCase
(use LowerCase/UpperCase). Removed operator[] bounds-checking because
VS2005 does that anyway.
Maybe fixed noncopyable warnings on VS2003.

This was SVN commit r4828.

source/scripting/ScriptableComplex.inl
@@ -131,12 +131,12 @@ public:
 
 				if( Instance )
 				{
-					size_t rlen = Instance->m_PropertyRoot.Length();
+					size_t rlen = Instance->m_PropertyRoot.length();
 
 					IJSComplex::PropertyTable::iterator iit;
 					for( iit = T::m_IntrinsicProperties.begin(); iit != T::m_IntrinsicProperties.end(); iit++ )
-						if( ( iit->first.Length() > rlen ) && ( iit->first.Left( rlen ) == Instance->m_PropertyRoot ) && ( iit->first[rlen] == '.' ) )
-							it->first.insert( iit->first.GetSubstring( rlen + 1, iit->first.Length() - rlen - 1 ).BeforeFirst( L"." ) );
+						if( ( iit->first.length() > rlen ) && ( iit->first.Left( rlen ) == Instance->m_PropertyRoot ) && ( iit->first[rlen] == '.' ) )
+							it->first.insert( iit->first.BeforeFirst( L".", rlen + 1 ) );
 
 
 					Instance->m_Owner->FillEnumerateSet( it, &( Instance->m_PropertyRoot ) );
@@ -919,10 +919,10 @@ void CJSComplex<T, ReadOnly>::FillEnumerateSet( IteratorState* it, CStrW* Proper
 	PropertyTable::iterator iit;
 	if( PropertyRoot )
 	{
-		size_t rlen = PropertyRoot->Length();
+		size_t rlen = PropertyRoot->length();
 		for( iit = m_Properties.begin(); iit != m_Properties.end(); iit++ )
-			if( ( iit->first.Length() > rlen ) && ( iit->first.Left( rlen ) == *PropertyRoot ) && ( iit->first[rlen] == '.' ) )
-				it->first.insert( iit->first.GetSubstring( rlen + 1, iit->first.Length() - rlen - 1 ).BeforeFirst( L"." ) );
+			if( ( iit->first.length() > rlen ) && ( iit->first.Left( rlen ) == *PropertyRoot ) && ( iit->first[rlen] == '.' ) )
+				it->first.insert( iit->first.BeforeFirst( L".", rlen + 1 ) );
 	}
 	else
 	{

source/graphics/GameView.h
@@ -21,7 +21,7 @@ struct JSObject;
 
 class CGameViewImpl;
 
-class CGameView : private Scene
+class CGameView : private Scene, public boost::noncopyable
 {
 public:
 	static const float defaultFOV, defaultNear, defaultFar;

source/graphics/MapReader.cpp
@@ -70,7 +70,7 @@ void CMapReader::LoadMap(const char* filename, CTerrain *pTerrain_,
 	if (unpacker.GetVersion() >= 3) {
 		// read the corresponding XML file
 		filename_xml = filename;
-		filename_xml = filename_xml.Left(filename_xml.Length()-4) + ".xml";
+		filename_xml = filename_xml.Left(filename_xml.length()-4) + ".xml";
 		RegMemFun(this, &CMapReader::ReadXML, L"CMapReader::ReadXML", 5800);
 	}
 

source/graphics/MapWriter.cpp
@@ -52,7 +52,7 @@ void CMapWriter::SaveMap(const char* filename, CTerrain* pTerrain,
 	packer.Write(filename);
 
 	CStr filename_xml (filename);
-	filename_xml = filename_xml.Left(filename_xml.Length()-4) + ".xml";
+	filename_xml = filename_xml.Left(filename_xml.length()-4) + ".xml";
 	WriteXML(filename_xml, pUnitMan, pWaterMan, pSkyMan, pLightEnv, pCamera, pCinema);
 }
 

source/graphics/MaterialManager.cpp
@@ -21,13 +21,13 @@ static SMaterialColor ParseColor(CStr colorStr)
     CStr tmp;
     int idx = 0;
     long pos = colorStr.Find(' ');
-    while(colorStr.Length())
+    while(colorStr.length())
     {
         if(pos == -1)
-            pos = (long)colorStr.Length();
+            pos = (long)colorStr.length();
 
-        tmp = colorStr.GetSubstring(0, pos);
-        colorStr = colorStr.GetSubstring(pos, colorStr.Length() - pos);
+        tmp = colorStr.substr(0, pos);
+        colorStr = colorStr.substr(pos);
         colorStr = colorStr.Trim(PS_TRIM_LEFT);
         pos = colorStr.Find(' ');
 
@@ -80,10 +80,10 @@ static SMaterialColor ParseColor(CStr colorStr)
 
 static bool ParseUsage(CStr temp)
 {
-    temp = temp.LCase().Trim(PS_TRIM_BOTH);
-    if(temp == CStr("blend") ||
-        temp == CStr("true") ||
-        temp == CStr("yes") ||
+    temp = temp.LowerCase().Trim(PS_TRIM_BOTH);
+    if(temp == "blend" ||
+        temp == "true" ||
+        temp == "yes" ||
         temp.ToInt() > 0)
         return true;
 
@@ -93,8 +93,8 @@ static bool ParseUsage(CStr temp)
 #if 0 // unused
 static GLenum ParseAlphaFunc(CStr temp)
 {
-    temp = temp.LCase().Trim(PS_TRIM_BOTH);
-    if(!temp.Length())
+    temp = temp.LowerCase().Trim(PS_TRIM_BOTH);
+    if(temp.empty())
         return GL_NONE;
 
     if(temp == CStr("never"))
@@ -119,8 +119,8 @@ static GLenum ParseAlphaFunc(CStr temp)
 
 static GLenum ParseBlendFunc(CStr temp)
 {
-    temp = temp.LCase().Trim(PS_TRIM_BOTH);
-    if(!temp.Length())
+    temp = temp.LowerCase().Trim(PS_TRIM_BOTH);
+    if(temp.empty())
         return GL_NONE;
 
     if(temp == CStr("zero"))
@@ -232,19 +232,19 @@ CMaterial &CMaterialManager::LoadMaterial(const char *file)
 			else if(token == el_colors)
 			{
 				temp = (CStr)attrs.getNamedItem(at_diffuse);
-				if(temp.Length() > 0)
+				if(! temp.empty())
 					material->SetDiffuse(ParseColor(temp));
 
 				temp = (CStr)attrs.getNamedItem(at_ambient);
-				if(temp.Length() > 0)
+				if(! temp.empty())
 					material->SetAmbient(ParseColor(temp));
 
 				temp = (CStr)attrs.getNamedItem(at_specular);
-				if(temp.Length() > 0)
+				if(! temp.empty())
 					material->SetSpecular(ParseColor(temp));
 
 				temp = (CStr)attrs.getNamedItem(at_specularpower);
-				if(temp.Length() > 0)
+				if(! temp.empty())
 					material->SetSpecularPower(ClampFloat(temp.ToFloat(), 0.0f, 1.0f));
 			}
 			else if(token == el_alpha)

source/graphics/MeshManager.cpp
@@ -142,8 +142,8 @@ CModelDefPtr CMeshManager::GetMesh(const CStr& filename)
 {
 	// Strip a three-letter file extension (if there is one) from the filename
 	CStr name;
-	if (filename.Length() > 4 && filename[filename.Length()-4] == '.')
-		name = filename.GetSubstring(0, filename.Length()-4);
+	if (filename.length() > 4 && filename[filename.length()-4] == '.')
+		name = filename.substr(0, filename.length()-4);
 	else
 		name = filename;
 
@@ -216,7 +216,7 @@ CModelDefPtr CMeshManager::GetMesh(const CStr& filename)
 		CStr cachedPmdVfsPath = "cache/";
 		cachedPmdVfsPath += realDaePath;
 		// Remove the .dae extension (which will certainly be there)
-		cachedPmdVfsPath = cachedPmdVfsPath.GetSubstring(0, cachedPmdVfsPath.Length()-4);
+		cachedPmdVfsPath = cachedPmdVfsPath.substr(0, cachedPmdVfsPath.length()-4);
 		// Add a _hash.pmd extension
 		cachedPmdVfsPath += "_";
 		cachedPmdVfsPath += hashString;

source/graphics/Model.cpp
@@ -222,9 +222,10 @@ CSkeletonAnim* CModel::BuildAnimation(const char* filename, const char* name, fl
 // Update: update this model by the given time, in seconds
 void CModel::Update(float time)
 {
-	if (m_Anim && m_Anim->m_AnimDef && m_BoneMatrices) {
+	if (m_Anim && m_Anim->m_AnimDef && m_BoneMatrices)
+	{
 		// adjust for animation speed
-		float animtime=time*m_AnimSpeed;
+		float animtime = time*m_AnimSpeed;
 
 		// update animation time, but don't calculate bone matrices - do that (lazily) when
 		// something requests them; that saves some calculation work for offscreen models,
@@ -233,12 +234,13 @@ void CModel::Update(float time)
 		m_AnimTime += animtime;
 		
 
-		float duration=m_Anim->m_AnimDef->GetDuration();
-		if (m_AnimTime > duration) {
+		float duration = m_Anim->m_AnimDef->GetDuration();
+		if (m_AnimTime > duration)
+		{
 			if (m_Flags & MODELFLAG_NOLOOPANIMATION)
 				SetAnimation(m_NextAnim);
 			else
-				m_AnimTime = (float) fmod(m_AnimTime, duration);
+				m_AnimTime = fmod(m_AnimTime, duration);
 		}
 		
 		// mark vertices as dirty
@@ -249,10 +251,8 @@ void CModel::Update(float time)
 	}
 
 	// update props
-	for (uint i=0; i<m_Props.size(); i++)
-	{
+	for (size_t i = 0; i < m_Props.size(); ++i)
 		m_Props[i].m_Model->Update(time);
-	}
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -462,8 +462,9 @@ void CModel::SetTransform(const CMatrix3D& transform)
 void CModel::SetMaterial(const CMaterial &material)
 {
 	m_Material = material;
-	if(m_Material.GetTexture().Trim(PS_TRIM_BOTH).Length() > 0)
+	if(m_Material.GetTexture().Trim(PS_TRIM_BOTH).length() > 0)
 	{
+		// [TODO: uh, shouldn't this be doing something?]
 	}
 }
 

source/graphics/ObjectBase.cpp
@@ -294,7 +294,7 @@ std::vector<u8> CObjectBase::CalculateVariationKey(const std::vector<std::set<CS
 		CObjectBase::Variant& var ((*grp)[match]);
 		for (std::vector<CObjectBase::Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
 		{
-			if (it->m_ModelName.Length())
+			if (! it->m_ModelName.empty())
 				chosenProps[it->m_PropPointName] = it->m_ModelName;
 			else
 				chosenProps.erase(it->m_PropPointName);
@@ -347,18 +347,18 @@ const CObjectBase::Variation CObjectBase::BuildVariation(const std::vector<u8>& 
 
 		// Apply its data:
 
-		if (var.m_TextureFilename.Length())
+		if (! var.m_TextureFilename.empty())
 			variation.texture = var.m_TextureFilename;
 
-		if (var.m_ModelFilename.Length())
+		if (! var.m_ModelFilename.empty())
 			variation.model = var.m_ModelFilename;
 
-		if (var.m_Color.Length())
+		if (! var.m_Color.empty())
 			variation.color = var.m_Color;
 
 		for (std::vector<CObjectBase::Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
 		{
-			if (it->m_ModelName.Length())
+			if (! it->m_ModelName.empty())
 				variation.props[it->m_PropPointName] = *it;
 			else
 				variation.props.erase(it->m_PropPointName);
@@ -470,7 +470,7 @@ std::set<CStr> CObjectBase::CalculateRandomVariation(const std::set<CStr>& initi
 		CObjectBase::Variant& var ((*grp)[match]);
 		for (std::vector<CObjectBase::Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
 		{
-			if (it->m_ModelName.Length())
+			if (! it->m_ModelName.empty())
 				chosenProps[it->m_PropPointName] = it->m_ModelName;
 			else
 				chosenProps.erase(it->m_PropPointName);
@@ -552,7 +552,7 @@ std::vector<std::vector<CStr> > CObjectBase::GetVariantGroups() const
 				const std::vector<Prop>& props = obj->m_VariantGroups[i][j].m_Props;
 				for (size_t k = 0; k < props.size(); ++k)
 				{
-					if (props[k].m_ModelName.Length())
+					if (! props[k].m_ModelName.empty())
 					{
 						CObjectBase* prop = m_ObjectManager.FindObjectBase(props[k].m_ModelName);
 						if (prop)

commit 29f44d66ede36e02a329930e4225af4a0dc08136

# Bug fix Fixes problem introduced in 8d0a7170f6, removes workaround from faf8358b3c
This was SVN commit r4957.

source/scripting/ScriptableComplex.inl
@@ -136,7 +136,7 @@ public:
 					IJSComplex::PropertyTable::iterator iit;
 					for( iit = T::m_IntrinsicProperties.begin(); iit != T::m_IntrinsicProperties.end(); iit++ )
 						if( ( iit->first.length() > rlen ) && ( iit->first.Left( rlen ) == Instance->m_PropertyRoot ) && ( iit->first[rlen] == '.' ) )
-							it->first.insert( iit->first.BeforeFirst( L".", rlen + 1 ) );
+							it->first.insert( CStrW( iit->first.substr( rlen + 1 ) ).BeforeFirst( L"." ) );
 
 
 					Instance->m_Owner->FillEnumerateSet( it, &( Instance->m_PropertyRoot ) );
@@ -922,7 +922,7 @@ void CJSComplex<T, ReadOnly>::FillEnumerateSet( IteratorState* it, CStrW* Proper
 		size_t rlen = PropertyRoot->length();
 		for( iit = m_Properties.begin(); iit != m_Properties.end(); iit++ )
 			if( ( iit->first.length() > rlen ) && ( iit->first.Left( rlen ) == *PropertyRoot ) && ( iit->first[rlen] == '.' ) )
-				it->first.insert( iit->first.BeforeFirst( L".", rlen + 1 ) );
+				it->first.insert( CStrW( iit->first.substr( rlen + 1 ) ).BeforeFirst( L"." ) );
 	}
 	else
 	{

binaries/data/mods/official/gui/test/functions_page_session_status_commands.js
@@ -308,7 +308,7 @@ function updateTab (tab, type, cellSheet, attribute, attribute2, arrayCells)
 			{	// If it's a list where each element is a value, (entity list)
 				for ( var i in attribute )
 				{
-					listArray[listArray.length] = lastPiece(i);
+					listArray[listArray.length] = i;
 					// Store any current quantity in the queue for this object.
 //					if (attribute[i].quantity)
 //						listArray[listArray.length].quantity = attribute[i].quantity;					
@@ -613,7 +613,6 @@ function refreshCommandButtons()
 			listRoot = selection[0].actions.create.list;
 			for (listTab in listRoot)
 			{
-				listTab = lastPiece(listTab);
 				console.write("Updating " + listTab);
 				if (listTab != "research")	// Do research later.
 					updateTab (listTab, "production", "Tab", "listRoot[listTab]", "");

binaries/data/mods/official/gui/test/functions_sim_player.js
@@ -62,7 +62,6 @@ function refreshResources ()
 	var shouldRefresh = false;
 	for (var currResource in resourcePool) 
 	{
-		currResource = lastPiece(currResource);
 		if( oldResources[currResource] != localPlayer.resources[currResource] )
 		{
 			oldResources[currResource] = localPlayer.resources[currResource].valueOf();
@@ -77,7 +76,6 @@ function refreshResources ()
 			var resourceCount = 0;
 			for (var currResource in resourcePool) 
 			{
-				currResource = lastPiece(currResource);
 				if(currResource != "housing")
 				{
 					// Pass the array index of the resource as the second parameter (as we'll need that to determine the centered screen position of each counter).
@@ -96,7 +94,7 @@ function refreshResource (resourceName, resourceIndex)
 	// Refresh the onscreen counter for a given resource (called to recalculate the size of the coordinates, as these dynamically adjust depending on character length).
 	
 	// Ensure resource name is title-case.
-	resourceName = toTitleCase (lastPiece(resourceName));	
+	resourceName = toTitleCase (resourceName);
 
 	// Ignore the "Housing" resource ... It doesn't work like normal resources and doesn't have a counter to resize.
 	if (resourceName == "Housing")

binaries/data/mods/official/gui/test/functions_utility.js
@@ -79,13 +79,3 @@ function toTitleCase (string)
 }
 
 // ====================================================================
-
-// Get last piece of a dot-separated string
-function lastPiece(x) {
-	var str = ""+x;
-	var idx = str.lastIndexOf('.');
-	if(idx >= 0) {
-		str = str.substring(idx+1, str.length);
-	}
-	return str;
-}