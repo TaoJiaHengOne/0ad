TaoJiaHengOne/0ad

commit e8cb2a71dadf77ed6027e0f0f82d3b798371e5c6

Fix floating units when a building they were tasked to construct finishes while they're still en-route, based on a patch by mattlott.
This was SVN commit r11775.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1368,6 +1368,11 @@ var UnitFsmSpec = {
 				// Save the current order's data in case we need it later
 				var oldAutocontinue = this.order.data.autocontinue;
 
+                // Save the current state so we can continue walking if necessary
+                // FinishOrder() below will switch to IDLE if there's no order, which sets the idle animation.
+                // Idle animation while moving towards finished construction looks weird (ghosty).
+                var oldState = this.GetCurrentState();
+
 				// We finished building it.
 				// Switch to the next order (if any)
 				if (this.FinishOrder())
@@ -1413,6 +1418,13 @@ var UnitFsmSpec = {
 					this.Repair(nearbyFoundation, oldAutocontinue, true);
 					return;
 				}
+
+                // Unit was approaching and there's nothing to do now, so switch to walking
+                if (oldState === "INDIVIDUAL.REPAIR.APPROACHING")
+                {
+					// We've already started walking to the given point
+					this.SetNextState("INDIVIDUAL.WALKING");
+                }
 			},
 
 			// Override the LeaveFoundation order since we don't want to be

commit 8f762b824865a0f20d195e033a52ce5dc54f91bd

Fix error caused by e8cb2a71da. Refs #1220.
This was SVN commit r11776.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1368,10 +1368,10 @@ var UnitFsmSpec = {
 				// Save the current order's data in case we need it later
 				var oldAutocontinue = this.order.data.autocontinue;
 
-                // Save the current state so we can continue walking if necessary
-                // FinishOrder() below will switch to IDLE if there's no order, which sets the idle animation.
-                // Idle animation while moving towards finished construction looks weird (ghosty).
-                var oldState = this.GetCurrentState();
+				// Save the current state so we can continue walking if necessary
+				// FinishOrder() below will switch to IDLE if there's no order, which sets the idle animation.
+				// Idle animation while moving towards finished construction looks weird (ghosty).
+				var oldState = this.GetCurrentState();
 
 				// We finished building it.
 				// Switch to the next order (if any)
@@ -1419,12 +1419,12 @@ var UnitFsmSpec = {
 					return;
 				}
 
-                // Unit was approaching and there's nothing to do now, so switch to walking
-                if (oldState === "INDIVIDUAL.REPAIR.APPROACHING")
-                {
-					// We've already started walking to the given point
-					this.SetNextState("INDIVIDUAL.WALKING");
-                }
+				// Unit was approaching and there's nothing to do now, so switch to walking
+				if (oldState === "INDIVIDUAL.REPAIR.APPROACHING")
+				{
+					// We've already walking to the given point, so add this as a order.
+					this.WalkToTarget(msg.data.newentity, true);
+				}
 			},
 
 			// Override the LeaveFoundation order since we don't want to be