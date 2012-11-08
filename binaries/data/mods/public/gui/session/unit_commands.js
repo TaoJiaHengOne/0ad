TaoJiaHengOne/0ad

commit 7e21db08d5a7b9dd9fcb3e21136bd7b754c8069d

Training limits. Limit heroes to one living per player. Allow heroes to be trained again. Closes #1432
This was SVN commit r12832.

binaries/data/mods/public/gui/session/unit_commands.js
@@ -139,6 +139,65 @@ function setOverlay(object, value)
 }
 
 /**
+ * Format entity count/limit message for the tooltip
+ */
+function formatLimitString(trainEntLimit, trainEntCount)
+{
+	if (trainEntLimit == undefined)
+		return "";
+	var text = "\n\nCurrent count: " + trainEntCount + ", limit: " + trainEntLimit + ".";
+	if (trainEntCount >= trainEntLimit)
+		text = "[color=\"red\"]" + text + "[/color]";
+	return text;
+}
+
+/**
+ * Format batch training string for the tooltip
+ * Examples:
+ * buildingsCountToTrainFullBatch = 1, fullBatchSize = 5, remainderBatch = 0:
+ * "Shift-click to train 5"
+ * buildingsCountToTrainFullBatch = 2, fullBatchSize = 5, remainderBatch = 0:
+ * "Shift-click to train 10 (2*5)"
+ * buildingsCountToTrainFullBatch = 1, fullBatchSize = 15, remainderBatch = 12:
+ * "Shift-click to train 27 (15 + 12)"
+ */
+function formatBatchTrainingString(buildingsCountToTrainFullBatch, fullBatchSize, remainderBatch)
+{
+	var totalBatchTrainingCount = buildingsCountToTrainFullBatch * fullBatchSize + remainderBatch;
+	// Don't show the batch training tooltip if either units of this type can't be trained at all
+	// or only one unit can be trained
+	if (totalBatchTrainingCount < 2)
+		return "";
+	var batchTrainingString = "";
+	var fullBatchesString = "";
+	if (buildingsCountToTrainFullBatch > 0)
+	{
+		if (buildingsCountToTrainFullBatch > 1)
+			fullBatchesString += buildingsCountToTrainFullBatch + "*";
+		fullBatchesString += fullBatchSize;
+	}
+	var remainderBatchString = remainderBatch > 0 ? remainderBatch : "";
+	var batchDetailsString = "";
+	// We need to display the batch details part if there is either more than
+	// one building with full batch or one building with the full batch and
+	// another with a partial batch
+	if (buildingsCountToTrainFullBatch > 1 ||
+		(buildingsCountToTrainFullBatch == 1 && remainderBatch > 0))
+	{
+		batchDetailsString += " (";
+		if (fullBatchesString != "" && remainderBatchString != "")
+			batchDetailsString += fullBatchesString + " + " + remainderBatchString;
+		else if (fullBatchesString != "")
+			batchDetailsString += fullBatchesString;
+		else
+			batchDetailsString += remainderBatchString;
+		batchDetailsString += ")";
+	}
+	return "\n\n[font=\"serif-bold-13\"]Shift-click[/font][font=\"serif-13\"] to train "
+		+ totalBatchTrainingCount + batchDetailsString + ".[/font]";
+}
+
+/**
  * Helper function for updateUnitCommands; sets up "unit panels" (i.e. panels with rows of icons) for the currently selected
  * unit.
  * 
@@ -150,7 +209,7 @@ function setOverlay(object, value)
  * @param items Panel-specific data to construct the icons with.
  * @param callback Callback function to argument to execute when an item's icon gets clicked. Takes a single 'item' argument.
  */
-function setupUnitPanel(guiName, usedPanels, unitEntState, items, callback)
+function setupUnitPanel(guiName, usedPanels, unitEntState, playerState, items, callback)
 {
 	usedPanels[guiName] = 1;
 
@@ -374,9 +433,6 @@ function setupUnitPanel(guiName, usedPanels, unitEntState, items, callback)
 				if (template.tooltip)
 					tooltip += "\n[font=\"serif-13\"]" + template.tooltip + "[/font]";
 
-				var [batchSize, batchIncrement] = getTrainingBatchStatus(unitEntState.id, entType);
-				var trainNum = batchSize ? batchSize+batchIncrement : batchIncrement;
-
 				tooltip += "\n" + getEntityCostTooltip(template);
 
 				if (template.health)
@@ -388,8 +444,13 @@ function setupUnitPanel(guiName, usedPanels, unitEntState, items, callback)
 				if (template.speed)
 					tooltip += "\n" + getEntitySpeed(template);
 
-				tooltip += "\n\n[font=\"serif-bold-13\"]Shift-click[/font][font=\"serif-13\"] to train " + trainNum + ".[/font]";
+				var [trainEntLimit, trainEntCount, canBeTrainedCount] =
+					getEntityLimitAndCount(playerState, entType)
+				tooltip += formatLimitString(trainEntLimit, trainEntCount);
 
+				var [buildingsCountToTrainFullBatch, fullBatchSize, remainderBatch] =
+					getTrainingBatchStatus(playerState, unitEntState.id, entType, selection);
+				tooltip += formatBatchTrainingString(buildingsCountToTrainFullBatch, fullBatchSize, remainderBatch);
 				break;
 				
 			case RESEARCH:
@@ -627,17 +688,30 @@ function setupUnitPanel(guiName, usedPanels, unitEntState, items, callback)
 					pair.hidden = true;
 					button1.hidden = true;
 					affordableMask1.hidden = true;
-  				}
+				}
 			}
 			else if (guiName == CONSTRUCTION || guiName == TRAINING)
 			{
+				if (guiName == TRAINING)
+				{
+					var trainingCategory = null;
+					if (template.trainingRestrictions)
+						trainingCategory = template.trainingRestrictions.category;
+					var grayscale = "";
+					if (trainingCategory && playerState.entityLimits[trainingCategory] &&
+						playerState.entityCounts[trainingCategory] >= playerState.entityLimits[trainingCategory])
+						grayscale = "grayscale:";
+					icon.sprite = "stretched:" + grayscale + "session/portraits/" + template.icon;
+				}
+
 				affordableMask.hidden = true;
 				var totalCosts = {};
 				var trainNum = 1;
 				if (Engine.HotkeyIsPressed("session.batchtrain") && guiName == TRAINING)
 				{
-					var [batchSize, batchIncrement] = getTrainingBatchStatus(unitEntState.id, entType);
-					trainNum = batchSize + batchIncrement;
+					var [buildingsCountToTrainFullBatch, fullBatchSize, remainderBatch] =
+						getTrainingBatchStatus(playerState, unitEntState.id, entType, selection);
+					trainNum = buildingsCountToTrainFullBatch * fullBatchSize + remainderBatch;
 				}
 
 				// Walls have no cost defined.
@@ -767,7 +841,7 @@ function setupUnitTradingPanel(usedPanels, unitEntState, selection)
 }
 
 // Sets up "unit barter panel" - special case for setupUnitPanel
-function setupUnitBarterPanel(unitEntState)
+function setupUnitBarterPanel(unitEntState, playerState)
 {
 	// Amount of player's resource to exchange
 	var amountToSell = BARTER_RESOURCE_AMOUNT_TO_SELL;
@@ -862,13 +936,18 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 	var player = Engine.GetPlayerID();
 	if (entState.player == player || g_DevSettings.controlAll)
 	{
+		// Get player state to check some constraints
+		// e.g. presence of a hero or build limits
+		var simState = Engine.GuiInterfaceCall("GetSimulationState");
+		var playerState = simState.players[player];
+
 		if (selection.length > 1)
-			setupUnitPanel(SELECTION, usedPanels, entState, g_Selection.groups.getTemplateNames(),
+			setupUnitPanel(SELECTION, usedPanels, entState, playerState, g_Selection.groups.getTemplateNames(),
 				function (entType) { changePrimarySelectionGroup(entType); } );
 
 		var commands = getEntityCommandsList(entState);
 		if (commands.length)
-			setupUnitPanel(COMMAND, usedPanels, entState, commands,
+			setupUnitPanel(COMMAND, usedPanels, entState, playerState, commands,
 				function (item) { performCommand(entState.id, item.name); } );
 
 		if (entState.garrisonHolder)
@@ -881,14 +960,14 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 					groups.add(state.garrisonHolder.entities)
 			}
 
-			setupUnitPanel(GARRISON, usedPanels, entState, groups.getTemplateNames(),
+			setupUnitPanel(GARRISON, usedPanels, entState, playerState, groups.getTemplateNames(),
 				function (item) { unloadTemplate(item); } );
 		}
 
 		var formations = Engine.GuiInterfaceCall("GetAvailableFormations");
 		if (hasClass(entState, "Unit") && !hasClass(entState, "Animal") && !entState.garrisonHolder && formations.length)
 		{
-			setupUnitPanel(FORMATION, usedPanels, entState, formations,
+			setupUnitPanel(FORMATION, usedPanels, entState, playerState, formations,
 				function (item) { performFormation(entState.id, item); } );
 		}
 
@@ -897,7 +976,7 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 		var stances = ["violent", "aggressive", "passive", "defensive", "standground"];
 		if (hasClass(entState, "Unit") && !hasClass(entState, "Animal") && stances.length)
 		{
-			setupUnitPanel(STANCE, usedPanels, entState, stances,
+			setupUnitPanel(STANCE, usedPanels, entState, playerState, stances,
 				function (item) { performStance(entState.id, item); } );
 		}
 
@@ -905,7 +984,7 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 		if (entState.barterMarket)
 		{
 			usedPanels["Barter"] = 1;
-			setupUnitBarterPanel(entState);
+			setupUnitBarterPanel(entState, playerState);
 		}
 
 		var buildableEnts = [];
@@ -929,10 +1008,10 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 
 		// The first selected entity's type has priority.
 		if (entState.buildEntities)
-			setupUnitPanel(CONSTRUCTION, usedPanels, entState, buildableEnts, startBuildingPlacement);
+			setupUnitPanel(CONSTRUCTION, usedPanels, entState, playerState, buildableEnts, startBuildingPlacement);
 		else if (entState.production && entState.production.entities)
-			setupUnitPanel(TRAINING, usedPanels, entState, trainableEnts,
-				function (trainEntType) { addTrainingToQueue(selection, trainEntType); } );
+			setupUnitPanel(TRAINING, usedPanels, entState, playerState, trainableEnts,
+				function (trainEntType) { addTrainingToQueue(selection, trainEntType, playerState); } );
 		else if (entState.trader)
 			setupUnitTradingPanel(usedPanels, entState, selection);
 		else if (!entState.foundation && entState.gate || hasClass(entState, "LongWall"))
@@ -994,7 +1073,7 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 				setupUnitPanel(CONSTRUCTION, usedPanels, entState, buildableEnts, startBuildingPlacement);
 			else if (trainableEnts.length)
 				setupUnitPanel(TRAINING, usedPanels, entState, trainableEnts,
-					function (trainEntType) { addTrainingToQueue(selection, trainEntType); } );
+					function (trainEntType) { addTrainingToQueue(selection, trainEntType, playerState); } );
 		}
 
 		// Show technologies if the active panel has at most one row of icons.
@@ -1007,7 +1086,7 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 		}
 
 		if (entState.production && entState.production.queue.length)
-			setupUnitPanel(QUEUE, usedPanels, entState, entState.production.queue,
+			setupUnitPanel(QUEUE, usedPanels, entState, playerState, entState.production.queue,
 				function (item) { removeFromProductionQueue(entState.id, item.id); } );
 		
 		supplementalDetailsPanel.hidden = false;

binaries/data/mods/public/gui/session/input.js
@@ -1467,17 +1467,76 @@ function exchangeResources(command)
 var batchTrainingEntities;
 var batchTrainingType;
 var batchTrainingCount;
+var batchTrainingEntityAllowedCount;
 const batchIncrementSize = 5;
 
 function flushTrainingBatch()
 {
-	Engine.PostNetworkCommand({"type": "train", "entities": batchTrainingEntities, "template": batchTrainingType, "count": batchTrainingCount});
+	var appropriateBuildings = getBuidlingsWhichCanTrainEntity(batchTrainingEntities, batchTrainingType);
+	// If training limits don't allow us to train batchTrainingCount in each appropriate building
+	if (batchTrainingEntityAllowedCount !== undefined &&
+		batchTrainingEntityAllowedCount < batchTrainingCount * appropriateBuildings.length)
+	{
+		// Train as many full batches as we can
+		var buildingsCountToTrainFullBatch = Math.floor(batchTrainingEntityAllowedCount / batchTrainingCount);
+		var buildingsToTrainFullBatch = appropriateBuildings.slice(0, buildingsCountToTrainFullBatch);
+		Engine.PostNetworkCommand({"type": "train", "entities": buildingsToTrainFullBatch,
+			"template": batchTrainingType, "count": batchTrainingCount});
+
+		// Train remainer in one more building
+		var remainderToTrain = batchTrainingEntityAllowedCount % batchTrainingCount;
+		Engine.PostNetworkCommand({"type": "train",
+			"entities": [ appropriateBuildings[buildingsCountToTrainFullBatch] ],
+			"template": batchTrainingType, "count": remainderToTrain});
+	}
+	else
+	{
+		Engine.PostNetworkCommand({"type": "train", "entities": appropriateBuildings,
+			"template": batchTrainingType, "count": batchTrainingCount});
+	}
+}
+
+function getBuidlingsWhichCanTrainEntity(entitiesToCheck, trainEntType)
+{
+	return entitiesToCheck.filter(function(entity) {
+		var state = GetEntityState(entity);
+		var canTrain = state && state.production && state.production.entities.length &&
+			state.production.entities.indexOf(trainEntType) != -1;
+		return canTrain;
+	});
+}
+
+function getEntityLimitAndCount(playerState, entType)
+{
+	var template = GetTemplateData(entType);
+	var trainingCategory = null;
+	if (template.trainingRestrictions)
+		trainingCategory = template.trainingRestrictions.category;
+	var trainEntLimit = undefined;
+	var trainEntCount = undefined;
+	var canBeTrainedCount = undefined;
+	if (trainingCategory && playerState.entityLimits[trainingCategory])
+	{
+		trainEntLimit = playerState.entityLimits[trainingCategory];
+		trainEntCount = playerState.entityCounts[trainingCategory];
+		canBeTrainedCount = trainEntLimit - trainEntCount;
+	}
+	return [trainEntLimit, trainEntCount, canBeTrainedCount];
 }
 
 // Called by GUI when user clicks training button
-function addTrainingToQueue(selection, trainEntType)
+function addTrainingToQueue(selection, trainEntType, playerState)
 {
-	if (Engine.HotkeyIsPressed("session.batchtrain"))
+	// Create list of buildings which can train trainEntType
+	var appropriateBuildings = getBuidlingsWhichCanTrainEntity(selection, trainEntType);
+
+	// Check trainEntType entity limit and count
+	var [trainEntLimit, trainEntCount, canBeTrainedCount] = getEntityLimitAndCount(playerState, trainEntType)
+
+	// Batch training possible if we can train at least 2 units
+	var batchTrainingPossible = canBeTrainedCount == undefined || canBeTrainedCount > 1;
+
+	if (Engine.HotkeyIsPressed("session.batchtrain") && batchTrainingPossible)
 	{
 		if (inputState == INPUT_BATCHTRAINING)
 		{
@@ -1494,9 +1553,13 @@ function addTrainingToQueue(selection, trainEntType)
 				}
 			}
 			// If we're already creating a batch of this unit (in the same building(s)), then just extend it
+			// (if training limits allow)
 			if (sameEnts && batchTrainingType == trainEntType)
 			{
-				batchTrainingCount += batchIncrementSize;
+				if (canBeTrainedCount == undefined ||
+					canBeTrainedCount > batchTrainingCount * appropriateBuildings.length)
+					batchTrainingCount += batchIncrementSize;
+				batchTrainingEntityAllowedCount = canBeTrainedCount;
 				return;
 			}
 			// Otherwise start a new one
@@ -1509,12 +1572,18 @@ function addTrainingToQueue(selection, trainEntType)
 		inputState = INPUT_BATCHTRAINING;
 		batchTrainingEntities = selection;
 		batchTrainingType = trainEntType;
+		batchTrainingEntityAllowedCount = canBeTrainedCount;
 		batchTrainingCount = batchIncrementSize;
 	}
 	else
 	{
-		// Non-batched - just create a single entity
-		Engine.PostNetworkCommand({"type": "train", "template": trainEntType, "count": 1, "entities": selection});
+		// Non-batched - just create a single entity in each building
+		// (but no more than entity limit allows)
+		var buildingsForTraining = appropriateBuildings;
+		if (trainEntLimit)
+			buildingsForTraining = buildingsForTraining.slice(0, canBeTrainedCount);
+		Engine.PostNetworkCommand({"type": "train", "template": trainEntType,
+			"count": 1, "entities": buildingsForTraining});
 	}
 }
 
@@ -1526,12 +1595,39 @@ function addResearchToQueue(entity, researchType)
 
 // Returns the number of units that will be present in a batch if the user clicks
 // the training button with shift down
-function getTrainingBatchStatus(entity, trainEntType)
+function getTrainingBatchStatus(playerState, entity, trainEntType, selection)
 {
-	if (inputState == INPUT_BATCHTRAINING && batchTrainingEntities.indexOf(entity) != -1 && batchTrainingType == trainEntType)
-		return [batchTrainingCount, batchIncrementSize];
+	var apporpriateBuildings = [entity];
+	if (selection && selection.indexOf(entity) != -1)
+		appropriateBuildings = getBuidlingsWhichCanTrainEntity(selection, trainEntType);
+	var nextBatchTrainingCount = 0;
+	if (inputState == INPUT_BATCHTRAINING && batchTrainingEntities.indexOf(entity) != -1 &&
+		batchTrainingType == trainEntType)
+	{
+		nextBatchTrainingCount = batchTrainingCount;
+		var canBeTrainedCount = batchTrainingEntityAllowedCount;
+	}
 	else
-		return [0, batchIncrementSize];
+	{
+		var [trainEntLimit, trainEntCount, canBeTrainedCount] =
+			getEntityLimitAndCount(playerState, trainEntType);
+		var batchSize = Math.min(canBeTrainedCount, batchIncrementSize);
+	}
+	// We need to calculate count after the next increment if it's possible
+	if (canBeTrainedCount == undefined ||
+		canBeTrainedCount > nextBatchTrainingCount * appropriateBuildings.length)
+		nextBatchTrainingCount += batchIncrementSize;
+	// If training limits don't allow us to train batchTrainingCount in each appropriate building
+	// train as many full batches as we can and remainer in one more building.
+	var buildingsCountToTrainFullBatch = appropriateBuildings.length;
+	var remainderToTrain = 0;
+	if (canBeTrainedCount !== undefined &&
+		canBeTrainedCount < nextBatchTrainingCount * appropriateBuildings.length)
+	{
+		buildingsCountToTrainFullBatch = Math.floor(canBeTrainedCount / nextBatchTrainingCount);
+		remainderToTrain = canBeTrainedCount % nextBatchTrainingCount;
+	}
+	return [buildingsCountToTrainFullBatch, nextBatchTrainingCount, remainderToTrain];
 }
 
 // Called by GUI when user clicks production queue item

binaries/data/mods/public/simulation/ai/jubot/gamestate.js
@@ -416,33 +416,35 @@ var GameState = Class({
 	 * Returns player build limits
 	 * an object where each key is a category corresponding to a build limit for the player.
 	 */
-	getBuildLimits: function()
+	getEntityLimits: function()
 	{
-		return this.playerData.buildLimits;
+		return this.playerData.entityLimits;
 	},
 	
 	/**
 	 * Returns player build counts
 	 * an object where each key is a category corresponding to the current building count for the player.
 	 */
-	getBuildCounts: function()
+	getEntityCounts: function()
 	{
-		return this.playerData.buildCounts;
+		return this.playerData.entityCounts;
 	},
 	
 	/**
 	 * Checks if the player's build limit has been reached for the given category.
-	 * The category comes from the entity tenplate, specifically the BuildRestrictions component.
+	 * The category comes from the entity template, specifically the
+	 * BuildRestrictions/TrainingRestrictions components.
 	 */
-	isBuildLimitReached: function(category)
+	isEntityLimitReached: function(category)
 	{
-		if (this.playerData.buildLimits[category] === undefined || this.playerData.buildCounts[category] === undefined)
+		if (this.playerData.entityLimits[category] === undefined || this.playerData.entityCounts[category] === undefined)
 			return false;
 		
 		// There's a special case of build limits per civ centre, so check that first
-		if (this.playerData.buildLimits[category].LimitPerCivCentre !== undefined)
-			return (this.playerData.buildCounts[category] >= this.playerData.buildCounts["CivilCentre"]*this.playerData.buildLimits[category].LimitPerCivCentre);
+		if (this.playerData.entityLimits[category].LimitPerCivCentre !== undefined)
+			return (this.playerData.entityCounts[category] >=
+				this.playerData.entityCounts["CivilCentre"] * this.playerData.entityLimits[category].LimitPerCivCentre);
 		else
-			return (this.playerData.buildCounts[category] >= this.playerData.buildLimits[category]);
+			return (this.playerData.entityCounts[category] >= this.playerData.entityLimits[category]);
 	},
 });

binaries/data/mods/public/simulation/ai/qbot-wc/gamestate.js
@@ -315,22 +315,23 @@ GameState.prototype.getResourceSupplies = function(resource){
 	return this.updatingCollection("resource-" + resource, Filters.byResource(resource), this.getEntities());
 };
 
-GameState.prototype.getBuildLimits = function() {
-	return this.playerData.buildLimits;
+GameState.prototype.getEntityLimits = function() {
+	return this.playerData.entityLimits;
 };
 
-GameState.prototype.getBuildCounts = function() {
-	return this.playerData.buildCounts;
+GameState.prototype.getEntityCounts = function() {
+	return this.playerData.entityCounts;
 };
 
-// Checks whether the maximum number of buildings have been cnstructed for a certain catergory
-GameState.prototype.isBuildLimitReached = function(category) {
-	if(this.playerData.buildLimits[category] === undefined || this.playerData.buildCounts[category] === undefined)
+// Checks whether the maximum number of buildings have been constructed for a certain catergory
+GameState.prototype.isEntityLimitReached = function(category) {
+	if(this.playerData.entityLimits[category] === undefined || this.playerData.entityCounts[category] === undefined)
 		return false;
-	if(this.playerData.buildLimits[category].LimitsPerCivCentre != undefined)
-		return (this.playerData.buildCounts[category] >= this.playerData.buildCounts["CivilCentre"]*this.playerData.buildLimits[category].LimitPerCivCentre);
+	if(this.playerData.entityLimits[category].LimitsPerCivCentre != undefined)
+		return (this.playerData.entityCounts[category] >=
+			this.playerData.entityCounts["CivilCentre"] * this.playerData.entityLimits[category].LimitPerCivCentre);
 	else
-		return (this.playerData.buildCounts[category] >= this.playerData.buildLimits[category]);
+		return (this.playerData.entityCounts[category] >= this.playerData.entityLimits[category]);
 };
 
 GameState.prototype.findTrainableUnits = function(classes){

binaries/data/mods/public/simulation/ai/qbot-wc/military.js
@@ -368,7 +368,7 @@ MilitaryAttackManager.prototype.measureEnemyStrength = function(gameState){
 // Adds towers to the defenceBuilding queue
 MilitaryAttackManager.prototype.buildDefences = function(gameState, queues){ 
 	if (gameState.countEntitiesAndQueuedByType(gameState.applyCiv('structures/{civ}_defense_tower'))
-			+ queues.defenceBuilding.totalLength() < gameState.getBuildLimits()["DefenseTower"]) {
+			+ queues.defenceBuilding.totalLength() < gameState.getEntityLimits()["DefenseTower"]) {
 		
 		gameState.getOwnEntities().forEach(function(dropsiteEnt) {
 			if (dropsiteEnt.resourceDropsiteTypes() && dropsiteEnt.getMetadata("defenseTower") !== true){
@@ -386,7 +386,7 @@ MilitaryAttackManager.prototype.buildDefences = function(gameState, queues){
 		numFortresses += gameState.countEntitiesAndQueuedByType(gameState.applyCiv(this.bFort[i]));
 	}
 	
-	if (numFortresses + queues.defenceBuilding.totalLength() < 1){ //gameState.getBuildLimits()["Fortress"]) {
+	if (numFortresses + queues.defenceBuilding.totalLength() < 1){ //gameState.getEntityLimits()["Fortress"]) {
 		if (gameState.getTimeElapsed() > 840 * 1000 + numFortresses * 300 * 1000){
 			if (gameState.ai.pathsToMe && gameState.ai.pathsToMe.length > 0){
 				var position = gameState.ai.pathsToMe.shift();

binaries/data/mods/public/simulation/ai/qbot/gamestate.js
@@ -303,20 +303,21 @@ GameState.prototype.getResourceSupplies = function(resource){
 	return this.updatingCollection("resource-" + resource, Filters.byResource(resource), this.getEntities());
 };
 
-GameState.prototype.getBuildLimits = function() {
-	return this.playerData.buildLimits;
+GameState.prototype.getEntityLimits = function() {
+	return this.playerData.entityLimits;
 };
 
-GameState.prototype.getBuildCounts = function() {
-	return this.playerData.buildCounts;
+GameState.prototype.getEntityCounts = function() {
+	return this.playerData.entityCounts;
 };
 
-// Checks whether the maximum number of buildings have been cnstructed for a certain catergory
-GameState.prototype.isBuildLimitReached = function(category) {
-	if(this.playerData.buildLimits[category] === undefined || this.playerData.buildCounts[category] === undefined)
+// Checks whether the maximum number of buildings have been constructed for a certain catergory
+GameState.prototype.isEntityLimitReached = function(category) {
+	if(this.playerData.entityLimits[category] === undefined || this.playerData.entityCounts[category] === undefined)
 		return false;
-	if(this.playerData.buildLimits[category].LimitsPerCivCentre != undefined)
-		return (this.playerData.buildCounts[category] >= this.playerData.buildCounts["CivilCentre"]*this.playerData.buildLimits[category].LimitPerCivCentre);
+	if(this.playerData.entityLimits[category].LimitsPerCivCentre != undefined)
+		return (this.playerData.entityCounts[category] >=
+			this.playerData.entityCounts["CivilCentre"] * this.playerData.entityLimits[category].LimitPerCivCentre);
 	else
-		return (this.playerData.buildCounts[category] >= this.playerData.buildLimits[category]);
+		return (this.playerData.entityCounts[category] >= this.playerData.entityLimits[category]);
 };

binaries/data/mods/public/simulation/ai/qbot/military.js
@@ -324,7 +324,7 @@ MilitaryAttackManager.prototype.measureEnemyStrength = function(gameState){
 // Adds towers to the defenceBuilding queue
 MilitaryAttackManager.prototype.buildDefences = function(gameState, queues){ 
 	if (gameState.countEntitiesAndQueuedByType(gameState.applyCiv('structures/{civ}_defense_tower'))
-			+ queues.defenceBuilding.totalLength() < gameState.getBuildLimits()["DefenseTower"]) {
+			+ queues.defenceBuilding.totalLength() < gameState.getEntityLimits()["DefenseTower"]) {
 		
 		
 		gameState.getOwnEntities().forEach(function(dropsiteEnt) {
@@ -343,7 +343,7 @@ MilitaryAttackManager.prototype.buildDefences = function(gameState, queues){
 		numFortresses += gameState.countEntitiesAndQueuedByType(gameState.applyCiv(this.bFort[i]));
 	}
 	
-	if (numFortresses + queues.defenceBuilding.totalLength() < gameState.getBuildLimits()["Fortress"]) {
+	if (numFortresses + queues.defenceBuilding.totalLength() < gameState.getEntityLimits()["Fortress"]) {
 		if (gameState.countEntitiesByType(gameState.applyCiv("units/{civ}_support_female_citizen")) > gameState.ai.modules["economy"].targetNumWorkers * 0.5){
 			if (gameState.getTimeElapsed() > 350 * 1000 * numFortresses){
 				if (gameState.ai.pathsToMe && gameState.ai.pathsToMe.length > 0){

commit 3a02bc296c4093bd1d5b5a29eb718e408ef5daa5

Fix setupUnitPanel usage broken in 7e21db08d5. Refs #1432
This was SVN commit r12835.

binaries/data/mods/public/gui/session/unit_commands.js
@@ -1058,7 +1058,7 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 			// Place wall conversion options after gate lock/unlock icons.
 			var items = gates.concat(walls);
 			if (items.length)
-				setupUnitPanel(GATE, usedPanels, entState, items);
+				setupUnitPanel(GATE, usedPanels, entState, playerState, items);
 			else
 				rightUsed = false;
 		}
@@ -1070,18 +1070,17 @@ function updateUnitCommands(entState, supplementalDetailsPanel, commandsPanel, s
 			// The right pane is empty. Fill the pane with a sane type.
 			// Prefer buildables for units and trainables for structures.
 			if (buildableEnts.length && (hasClass(entState, "Unit") || !trainableEnts.length))
-				setupUnitPanel(CONSTRUCTION, usedPanels, entState, buildableEnts, startBuildingPlacement);
+				setupUnitPanel(CONSTRUCTION, usedPanels, entState, playerState, buildableEnts, startBuildingPlacement);
 			else if (trainableEnts.length)
-				setupUnitPanel(TRAINING, usedPanels, entState, trainableEnts,
+				setupUnitPanel(TRAINING, usedPanels, entState, playerState, trainableEnts,
 					function (trainEntType) { addTrainingToQueue(selection, trainEntType, playerState); } );
 		}
-
 		// Show technologies if the active panel has at most one row of icons.
 		if (entState.production && entState.production.technologies.length)
 		{
 			var activepane = usedPanels[CONSTRUCTION] ? buildableEnts.length : trainableEnts.length;
 			if (selection.length == 1 || activepane <= 8)
-				setupUnitPanel(RESEARCH, usedPanels, entState, entState.production.technologies,
+				setupUnitPanel(RESEARCH, usedPanels, entState, playerState, entState.production.technologies,
 					function (researchType) { addResearchToQueue(entState.id, researchType); } );
 		}
 