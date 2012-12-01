TaoJiaHengOne/0ad

commit 3787f17e4da693e27ab7c5c8c8888548a57776a1

merge with local codebase: fix PopulationCount
This was SVN commit r12907.

source/lib/bits.h
@@ -147,11 +147,12 @@ inline size_t SparsePopulationCount(T mask)
 template<typename T>
 static inline size_t PopulationCount(T x)
 {
+    cassert(!std::numeric_limits<T>::is_signed);
 	const T mask = T(~T(0));
 	x -= (x >> 1) & (mask/3);	// count 2 bits
 	x = (x & (mask/15*3)) + ((x >> 2) & (mask/15*3));	// count 4 bits
 	x = (x + (x >> 4)) & (mask/255*15);	// count 8 bits
-	return (x * (mask/255)) >> ((sizeof(T)-1)*CHAR_BIT);
+	return T(x * (mask/255)) >> ((sizeof(T)-1)*CHAR_BIT);
 }
 
 

commit 9d0da11eab2d5c1adba354e2dbd39a62b54c5f77

Fixes compile error introduced in 3787f17e4d
This was SVN commit r12909.

source/lib/bits.h
@@ -147,7 +147,7 @@ inline size_t SparsePopulationCount(T mask)
 template<typename T>
 static inline size_t PopulationCount(T x)
 {
-    cassert(!std::numeric_limits<T>::is_signed);
+    cassert_dependent(!std::numeric_limits<T>::is_signed);
 	const T mask = T(~T(0));
 	x -= (x >> 1) & (mask/3);	// count 2 bits
 	x = (x & (mask/15*3)) + ((x >> 2) & (mask/15*3));	// count 4 bits