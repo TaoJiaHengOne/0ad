TaoJiaHengOne/0ad

commit af8a6f5e8c0278d23e88033532f9b75960180110

Removes generic hele and celt civs. Based on patch by prodigalson and comments by s0600204. Fixes #2619.
Also removes the theb civ json file.

This was SVN commit r16416.

binaries/data/mods/public/simulation/helpers/Cheat.js
@@ -95,10 +95,7 @@ function Cheat(input)
 			special = {"athen":"athen"};
 		}
 		else if (!cmpTechnologyManager.IsTechnologyResearched("phase_city"))
-		{
 			phaseToGo = "city";
-			special = {"athen":"athen", "celt": Math.round(Math.random()) == 0 ? "britons":"gauls"};
-		}
 		else
 			return;
 

commit 09796350c817d4e6e08808bf4fb5ec2d2e7c33c9

Fix changephase cheat broken in af8a6f5e8c.
This was SVN commit r16443.

binaries/data/mods/public/simulation/helpers/Cheat.js
@@ -95,7 +95,10 @@ function Cheat(input)
 			special = {"athen":"athen"};
 		}
 		else if (!cmpTechnologyManager.IsTechnologyResearched("phase_city"))
+		{
 			phaseToGo = "city";
+			special = {"athen":"athen"};
+		}
 		else
 			return;
 