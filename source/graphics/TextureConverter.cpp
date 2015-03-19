TaoJiaHengOne/0ad

commit 4741e896c4038aedebf56e8d868dbc99784364c4

Improves handling of PNG textures based on GdkPixbuf's setup_png_transformations. The PNG decoder should now support grayscale, RGB or indexed color; bit depth up to 16; and interlacing. Fixes #1640, refs #2823, #3082
This was SVN commit r16439.

source/lib/tex/tex_png.cpp
@@ -125,22 +125,58 @@ static Status png_decode_impl(MemoryStream* stream, png_structp png_ptr, png_inf
 	// read header and determine format
 	png_read_info(png_ptr, info_ptr);
 	png_uint_32 w, h;
-	int bit_depth, color_type;
-	png_get_IHDR(png_ptr, info_ptr, &w, &h, &bit_depth, &color_type, 0, 0, 0);
+	int bit_depth, color_type, interlace_type;
+	png_get_IHDR(png_ptr, info_ptr, &w, &h, &bit_depth, &color_type, &interlace_type, 0, 0);
+	
+	// (The following is based on GdkPixbuf's PNG image loader)
+
+	// Convert the following images to 8-bit RGB/RGBA:
+	// * indexed colors
+	// * grayscale
+	// * transparency header
+	// * bit depth of 16 or less than 8
+	// * interlaced
+	if (color_type == PNG_COLOR_TYPE_PALETTE && bit_depth <= 8)
+		png_set_expand(png_ptr);
+	else if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+		png_set_expand(png_ptr);
+	else if (bit_depth < 8)
+		png_set_expand(png_ptr);
+
+	if (bit_depth == 16)
+		png_set_strip_16(png_ptr);
+	if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+		png_set_gray_to_rgb(png_ptr);
+	if (interlace_type != PNG_INTERLACE_NONE)
+		png_set_interlace_handling(png_ptr);
+
+	// Update info after transformations
+	png_read_update_info(png_ptr, info_ptr);
+	
+	png_get_IHDR(png_ptr, info_ptr, &w, &h, &bit_depth, &color_type, &interlace_type, 0, 0);
+
+	// make sure format is acceptable:
+	// * non-zero dimensions
+	// * 8-bit depth
+	// * RGB or RGBA
+	// * 3 or 4 channels
+	if (w == 0 || h == 0)
+		WARN_RETURN(ERR::TEX_INVALID_SIZE);
+	if (bit_depth != 8)
+		WARN_RETURN(ERR::TEX_NOT_8BIT_PRECISION);
+	if (color_type != PNG_COLOR_TYPE_RGB && color_type != PNG_COLOR_TYPE_RGB_ALPHA)
+		WARN_RETURN(ERR::TEX_INVALID_COLOR_TYPE);
+	
+	const int channels = png_get_channels(png_ptr, info_ptr);
+	if (channels != 3 && channels != 4)
+		WARN_RETURN(ERR::TEX_FMT_INVALID);
+
 	const size_t pitch = png_get_rowbytes(png_ptr, info_ptr);
-	const u32 bpp = (u32)(pitch/w * 8);
+	const u32 bpp = (u32)(pitch / w * 8);
 
 	size_t flags = 0;
-	if(bpp == 32)
+	if (color_type == PNG_COLOR_TYPE_RGB_ALPHA)
 		flags |= TEX_ALPHA;
-	if(color_type == PNG_COLOR_TYPE_GRAY)
-		flags |= TEX_GREY;
-
-	// make sure format is acceptable
-	if(bit_depth != 8)
-		WARN_RETURN(ERR::TEX_NOT_8BIT_PRECISION);
-	if(color_type & PNG_COLOR_MASK_PALETTE)
-		WARN_RETURN(ERR::TEX_INVALID_COLOR_TYPE);
 
 	const size_t img_size = pitch * h;
 	shared_ptr<u8> data;

commit 89107d1406e143284a6325659b6c071524ca9003

Fixes grayscale PNG handling, there is no need to convert them all to RGB and 4741e896c4 broke special font texture handling. Instead, only grayscale with alpha PNGs will be converted to RGBA (they fail to load otherwise).
Removes warning about converting grayscale textures, refs #1640.

This was SVN commit r16449.

source/graphics/TextureConverter.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013 Wildfire Games.
+/* Copyright (C) 2015 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -351,15 +351,6 @@ bool CTextureConverter::ConvertTexture(const CTexturePtr& texture, const VfsPath
 	}
 	else
 	{
-		// TODO: grayscale images will fail on some systems
-		// see http://trac.wildfiregames.com/ticket/1640
-		// (plain_transform doesn't know how to construct the alpha channel)
-		if (tex.m_Flags & TEX_GREY)
-		{
-			LOGERROR("Failed to convert grayscale texture \"%s\" - only RGB textures are currently supported", src.string8());
-			return false;
-		}
-
 		// Convert to uncompressed BGRA with no mipmaps
 		if (tex.transform_to((tex.m_Flags | TEX_BGR | TEX_ALPHA) & ~(TEX_DXT | TEX_MIPMAPS)) < 0)
 		{

source/lib/tex/tex_png.cpp
@@ -132,7 +132,7 @@ static Status png_decode_impl(MemoryStream* stream, png_structp png_ptr, png_inf
 
 	// Convert the following images to 8-bit RGB/RGBA:
 	// * indexed colors
-	// * grayscale
+	// * grayscale with alpha
 	// * transparency header
 	// * bit depth of 16 or less than 8
 	// * interlaced
@@ -145,7 +145,7 @@ static Status png_decode_impl(MemoryStream* stream, png_structp png_ptr, png_inf
 
 	if (bit_depth == 16)
 		png_set_strip_16(png_ptr);
-	if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+	if (color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
 		png_set_gray_to_rgb(png_ptr);
 	if (interlace_type != PNG_INTERLACE_NONE)
 		png_set_interlace_handling(png_ptr);
@@ -158,17 +158,17 @@ static Status png_decode_impl(MemoryStream* stream, png_structp png_ptr, png_inf
 	// make sure format is acceptable:
 	// * non-zero dimensions
 	// * 8-bit depth
-	// * RGB or RGBA
-	// * 3 or 4 channels
+	// * RGB, RGBA, or grayscale
+	// * 1, 3 or 4 channels
 	if (w == 0 || h == 0)
 		WARN_RETURN(ERR::TEX_INVALID_SIZE);
 	if (bit_depth != 8)
 		WARN_RETURN(ERR::TEX_NOT_8BIT_PRECISION);
-	if (color_type != PNG_COLOR_TYPE_RGB && color_type != PNG_COLOR_TYPE_RGB_ALPHA)
+	if (!(color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_RGB_ALPHA || color_type == PNG_COLOR_TYPE_GRAY))
 		WARN_RETURN(ERR::TEX_INVALID_COLOR_TYPE);
 	
 	const int channels = png_get_channels(png_ptr, info_ptr);
-	if (channels != 3 && channels != 4)
+	if (!(channels == 3 || channels == 4 || channels == 1))
 		WARN_RETURN(ERR::TEX_FMT_INVALID);
 
 	const size_t pitch = png_get_rowbytes(png_ptr, info_ptr);
@@ -177,6 +177,8 @@ static Status png_decode_impl(MemoryStream* stream, png_structp png_ptr, png_inf
 	size_t flags = 0;
 	if (color_type == PNG_COLOR_TYPE_RGB_ALPHA)
 		flags |= TEX_ALPHA;
+	if (color_type == PNG_COLOR_TYPE_GRAY)
+		flags |= TEX_GREY;
 
 	const size_t img_size = pitch * h;
 	shared_ptr<u8> data;