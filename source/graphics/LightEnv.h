TaoJiaHengOne/0ad

commit cc67d54aeb3945f5ad100071f1ac655b4304446e

Fix compiler warnings on VS 2015, refs #3439, refs #5069.
Patch By: Stan
Reviewed By: Itms, vladislavbelov
Differential Revision: https://code.wildfiregames.com/D1262
This was SVN commit r21480.

source/graphics/LightEnv.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -128,7 +128,7 @@ public:
 	SColor4ub EvaluateTerrainDiffuseFactor(const CVector3D& normal) const
 	{
 		float dot = -normal.Dot(m_SunDir);
-		int c = clamp((int)(dot * 255), 0, 255);
+		u8 c = clamp(static_cast<u8>(dot * 255), static_cast<u8>(0), static_cast<u8>(255));
 		return SColor4ub(c, c, c, 255);
 	}
 

source/collada/CommonConvert.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -311,13 +311,13 @@ void SkinReduceInfluences(FCDSkinController* skin, size_t maxInfluenceCount, flo
 		FCDSkinControllerVertex& influence = *skin->GetVertexInfluence(i);
 
 		std::vector<FCDJointWeightPair> newWeights;
-		for (size_t i = 0; i < influence.GetPairCount(); ++i)
+		for (size_t j = 0; j < influence.GetPairCount(); ++j)
 		{
-			FCDJointWeightPair* weight = influence.GetPair(i);
+			FCDJointWeightPair* weight = influence.GetPair(j);
 
-			for (size_t j = 0; j < newWeights.size(); ++j)
+			for (size_t k = 0; k < newWeights.size(); ++k)
 			{
-				FCDJointWeightPair& newWeight = newWeights[j];
+				FCDJointWeightPair& newWeight = newWeights[k];
 				if (weight->jointIndex == newWeight.jointIndex)
 				{
 					newWeight.weight += weight->weight;

source/collada/Decompose.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -32,15 +32,15 @@
 #define mat_pad(A) (A[W][X]=A[X][W]=A[W][Y]=A[Y][W]=A[W][Z]=A[Z][W]=0,A[W][W]=1)
 
 /** Copy nxn matrix A to C using "gets" for assignment **/
-#define mat_copy(C,gets,A,n) {int i,j; for(i=0;i<n;i++) for(j=0;j<n;j++)\
+#define mat_copy(C,gets,A,n) {for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j)\
     C[i][j] gets (A[i][j]);}
 
 /** Copy transpose of nxn matrix A to C using "gets" for assignment **/
-#define mat_tpose(AT,gets,A,n) {int i,j; for(i=0;i<n;i++) for(j=0;j<n;j++)\
+#define mat_tpose(AT,gets,A,n) {for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j)\
     AT[i][j] gets (A[j][i]);}
 
 /** Assign nxn matrix C the element-wise combination of A and B using "op" **/
-#define mat_binop(C,gets,A,op,B,n) {int i,j; for(i=0;i<n;i++) for(j=0;j<n;j++)\
+#define mat_binop(C,gets,A,op,B,n) {for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j)\
     C[i][j] gets (A[i][j]) op (B[i][j]);}
 
 /** Multiply the upper left 3x3 parts of A and B to get AB **/
@@ -276,7 +276,6 @@ float polar_decomp(HMatrix M, HMatrix Q, HMatrix S)
 #define TOL 1.0e-6
     HMatrix Mk, MadjTk, Ek;
     float det, M_one, M_inf, MadjT_one, MadjT_inf, E_one, gamma, g1, g2;
-    int i, j;
     mat_tpose(Mk,=,M,3);
     M_one = norm_one(Mk);  M_inf = norm_inf(Mk);
     do {
@@ -295,7 +294,7 @@ float polar_decomp(HMatrix M, HMatrix Q, HMatrix S)
     } while (E_one>(M_one*TOL));
     mat_tpose(Q,=,Mk,3); mat_pad(Q);
     mat_mult(Mk, M, S);	 mat_pad(S);
-    for (i=0; i<3; i++) for (j=i; j<3; j++)
+	for (int i = 0; i < 3; i++) for (int j = i; j < 3; j++)
 	S[i][j] = S[j][i] = 0.5*(S[i][j]+S[j][i]);
     return (det);
 }
@@ -325,48 +324,68 @@ float polar_decomp(HMatrix M, HMatrix Q, HMatrix S)
  */
 HVect spect_decomp(HMatrix S, HMatrix U)
 {
-    HVect kv;
-    double Diag[3],OffD[3]; /* OffD is off-diag (by omitted index) */
-    double g,h,fabsh,fabsOffDi,t,theta,c,s,tau,ta,OffDq,a,b;
-    static char nxt[] = {Y,Z,X};
-    int sweep, i, j;
-    mat_copy(U,=,mat_id,4);
-    Diag[X] = S[X][X]; Diag[Y] = S[Y][Y]; Diag[Z] = S[Z][Z];
-    OffD[X] = S[Y][Z]; OffD[Y] = S[Z][X]; OffD[Z] = S[X][Y];
-    for (sweep=20; sweep>0; sweep--) {
-	float sm = fabs(OffD[X])+fabs(OffD[Y])+fabs(OffD[Z]);
-	if (sm==0.0) break;
-	for (i=Z; i>=X; i--) {
-	    int p = nxt[i]; int q = nxt[p];
-	    fabsOffDi = fabs(OffD[i]);
-	    g = 100.0*fabsOffDi;
-	    if (fabsOffDi>0.0) {
-		h = Diag[q] - Diag[p];
-		fabsh = fabs(h);
-		if (fabsh+g==fabsh) {
-		    t = OffD[i]/h;
-		} else {
-		    theta = 0.5*h/OffD[i];
-		    t = 1.0/(fabs(theta)+sqrt(theta*theta+1.0));
-		    if (theta<0.0) t = -t;
+	HVect kv;
+	double Diag[3], OffD[3]; /* OffD is off-diag (by omitted index) */
+	double g, h, fabsh, fabsOffDi, t, theta, c, s, tau, ta, OffDq, a, b;
+	static char nxt[] = {Y, Z, X};
+	mat_copy(U, =, mat_id, 4);
+	Diag[X] = S[X][X];
+	Diag[Y] = S[Y][Y];
+	Diag[Z] = S[Z][Z];
+	OffD[X] = S[Y][Z];
+	OffD[Y] = S[Z][X];
+	OffD[Z] = S[X][Y];
+	for (int sweep = 20; sweep > 0; --sweep)
+	{
+		float sm = fabs(OffD[X]) + fabs(OffD[Y]) + fabs(OffD[Z]);
+		if (sm == 0.0)
+			break;
+		for (int i = Z; i >= X; --i)
+		{
+			int p = nxt[i];
+			int q = nxt[p];
+			fabsOffDi = fabs(OffD[i]);
+			g = 100.0 * fabsOffDi;
+			if (fabsOffDi > 0.0)
+			{
+				h = Diag[q] - Diag[p];
+				fabsh = fabs(h);
+				if (fabsh + g == fabsh)
+				{
+					t = OffD[i] / h;
+				}
+				else
+				{
+					theta = 0.5 * h / OffD[i];
+					t = 1.0 / (fabs(theta) + sqrt(theta * theta + 1.0));
+					if (theta < 0.0)
+						t = -t;
+				}
+				c = 1.0 / sqrt(t * t + 1.0);
+				s = t * c;
+				tau = s / (c + 1.0);
+				ta = t * OffD[i];
+				OffD[i] = 0.0;
+				Diag[p] -= ta;
+				Diag[q] += ta;
+				OffDq = OffD[q];
+				OffD[q] -= s * (OffD[p] + tau * OffD[q]);
+				OffD[p] += s * (OffDq - tau * OffD[p]);
+				for (int j = Z; j >= X; --j)
+				{
+					a = U[j][p];
+					b = U[j][q];
+					U[j][p] -= s * (b + tau * a);
+					U[j][q] += s * (a - tau * b);
+				}
+			}
 		}
-		c = 1.0/sqrt(t*t+1.0); s = t*c;
-		tau = s/(c+1.0);
-		ta = t*OffD[i]; OffD[i] = 0.0;
-		Diag[p] -= ta; Diag[q] += ta;
-		OffDq = OffD[q];
-		OffD[q] -= s*(OffD[p] + tau*OffD[q]);
-		OffD[p] += s*(OffDq   - tau*OffD[p]);
-		for (j=Z; j>=X; j--) {
-		    a = U[j][p]; b = U[j][q];
-		    U[j][p] -= s*(b + tau*a);
-		    U[j][q] += s*(a - tau*b);
-		}
-	    }
 	}
-    }
-    kv.x = Diag[X]; kv.y = Diag[Y]; kv.z = Diag[Z]; kv.w = 1.0;
-    return (kv);
+	kv.x = Diag[X];
+	kv.y = Diag[Y];
+	kv.z = Diag[Z];
+	kv.w = 1.0;
+	return kv;
 }
 
 /******* Spectral Axis Adjustment *******/
@@ -387,7 +406,7 @@ Quat snuggle(Quat q, HVect *k)
 		    else   {a[3]=a[2]; a[2]=a[1]; a[1]=a[0]; a[0]=a[3];}
     Quat p;
     float ka[4];
-    int i, turn = -1;
+    int turn = -1;
     ka[X] = k->x; ka[Y] = k->y; ka[Z] = k->z;
     if (ka[X]==ka[Y]) {if (ka[X]==ka[Z]) turn = W; else turn = Z;}
     else {if (ka[X]==ka[Z]) turn = Y; else if (ka[Y]==ka[Z]) turn = X;}
@@ -395,8 +414,8 @@ Quat snuggle(Quat q, HVect *k)
 	Quat qtoz, qp;
 	unsigned neg[3], win;
 	double mag[3], t;
-	static Quat qxtoz = {0,SQRTHALF,0,SQRTHALF};
-	static Quat qytoz = {SQRTHALF,0,0,SQRTHALF};
+	static Quat qxtoz = {.0f, static_cast<float>(SQRTHALF), .0f, static_cast<float>(SQRTHALF)};
+	static Quat qytoz = {static_cast<float>(SQRTHALF), .0f, .0f, static_cast<float>(SQRTHALF)};
 	static Quat qppmm = { 0.5, 0.5,-0.5,-0.5};
 	static Quat qpppp = { 0.5, 0.5, 0.5, 0.5};
 	static Quat qmpmm = {-0.5, 0.5,-0.5,-0.5};
@@ -413,7 +432,7 @@ Quat snuggle(Quat q, HVect *k)
 	mag[0] = (double)q.z*q.z+(double)q.w*q.w-0.5;
 	mag[1] = (double)q.x*q.z-(double)q.y*q.w;
 	mag[2] = (double)q.y*q.z+(double)q.x*q.w;
-	for (i=0; i<3; i++) if ((neg[i] = (mag[i]<0.0)) != 0) mag[i] = -mag[i];
+	for (int i = 0; i < 3; ++i) if ((neg[i] = (mag[i] < 0.0)) != 0) mag[i] = -mag[i];
 	if (mag[0]>mag[1]) {if (mag[0]>mag[2]) win = 0; else win = 2;}
 	else		   {if (mag[1]>mag[2]) win = 1; else win = 2;}
 	switch (win) {
@@ -430,7 +449,7 @@ Quat snuggle(Quat q, HVect *k)
 	unsigned lo, hi, neg[4], par = 0;
 	double all, big, two;
 	qa[0] = q.x; qa[1] = q.y; qa[2] = q.z; qa[3] = q.w;
-	for (i=0; i<4; i++) {
+	for (int i = 0; i < 4; ++i) {
 	    pa[i] = 0.0;
 	    if ((neg[i] = (qa[i]<0.0)) != 0) qa[i] = -qa[i];
 	    par ^= neg[i];

source/collada/PSAConvert.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -244,12 +244,12 @@ public:
 				// Iterate over all curves to find the earliest and latest keys
 				const FCDAnimated* anim = transform->GetAnimated();
 				const FCDAnimationCurveListList& curvesList = anim->GetCurves();
-				for (size_t j = 0; j < curvesList.size(); ++j)
+				for (size_t k = 0; k < curvesList.size(); ++k)
 				{
-					const FCDAnimationCurveTrackList& curves = curvesList[j];
-					for (size_t k = 0; k < curves.size(); ++k)
+					const FCDAnimationCurveTrackList& curves = curvesList[k];
+					for (size_t l = 0; l < curves.size(); ++l)
 					{
-						const FCDAnimationCurve* curve = curves[k];
+						const FCDAnimationCurve* curve = curves[l];
 						timeStart = std::min(timeStart, curve->GetKeys()[0]->input);
 						timeEnd = std::max(timeEnd, curve->GetKeys()[curve->GetKeyCount()-1]->input);
 					}

source/graphics/MaterialManager.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -78,16 +78,12 @@ CMaterial CMaterialManager::LoadMaterial(const VfsPath& pathname)
 	#undef AT
 	#undef EL
 
-	CMaterial material;
-
-	XMBElement root = xeroFile.GetRoot();
-
 	CPreprocessorWrapper preprocessor;
 	preprocessor.AddDefine("CFG_FORCE_ALPHATEST", g_Renderer.m_Options.m_ForceAlphaTest ? "1" : "0");
+	CMaterial material;
+	material.AddStaticUniform("qualityLevel", CVector4D(qualityLevel, 0, 0, 0));
 
-	CVector4D vec(qualityLevel,0,0,0);
-	material.AddStaticUniform("qualityLevel", vec);
-
+	XMBElement root = xeroFile.GetRoot();
 	XERO_ITER_EL(root, node)
 	{
 		int token = node.GetNodeName();

source/graphics/ObjectBase.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -368,20 +368,17 @@ std::vector<u8> CObjectBase::CalculateVariationKey(const std::vector<std::set<CS
 		}
 
 		choices.push_back(match);
-
 		// Remember which props were chosen, so we can call CalculateVariationKey on them
 		// at the end.
-		Variant& var ((*grp)[match]);
-		for (std::vector<Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
-		{
-			// Erase all existing props which are overridden by this variant:
-			for (std::vector<Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
-				chosenProps.erase(it->m_PropPointName);
-			// and then insert the new ones:
-			for (std::vector<Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
-				if (! it->m_ModelName.empty())
-					chosenProps.insert(make_pair(it->m_PropPointName, it->m_ModelName));
-		}
+		// Erase all existing props which are overridden by this variant:
+		Variant& var((*grp)[match]);
+
+		for (const Prop& prop : var.m_Props)
+			chosenProps.erase(prop.m_PropPointName);
+		// and then insert the new ones:
+		for (const Prop& prop : var.m_Props)
+			if (!prop.m_ModelName.empty())
+				chosenProps.insert(make_pair(prop.m_PropPointName, prop.m_ModelName));
 	}
 
 	// Load each prop, and add their CalculateVariationKey to our key:
@@ -586,16 +583,13 @@ std::set<CStr> CObjectBase::CalculateRandomRemainingSelections(rng_t& rng, const
 		// Remember which props were chosen, so we can call CalculateRandomVariation on them
 		// at the end.
 		Variant& var ((*grp)[match]);
-		for (std::vector<Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
-		{
-			// Erase all existing props which are overridden by this variant:
-			for (std::vector<Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
-				chosenProps.erase(it->m_PropPointName);
-			// and then insert the new ones:
-			for (std::vector<Prop>::iterator it = var.m_Props.begin(); it != var.m_Props.end(); ++it)
-				if (! it->m_ModelName.empty())
-					chosenProps.insert(make_pair(it->m_PropPointName, it->m_ModelName));
-		}
+		// Erase all existing props which are overridden by this variant:
+		for (const Prop& prop : var.m_Props)
+			chosenProps.erase(prop.m_PropPointName);
+		// and then insert the new ones:
+		for (const Prop& prop : var.m_Props)
+			if (!prop.m_ModelName.empty())
+				chosenProps.insert(make_pair(prop.m_PropPointName, prop.m_ModelName));
 	}
 
 	// Load each prop, and add their required selections to ours:

source/graphics/TerrainTextureEntry.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -91,15 +91,15 @@ CTerrainTextureEntry::CTerrainTextureEntry(CTerrainPropertiesPtr properties, con
 				ENSURE(textures_element.GetNodeName() == el_texture);
 
 				CStr name;
-				VfsPath path;
-				XERO_ITER_ATTR(textures_element, se)
+				VfsPath terrainTexturePath;
+				XERO_ITER_ATTR(textures_element, relativePath)
 				{
-					if (se.Name == at_file)
-						path = VfsPath("art/textures/terrain") / se.Value.FromUTF8();
-					else if (se.Name == at_name)
-						name = se.Value;
+					if (relativePath.Name == at_file)
+						terrainTexturePath = VfsPath("art/textures/terrain") / relativePath.Value.FromUTF8();
+					else if (relativePath.Name == at_name)
+						name = relativePath.Value;
 				}
-				samplers.emplace_back(name, path);
+				samplers.emplace_back(name, terrainTexturePath);
 			}
 
 		}

source/graphics/TextRenderer.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Wildfire Games.
+/* Copyright (C) 2018 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -300,12 +300,12 @@ void CTextRenderer::Render()
 				vertexes[idx*4+3].x = g->x1 + x;
 				vertexes[idx*4+3].y = g->y1 + y;
 
-				indexes[idx*6+0] = idx*4+0;
-				indexes[idx*6+1] = idx*4+1;
-				indexes[idx*6+2] = idx*4+2;
-				indexes[idx*6+3] = idx*4+2;
-				indexes[idx*6+4] = idx*4+3;
-				indexes[idx*6+5] = idx*4+0;
+				indexes[idx*6+0] = static_cast<u16>(idx*4+0);
+				indexes[idx*6+1] = static_cast<u16>(idx*4+1);
+				indexes[idx*6+2] = static_cast<u16>(idx*4+2);
+				indexes[idx*6+3] = static_cast<u16>(idx*4+2);
+				indexes[idx*6+4] = static_cast<u16>(idx*4+3);
+				indexes[idx*6+5] = static_cast<u16>(idx*4+0);
 
 				x += g->xadvance;
 

commit 6d93be0f58bc810acd44bc843a941b6af1cb7d37

Fix the display of light broken by  cc67d54aeb Reviewed by: Elexis, vladislav Differential Revisions : https://code.wildfiregames.com/D1389 Refs: https://code.wildfiregames.com/D1262 https://code.wildfiregames.com/D1372
This was SVN commit r21557.

source/graphics/LightEnv.h
@@ -128,7 +128,7 @@ public:
 	SColor4ub EvaluateTerrainDiffuseFactor(const CVector3D& normal) const
 	{
 		float dot = -normal.Dot(m_SunDir);
-		u8 c = clamp(static_cast<u8>(dot * 255), static_cast<u8>(0), static_cast<u8>(255));
+		u8 c = static_cast<u8>(clamp(dot * 255.f, 0.f, 255.f));
 		return SColor4ub(c, c, c, 255);
 	}
 