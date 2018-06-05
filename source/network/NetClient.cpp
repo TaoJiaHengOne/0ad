TaoJiaHengOne/0ad

commit fa85527baf480a6b7784571672499905917dc3eb

Send messages in queue before closing connection, patch by boeseRaupe. Fixes #2420
This was SVN commit r14732.

source/network/NetClient.cpp
@@ -144,6 +144,12 @@ void CNetClient::SetAndOwnSession(CNetClientSession* session)
 
 void CNetClient::DestroyConnection()
 {
+	// Send network messages from the current frame before connection is destroyed.
+	if (m_ClientTurnManager)
+	{
+		m_ClientTurnManager->OnDestroyConnection(); // End sending of commands for scheduled turn.
+		Flush(); // Make sure the messages are sent.
+	}
 	SAFE_DELETE(m_Session);
 }
 

source/network/NetTurnManager.cpp
@@ -419,6 +419,11 @@ void CNetClientTurnManager::NotifyFinishedUpdate(u32 turn)
 	m_NetClient.SendMessage(&msg);
 }
 
+void CNetClientTurnManager::OnDestroyConnection()
+{
+	NotifyFinishedOwnCommands(m_CurrentTurn + COMMAND_DELAY);
+}
+
 void CNetClientTurnManager::OnSimulationMessage(CSimulationMessage* msg)
 {
 	// Command received from the server - store it for later execution

source/network/NetTurnManager.h
@@ -200,6 +200,11 @@ public:
 	virtual void OnSimulationMessage(CSimulationMessage* msg);
 
 	virtual void PostCommand(CScriptValRooted data);
+	
+	/**
+	 * Notifiy the server that all commands are sent to prepare the connection for termination.
+	 */
+	void OnDestroyConnection();
 
 protected:
 	virtual void NotifyFinishedOwnCommands(u32 turn);

source/ps/GameSetup/GameSetup.cpp
@@ -654,8 +654,8 @@ static void ShutdownSDL()
 
 void EndGame()
 {
-	SAFE_DELETE(g_NetServer);
 	SAFE_DELETE(g_NetClient);
+	SAFE_DELETE(g_NetServer);
 	SAFE_DELETE(g_Game);
 
 	ISoundManager::CloseGame();

commit 43730f15f3549ab38f51d667a5db416506674bf2

Fix network FSM errors when a client closes the game during the loading screen or rejoin synchronization stage, fixes #4594, refs 3199.
Fix comments claiming to ensure reliability when in reality there is
only a small likelihood of the message being received.

Broken by fa85527baf / refs #2420 which was a preparation for a3e1c68b9a
/ refs #2373, which hence didn't actually work and was circumvented by
9b136a45fc without clarification.
Might have caused #3643.
Differential Revision: https://code.wildfiregames.com/D1557
Reviewed By: Imarok
This was SVN commit r21837.

source/network/NetClient.cpp
@@ -181,11 +181,11 @@ void CNetClient::SetAndOwnSession(CNetClientSession* session)
 
 void CNetClient::DestroyConnection()
 {
-	// Send network messages from the current frame before connection is destroyed.
+	// Attempt to send network messages from the current frame before connection is destroyed.
 	if (m_ClientTurnManager)
 	{
-		m_ClientTurnManager->OnDestroyConnection(); // End sending of commands for scheduled turn.
-		Flush(); // Make sure the messages are sent.
+		m_ClientTurnManager->OnDestroyConnection();
+		Flush();
 	}
 	SAFE_DELETE(m_Session);
 }

source/network/NetClientTurnManager.cpp
@@ -95,7 +95,10 @@ void CNetClientTurnManager::NotifyFinishedUpdate(u32 turn)
 
 void CNetClientTurnManager::OnDestroyConnection()
 {
-	NotifyFinishedOwnCommands(m_CurrentTurn + COMMAND_DELAY);
+	// Attempt to flush messages before leaving.
+	// Notice the sending is not reliable and rarely makes it to the Server.
+	if (m_NetClient.GetCurrState() == NCS_INGAME)
+		NotifyFinishedOwnCommands(m_CurrentTurn + COMMAND_DELAY);
 }
 
 void CNetClientTurnManager::OnSimulationMessage(CSimulationMessage* msg)