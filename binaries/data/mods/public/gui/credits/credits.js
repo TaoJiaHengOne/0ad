TaoJiaHengOne/0ad

commit bcf2c75513f90c079e9a309afa810f856784e9c5

New in-game Credits page, fixes #3744.
This was SVN commit r17830.

binaries/data/mods/public/gui/credits/credits.js
@@ -1,0 +1,74 @@
+var g_PanelNames = ["special", "programming", "art", "translators", "misc", "donators"];
+var g_PanelTexts = [];
+var g_ActivePanel = -1;
+
+function init()
+{
+	// Load credits list from the disk and parse them
+	for (let name of g_PanelNames)
+		g_PanelTexts.push(parseJSONCredits(name));
+
+	selectPanel(0);
+}
+
+// Run through a "Content" list and parse elements for formatting and translation
+function parseHelper(list)
+{
+	let result = "";
+
+	for (let object of list)
+	{
+		if (object.Title)
+			result += "[font=\"sans-bold-stroke-14\"]" + translate(object.Title) + "\n";
+
+		if (object.Subtitle)
+			result += "[font=\"sans-bold-14\"]" + translate(object.Subtitle) + "\n";
+
+		if (object.List)
+		{
+			for (let element of object.List)
+			{
+				if (element.nick && element.name)
+					result += "[font=\"sans-14\"]" + sprintf(translate("%(nick)s - %(name)s"), { "nick": element.nick, "name": element.name }) + "\n";
+				else if (element.nick)
+					result += "[font=\"sans-14\"]" + element.nick + "\n";
+				else if (element.name)
+					result += "[font=\"sans-14\"]" + element.name + "\n";
+			}
+			result += "\n";
+		}
+
+		if (object.Content)
+			result += "\n" + parseHelper(object.Content) + "\n";
+	}
+
+	return result;
+}
+
+function parseJSONCredits(panelName)
+{
+	let json = Engine.ReadJSONFile("gui/credits/texts/" + panelName + ".json");
+	if (!json || !json.Content)
+	{
+		error("Could not load credits for " + panelName + "!");
+		return "";
+	}
+
+	return parseHelper(json.Content);
+}
+
+function selectPanel(i)
+{
+	if (g_ActivePanel != -1)
+	{
+		let oldPanelButton = Engine.GetGUIObjectByName(g_PanelNames[g_ActivePanel] + "PanelButton");
+		oldPanelButton.sprite = "BackgroundBox";
+	}
+
+	g_ActivePanel = i;
+	let newPanelButton = Engine.GetGUIObjectByName(g_PanelNames[g_ActivePanel] + "PanelButton");
+	newPanelButton.sprite = "ForegroundBox";
+
+	let creditsText = Engine.GetGUIObjectByName("creditsText");
+	creditsText.caption = g_PanelTexts[i];
+}

binaries/data/mods/public/gui/credits/credits.xml
@@ -1,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<!--
+==========================================
+- CREDITS PAGE -
+==========================================
+-->
+
+<objects>
+	<script file="gui/common/functions_global_object.js"/>
+	<script file="gui/credits/credits.js"/>
+
+	<!-- Add a translucent black background to fade out the menu page -->
+	<object type="image" z="0" sprite="ModernFade"/>
+
+	<object type="image" style="ModernDialog" size="50%-473 24 50%+473 100%-24">
+		<object style="ModernLabelText" type="text" size="50%-128 -18 50%+128 14">
+			<translatableAttribute id="caption">0 A.D. Credits</translatableAttribute>
+		</object>
+
+		<object name="specialPanelButton" type="button" sprite="BackgroundBox" size="20 50 150 80">
+			<action on="Press">selectPanel(0);</action>
+			<object type="text" style="ModernLabelText" ghost="true">
+				<translatableAttribute id="caption">Special Credits</translatableAttribute>
+			</object>
+		</object>
+
+		<object name="programmingPanelButton" type="button" sprite="BackgroundBox" size="155 50 305 80">
+			<action on="Press">selectPanel(1);</action>
+			<object type="text" style="ModernLabelText" ghost="true">
+				<translatableAttribute id="caption">Programming</translatableAttribute>
+			</object>
+		</object>
+
+		<object name="artPanelButton" type="button" sprite="BackgroundBox" size="310 50 460 80">
+			<action on="Press">selectPanel(2);</action>
+			<object type="text" style="ModernLabelText" ghost="true">
+				<translatableAttribute id="caption">Art</translatableAttribute>
+			</object>
+		</object>
+
+		<object name="translatorsPanelButton" type="button" sprite="BackgroundBox" size="465 50 615 80">
+			<action on="Press">selectPanel(3);</action>
+			<object type="text" style="ModernLabelText" ghost="true">
+				<translatableAttribute id="caption">Translators</translatableAttribute>
+			</object>
+		</object>
+
+		<object name="miscPanelButton" type="button" sprite="BackgroundBox" size="620 50 770 80">
+			<action on="Press">selectPanel(4);</action>
+			<object type="text" style="ModernLabelText" ghost="true">
+				<translatableAttribute id="caption">Miscellaneous</translatableAttribute>
+			</object>
+		</object>
+
+		<object name="donatorsPanelButton" type="button" sprite="BackgroundBox" size="775 50 925 80">
+			<action on="Press">selectPanel(5);</action>
+			<object type="text" style="ModernLabelText" ghost="true">
+				<translatableAttribute id="caption">Donators</translatableAttribute>
+			</object>
+		</object>
+
+		<object type="image" sprite="ModernFade" size="20 85 100%-20 100%-54">
+			<object name="creditsText" type="text" style="textPanel"/>
+		</object>
+
+		<!-- Close dialog -->
+		<object type="button" style="ModernButtonRed" size="100%-200 100%-45 100%-17 100%-17" hotkey="cancel">
+			<translatableAttribute id="caption">Close</translatableAttribute>
+			<action on="Press">
+				Engine.SwitchGuiPage("page_pregame.xml");
+			</action>
+		</object>
+	</object>
+</objects>

binaries/data/mods/public/gui/credits/sprites.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<sprites>
+	<sprite name="BackgroundBox">
+		<image backcolor="50 35 0 120" size="0 0 100% 100%"/>
+		<image backcolor="gold" size="0 0 1 100%"/>
+		<image backcolor="gold" size="1 0 100%-1 1"/>
+		<image backcolor="gold" size="100%-1 0 100% 100%"/>
+		<image backcolor="gold" size="0 100%-1 100% 100%"/>
+	</sprite>
+
+	<sprite name="ForegroundBox">
+		<image backcolor="255 255 255 40" size="0 0 100% 100%"/>
+		<image backcolor="gold" size="0 0 2 100%"/>
+		<image backcolor="gold" size="2 100%-2 100%-2 100%"/>
+		<image backcolor="gold" size="100%-2 0 100% 100%"/>
+		<image backcolor="gold" size="0 0 100% 100%"/>
+	</sprite>
+</sprites>

binaries/data/mods/public/gui/credits/styles.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<styles>
+
+	<style name="textPanel"
+		buffer_zone="8"
+		scrollbar="true"
+		scrollbar_style="ModernScrollBar"
+		font="sans-bold-stroke-14"
+		textcolor="white"
+		text_align="center"
+		text_valign="center"
+		sprite="ModernDarkBoxGold"
+	/>
+
+</styles>

binaries/data/mods/public/gui/page_credits.xml
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<page>
+    <include>common/modern/setup.xml</include>
+    <include>common/modern/styles.xml</include>
+    <include>common/modern/sprites.xml</include>
+
+    <include>common/global.xml</include>
+
+    <include>credits/styles.xml</include>
+    <include>credits/sprites.xml</include>
+    <include>credits/credits.xml</include>
+</page>

binaries/data/mods/public/gui/pregame/mainmenu.xml
@@ -535,28 +535,14 @@
 						<translate>Alpha XIX: Syllepsis</translate>
 						<keep>[/font]\n\n</keep>
 						<translate>WARNING: This is an early development version of the game. Many features have not been added yet.</translate>
-						<keep>\n\n</keep>
-						<translate>Get involved at: play0ad.com</translate>
 					</attribute>
 				</object>
 
-				<!-- CONTACT METHODS -->
+				<!-- GET INVOLVED -->
 				<object type="button"
 					style="StoneButton"
 					tooltip_style="pgToolTip"
-					size="8 100%-108 100%-8 100%-80"
-				>
-					<translatableAttribute id="caption">Help with the Translation!</translatableAttribute>
-					<translatableAttribute id="tooltip">Click to open the 0 A.D. translate page in your browser.</translatableAttribute>
-					<action on="Press"><![CDATA[
-						openURL("http://trac.wildfiregames.com/wiki/Localization");
-					]]></action>
-				</object>
-
-				<object type="button"
-					style="StoneButton"
-					tooltip_style="pgToolTip"
-					size="8 100%-72 50%-4 100%-44"
+					size="8 100%-144 50%-4 100%-116"
 				>
 					<translatableAttribute id="caption">Website</translatableAttribute>
 					<translatableAttribute id="tooltip">Click to open play0ad.com in your web browser.</translatableAttribute>
@@ -568,7 +554,7 @@
 				<object type="button"
 					style="StoneButton"
 					tooltip_style="pgToolTip"
-					size="50%+4 100%-72 100%-8 100%-44"
+					size="50%+4 100%-144 100%-8 100%-116"
 				>
 					<translatableAttribute id="caption">Chat</translatableAttribute>
 					<translatableAttribute id="tooltip">Click to open the 0 A.D. IRC chat in your browser. (#0ad on webchat.quakenet.org)</translatableAttribute>
@@ -580,13 +566,37 @@
 				<object type="button"
 					style="StoneButton"
 					tooltip_style="pgToolTip"
-					size="8 100%-36 100%-8 100%-8"
+					size="8 100%-108 100%-8 100%-80"
 				>
 					<translatableAttribute id="caption">Report a Bug</translatableAttribute>
 					<translatableAttribute id="tooltip">Click to visit 0 A.D. Trac to report a bug, crash, or error.</translatableAttribute>
 					<action on="Press"><![CDATA[
 						openURL("http://trac.wildfiregames.com/wiki/ReportingErrors/");
 					]]></action>
+				</object>
+
+				<object type="button"
+					style="StoneButton"
+					tooltip_style="pgToolTip"
+					size="8 100%-72 100%-8 100%-44"
+				>
+					<translatableAttribute id="caption">Translate the game</translatableAttribute>
+					<translatableAttribute id="tooltip">Click to open the 0 A.D. translate page in your browser.</translatableAttribute>
+					<action on="Press"><![CDATA[
+						openURL("http://trac.wildfiregames.com/wiki/Localization");
+					]]></action>
+				</object>
+
+				<object type="button"
+					style="StoneButton"
+					tooltip_style="pgToolTip"
+					size="8 100%-36 100%-8 100%-8"
+				>
+					<translatableAttribute id="caption">Credits</translatableAttribute>
+					<translatableAttribute id="tooltip">Click to see the 0 A.D. credits.</translatableAttribute>
+					<action on="Press">
+						Engine.PushGuiPage("page_credits.xml", {});
+					</action>
 				</object>
 
 			</object>

source/tools/i18n/creditTranslators.py
@@ -1,0 +1,127 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2016 Wildfire Games.
+# This file is part of 0 A.D.
+#
+# 0 A.D. is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# 0 A.D. is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This file imports the translators credits located in the public mod GUI files and
+runs through .po files to add possible new translators to it.
+It only appends new people, so it is possible to manually add names in the credits
+file and they won't be overwritten by running this script.
+
+Translatable strings will be extracted from the generated file, so this should be ran
+before updateTemplates.py.
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import json, os, glob, re
+
+# Credited languages - Keep in sync with source/tools/dist/remove-incomplete-translations.sh
+langs = {
+    'cs': 'Czech',
+    'de': 'German',
+    'en_GB': 'English (UK)',
+    'es': 'Spanish',
+    'fr': 'French',
+    'gd': 'Gaelic',
+    'gl': 'Galician',
+    'id': 'Indonesian',
+    'it': 'Italian',
+    'nl': 'Dutch',
+    'pl': 'Polish',
+    'pt_PT': 'Portuguese (Portugal)',
+    'pt_BR': 'Portuguese (Brazil)',
+    'ru': 'Russian',
+    'sk': 'Slovak',
+    'tr': 'Turkish'}
+
+root = '../../../'
+
+poLocations = [
+    'binaries/data/l10n/',
+    'binaries/data/mods/public/l10n/',
+    'binaries/data/mods/mod/l10n/']
+
+creditsLocation = 'binaries/data/mods/public/gui/credits/texts/translators.json'
+
+# Load JSON data
+creditsFile = open(root + creditsLocation)
+JSONData = json.load(creditsFile)
+creditsFile.close()
+
+# This dictionnary will hold creditors lists for each language, indexed by code
+langsLists = {}
+
+# Create the new JSON data
+newJSONData = {'Content': []}
+
+# First get the already existing lists. If they correspond with some of the credited languages,
+# add them to the new data after processing, else add them immediately.
+# NB: All of this is quite inefficient
+for element in JSONData['Content']:
+    if 'Title' not in element or element['Title'] not in langs.values():
+        newJSONData['Content'].append(element)
+        continue
+
+    for (langCode, langName) in langs.items():
+        if element['Title'] == langName:
+            langsLists[langCode] = element['List']
+            break
+
+# Now actually go through the list of languages and search the .po files for people
+
+# Prepare some regexes
+commentMatch = re.compile('#.*')
+translatorMatch = re.compile('# ([\w\s]*)(?: <.*>)?, [0-9-]', re.UNICODE)
+
+# Search
+for lang in langs.keys():
+    if lang not in langsLists.keys():
+        langsLists[lang] = []
+
+    for location in poLocations:
+        files = glob.glob(root + location + lang + '.*.po')
+        for file in files:
+            poFile = open(file.replace('\\', '/'))
+            reached = False
+            for line in poFile:
+                line = line.decode('utf8')
+                if reached:
+                    if not commentMatch.match(line):
+                        break
+                    m = translatorMatch.match(line)
+                    if m:
+                        langsLists[lang].append(m.group(1))
+                if line.strip() == '# Translators:':
+                    reached = True
+            poFile.close()
+
+    # Sort and remove duplicates
+    # Sorting should ignore case to have a neat credits list
+    langsLists[lang] = sorted(set(langsLists[lang]), cmp=lambda x,y: cmp(x.lower(), y.lower()))
+
+# Now insert the new data into the new JSON file
+for (langCode, langList) in sorted(langsLists.items()):
+    newJSONData['Content'].append({'Title': langs[langCode], 'List': []})
+    for name in langList:
+        newJSONData['Content'][-1]['List'].append({'name': name})
+
+# Save the JSON data to the credits file
+creditsFile = open(root + creditsLocation, 'w')
+json.dump(newJSONData, creditsFile, indent=4)
+creditsFile.close()

commit 3e1361e5a53e389cbb8ff1e116234ee13179a94b

Show translations of the labels of the tab-buttons in the credits gui page.
Mistake introduced in bcf2c75513, made apparent in 19a218cd48, and not
caught in 4223fad060.

Reported by: thekolian1996 (forums)
Reviewed By: elexis
Differential Revision: https://code.wildfiregames.com/D1695
This was SVN commit r22045.

binaries/data/mods/public/gui/credits/credits.js
@@ -29,6 +29,7 @@ function init()
 			error("Could not load credits for " + category + "!");
 			continue;
 		}
+		translateObjectKeys(json, ["Title", "Subtitle"]);
 		g_PanelData.push({
 			"label": json.Title || category,
 			"content": parseHelper(json.Content)
@@ -56,10 +57,10 @@ function parseHelper(list)
 			result += "[font=\"sans-bold-stroke-14\"]" + object.LangName + "\n";
 
 		if (object.Title)
-			result += "[font=\"sans-bold-stroke-14\"]" + translate(object.Title) + "\n";
+			result += "[font=\"sans-bold-stroke-14\"]" + object.Title + "\n";
 
 		if (object.Subtitle)
-			result += "[font=\"sans-bold-14\"]" + translate(object.Subtitle) + "\n";
+			result += "[font=\"sans-bold-14\"]" + object.Subtitle + "\n";
 
 		if (object.List)
 		{