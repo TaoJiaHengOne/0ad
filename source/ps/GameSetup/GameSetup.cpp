TaoJiaHengOne/0ad

commit 6d54ab4c1fced537c42776349cc79599ec68b085

Implement combine victory conditions
Excluding the changes to scenario and skirmisch maps
Transform gameType string to victoryCondition array in load/replaymenu
Adapt the gamesetup to use checkboxes for every victory condition and an
array for storing them
Allow multiple queries in conquestCommon
Remove conquest from regicide, wonder and capture the relic
Move the endless gamedescription from settings to gamedescription
Fixing wrong tabulation from a8a29271ce

This commit will break all scenario and skirmisch maps, their "Gametype"
string needs to be transformed in a "VictoryCondition" array as is done
in the tutorial map (counting endless as an empty array). This counts
for mods too!
Old svn replays and savegame will throw warnings/errors as they are
incompatible after this commit. So svn users will need to delete all
those.

Comments on ai and autostart games By: mimo
Comments on Atlas By: Vladislav
Reviewed By: elexis
Differential Revision: https://code.wildfiregames.com/D1240
fixes: #4014

This was SVN commit r21474.

source/ps/GameSetup/GameSetup.cpp
@@ -1464,24 +1464,37 @@ bool Autostart(const CmdLineArgs& args)
 		triggerScriptsVector.push_back(nonVisualScript.FromUTF8());
 	}
 
-	CStr victory = "conquest";
+	std::vector<CStr> victoryConditions(1, "conquest");
 	if (args.Has("autostart-victory"))
-		victory = args.Get("autostart-victory");
+		victoryConditions = args.GetMultiple("autostart-victory");
 
-	scriptInterface.SetProperty(settings, "GameType", std::string(victory));
+	if (victoryConditions.size() == 1 && victoryConditions[0] == "endless")
+		victoryConditions.clear();
 
-	CStrW scriptPath = L"simulation/data/settings/victory_conditions/" + victory.FromUTF8() + L".json";
-	JS::RootedValue scriptData(cx);
-	JS::RootedValue data(cx);
-	JS::RootedValue victoryScripts(cx);
-	scriptInterface.ReadJSONFile(scriptPath, &scriptData);
-	if (!scriptData.isUndefined() && scriptInterface.GetProperty(scriptData, "Data", &data) && !data.isUndefined()
-		&& scriptInterface.GetProperty(data, "Scripts", &victoryScripts) && !victoryScripts.isUndefined())
+	scriptInterface.SetProperty(settings, "VictoryConditions", victoryConditions);
+
+	for (const CStr& victory : victoryConditions)
 	{
-		std::vector<CStrW> victoryScriptsVector;
-		FromJSVal_vector(cx, victoryScripts, victoryScriptsVector);
-		triggerScriptsVector.insert(triggerScriptsVector.end(), victoryScriptsVector.begin(), victoryScriptsVector.end());
+		JS::RootedValue scriptData(cx);
+		JS::RootedValue data(cx);
+		JS::RootedValue victoryScripts(cx);
+
+		CStrW scriptPath = L"simulation/data/settings/victory_conditions/" + victory.FromUTF8() + L".json";
+		scriptInterface.ReadJSONFile(scriptPath, &scriptData);
+		if (!scriptData.isUndefined() && scriptInterface.GetProperty(scriptData, "Data", &data) && !data.isUndefined()
+			&& scriptInterface.GetProperty(data, "Scripts", &victoryScripts) && !victoryScripts.isUndefined())
+		{
+			std::vector<CStrW> victoryScriptsVector;
+			FromJSVal_vector(cx, victoryScripts, victoryScriptsVector);
+			triggerScriptsVector.insert(triggerScriptsVector.end(), victoryScriptsVector.begin(), victoryScriptsVector.end());
+		}
+		else
+		{
+			LOGERROR("Autostart: Error reading victory script '%s'", utf8_from_wstring(scriptPath));
+			throw PSERROR_Game_World_MapLoadFailed("Error reading victory script.\nCheck application log for details.");
+		}
 	}
+
 	ToJSVal_vector(cx, &triggerScripts, triggerScriptsVector);
 	scriptInterface.SetProperty(settings, "TriggerScripts", triggerScripts);
 

binaries/data/mods/public/gui/common/gamedescription.js
@@ -216,12 +216,23 @@ function formatPlayerInfo(playerDataArray, playerStates)
 function getGameDescription(extended = false)
 {
 	let titles = [];
+	if (!g_GameAttributes.settings.VictoryConditions.length)
+		titles.push({
+			"label": translateWithContext("victory condition", "Endless Game"),
+			"value": translate("No winner will be determined, even if everyone is defeated.")
+		});
 
-	let victoryIdx = g_VictoryConditions.Name.indexOf(g_GameAttributes.settings.GameType || g_VictoryConditions.Default);
-	if (victoryIdx != -1)
+	let victoryConditions = g_GameAttributes.settings.VictoryConditions.map(victoryConditionName =>
+		g_VictoryConditions.find(victoryCondition => victoryCondition.Name == victoryConditionName) || {
+			"Name": victoryConditionName,
+			"Description": ""
+		});
+
+	for (let victoryCondition of victoryConditions.sort((a, b) =>
+		a.GUIOrder - b.GUIOrder || (a.Title > b.Title ? 1 : a.Title > b.Title ? -1 : 0)))
 	{
-		let title = g_VictoryConditions.Title[victoryIdx];
-		if (g_VictoryConditions.Name[victoryIdx] == "wonder")
+		let title = translateVictoryCondition(victoryCondition.Name);
+		if (victoryCondition.Name == "wonder")
 			title = sprintf(
 				translatePluralWithContext(
 					"victory condition",
@@ -232,7 +243,7 @@ function getGameDescription(extended = false)
 				{ "min": g_GameAttributes.settings.WonderDuration }
 			);
 
-		let isCaptureTheRelic = g_VictoryConditions.Name[victoryIdx] == "capture_the_relic";
+		let isCaptureTheRelic = victoryCondition.Name == "capture_the_relic";
 		if (isCaptureTheRelic)
 			title = sprintf(
 				translatePluralWithContext(
@@ -246,7 +257,7 @@ function getGameDescription(extended = false)
 
 		titles.push({
 			"label": title,
-			"value": g_VictoryConditions.Description[victoryIdx]
+			"value": victoryCondition.Description
 		});
 
 		if (isCaptureTheRelic)
@@ -255,7 +266,7 @@ function getGameDescription(extended = false)
 				"value": g_GameAttributes.settings.RelicCount
 			});
 
-		if (g_VictoryConditions.Name[victoryIdx] == "regicide")
+		if (victoryCondition.Name == "regicide")
 			if (g_GameAttributes.settings.RegicideGarrison)
 				titles.push({
 					"label": translate("Hero Garrison"),
@@ -264,7 +275,7 @@ function getGameDescription(extended = false)
 			else
 				titles.push({
 					"label": translate("Exposed Heroes"),
-					"value": translate("Heroes cannot be garrisoned, and they are vulnerable to raids.")
+					"value": translate("Heroes cannot be garrisoned and they are vulnerable to raids.")
 				});
 	}
 

binaries/data/mods/public/gui/common/settings.js
@@ -247,7 +247,7 @@ function loadBiomes()
 }
 
 /**
- * Loads available gametypes.
+ * Loads available victoryCondtions from json files.
  *
  * @returns {Array|undefined}
  */
@@ -256,23 +256,14 @@ function loadVictoryConditions()
 	let subdir = "victory_conditions/";
 
 	let victoryConditions = listFiles(g_SettingsDirectory + subdir, ".json", false).map(victoryScriptName => {
-		let vc = loadSettingValuesFile(subdir + victoryScriptName + ".json");
-		if (vc)
-			vc.Name = victoryScriptName;
-		return vc;
+		let victoryCondition = loadSettingValuesFile(subdir + victoryScriptName + ".json");
+		if (victoryCondition)
+			victoryCondition.Name = victoryScriptName;
+		return victoryCondition;
 	});
 
-	if (victoryConditions.some(vc => vc == undefined))
+	if (victoryConditions.some(victoryCondition => victoryCondition == undefined))
 		return undefined;
-
-	// TODO: We might support enabling victory conditions separately sometime.
-	// Until then, we supplement the endless gametype here.
-	victoryConditions.push({
-		"Name": "endless",
-		"Title": translateWithContext("victory condition", "None"),
-		"Description": translate("Endless game."),
-		"Scripts": []
-	});
 
 	return victoryConditions;
 }
@@ -428,11 +419,11 @@ function translatePopulationCapacity(population)
 /**
  * Returns title or placeholder.
  *
- * @param {string} gameType - for example "conquest"
+ * @param {string} victoryConditionName - For example "conquest".
  * @returns {string}
  */
-function translateVictoryCondition(gameType)
+function translateVictoryCondition(victoryConditionName)
 {
-	let victoryCondition = g_Settings.VictoryConditions.find(vc => vc.Name == gameType);
-	return victoryCondition ? victoryCondition.Title : translateWithContext("victory condition", "Unknown");
+	let victoryCondition = g_Settings.VictoryConditions.find(victoryCondition => victoryCondition.Name == victoryConditionName);
+	return victoryCondition ? victoryCondition.Title : translate("Unknown Victory Condition");
 }

binaries/data/mods/public/gui/gamesetup/gamesetup.js
@@ -7,8 +7,9 @@ const g_MapTypes = prepareForDropdown(g_Settings && g_Settings.MapTypes);
 const g_TriggerDifficulties = prepareForDropdown(g_Settings && g_Settings.TriggerDifficulties);
 const g_PopulationCapacities = prepareForDropdown(g_Settings && g_Settings.PopulationCapacities);
 const g_StartingResources = prepareForDropdown(g_Settings && g_Settings.StartingResources);
-const g_VictoryConditions = prepareForDropdown(g_Settings && g_Settings.VictoryConditions);
 const g_VictoryDurations = prepareForDropdown(g_Settings && g_Settings.VictoryDurations);
+const g_VictoryConditions = g_Settings && g_Settings.VictoryConditions;
+
 var g_GameSpeeds = getGameSpeedChoices(false);
 
 /**
@@ -433,7 +434,7 @@ var g_SettingsTabsGUI = [
 	{
 		"label": translateWithContext("Match settings tab name", "Game Type"),
 		"settings": [
-			"victoryCondition",
+			...g_VictoryConditions.map(victoryCondition => victoryCondition.Name),
 			"relicCount",
 			"relicDuration",
 			"wonderDuration",
@@ -640,22 +641,6 @@ var g_Dropdowns = {
 		"enabled": () => g_GameAttributes.mapType != "scenario",
 		"initOrder": 1000
 	},
-	"victoryCondition": {
-		"title": () => translate("Victory Condition"),
-		"tooltip": (hoverIdx) => g_VictoryConditions.Description[hoverIdx] || translate("Select victory condition."),
-		"labels": () => g_VictoryConditions.Title,
-		"ids": () => g_VictoryConditions.Name,
-		"default": () => g_VictoryConditions.Default,
-		"defined": () => g_GameAttributes.settings.GameType !== undefined,
-		"get": () => g_GameAttributes.settings.GameType,
-		"select": (itemIdx) => {
-			g_GameAttributes.settings.GameType = g_VictoryConditions.Name[itemIdx];
-			g_GameAttributes.settings.VictoryScripts = g_VictoryConditions.Scripts[itemIdx];
-		},
-		"enabled": () => g_GameAttributes.mapType != "scenario",
-		"autocomplete": 0,
-		"initOrder": 1000
-	},
 	"relicCount": {
 		"title": () => translate("Relic Count"),
 		"tooltip": (hoverIdx) => translate("Total number of relics spawned on the map. Relic victory is most realistic with only one or two relics. With greater numbers, the relics are important to capture to receive aura bonuses."),
@@ -667,7 +652,7 @@ var g_Dropdowns = {
 		"select": (itemIdx) => {
 			g_GameAttributes.settings.RelicCount = g_RelicCountList[itemIdx];
 		},
-		"hidden": () => g_GameAttributes.settings.GameType != "capture_the_relic",
+		"hidden": () => g_GameAttributes.settings.VictoryConditions.indexOf("capture_the_relic") == -1,
 		"enabled": () => g_GameAttributes.mapType != "scenario",
 		"initOrder": 1000
 	},
@@ -682,7 +667,7 @@ var g_Dropdowns = {
 		"select": (itemIdx) => {
 			g_GameAttributes.settings.RelicDuration = g_VictoryDurations.Duration[itemIdx];
 		},
-		"hidden": () => g_GameAttributes.settings.GameType != "capture_the_relic",
+		"hidden": () => g_GameAttributes.settings.VictoryConditions.indexOf("capture_the_relic") == -1,
 		"enabled": () => g_GameAttributes.mapType != "scenario",
 		"initOrder": 1000
 	},
@@ -697,7 +682,7 @@ var g_Dropdowns = {
 		"select": (itemIdx) => {
 			g_GameAttributes.settings.WonderDuration = g_VictoryDurations.Duration[itemIdx];
 		},
-		"hidden": () => g_GameAttributes.settings.GameType != "wonder",
+		"hidden": () => g_GameAttributes.settings.VictoryConditions.indexOf("wonder") == -1,
 		"enabled": () => g_GameAttributes.mapType != "scenario",
 		"initOrder": 1000
 	},
@@ -822,7 +807,34 @@ var g_PlayerDropdowns = {
 /**
  * Contains the logic of all boolean gamesettings.
  */
-var g_Checkboxes = {
+var g_Checkboxes = Object.assign(
+	{},
+	g_VictoryConditions.reduce((obj, victoryCondition) => {
+		obj[victoryCondition.Name] = {
+			"title": () => victoryCondition.Title,
+			"tooltip": () => victoryCondition.Description,
+			// Defaults are set in supplementDefault directly from g_VictoryConditions since we use an array
+			"defined": () => true,
+			"get": () => g_GameAttributes.settings.VictoryConditions.indexOf(victoryCondition.Name) != -1,
+			"set": checked => {
+				if (checked)
+				{
+					g_GameAttributes.settings.VictoryConditions.push(victoryCondition.Name);
+					if (victoryCondition.Set)
+						for (let setting in victoryCondition.ChangeWhenChecked)
+							g_Checkboxes[setting].set(victoryCondition.ChangeOnChecked[setting]);
+				}
+				else
+					g_GameAttributes.settings.VictoryConditions = g_GameAttributes.settings.VictoryConditions.filter(victoryConditionName => victoryConditionName != victoryCondition.Name);
+			},
+			"enabled": () =>
+				g_GameAttributes.mapType != "scenario" &&
+				(!victoryCondition.DisabledWhenChecked ||
+				victoryCondition.DisabledWhenChecked.every(victoryConditionName => g_GameAttributes.settings.VictoryConditions.indexOf(victoryConditionName) == -1))
+		};
+		return obj;
+	}, {}),
+	{
 		"regicideGarrison": {
 			"title": () => translate("Hero Garrison"),
 			"tooltip": () => translate("Toggle whether heroes can be garrisoned."),
@@ -832,7 +844,7 @@ var g_Checkboxes = {
 			"set": checked => {
 				g_GameAttributes.settings.RegicideGarrison = checked;
 			},
-			"hidden": () => g_GameAttributes.settings.GameType != "regicide",
+			"hidden": () => g_GameAttributes.settings.VictoryConditions.indexOf("regicide") == -1,
 			"enabled": () => g_GameAttributes.mapType != "scenario",
 			"initOrder": 1000
 		},
@@ -968,7 +980,8 @@ var g_Checkboxes = {
 			},
 			"initOrder": 1000
 		},
-};
+	}
+);
 
 /**
  * For setting up arbitrary GUI objects.
@@ -1149,6 +1162,9 @@ function initDefaults()
  */
 function supplementDefaults()
 {
+	g_GameAttributes.settings.VictoryConditions = g_GameAttributes.settings.VictoryConditions ||
+		g_VictoryConditions.filter(victoryCondition => !!victoryCondition.Default).map(victoryCondition => victoryCondition.Name);
+
 	for (let dropdown in g_Dropdowns)
 		if (!g_Dropdowns[dropdown].defined())
 			g_Dropdowns[dropdown].select(g_Dropdowns[dropdown].default());
@@ -1987,13 +2003,7 @@ function selectMap(name)
 	let mapSettings = mapData && mapData.settings ? clone(mapData.settings) : {};
 
 	if (g_GameAttributes.mapType != "random")
-	{
 		delete g_GameAttributes.settings.Nomad;
-
-		let victoryIdx = g_VictoryConditions.Name.indexOf(mapSettings.GameType || "") != -1 ? g_VictoryConditions.Name.indexOf(mapSettings.GameType) : g_VictoryConditions.Default;
-		g_GameAttributes.settings.GameType = g_VictoryConditions.Name[victoryIdx];
-		g_GameAttributes.settings.VictoryScripts = g_VictoryConditions.Scripts[victoryIdx];
-	}
 
 	if (g_GameAttributes.mapType == "scenario")
 	{
@@ -2134,19 +2144,18 @@ function launchGame()
 
 	// Select random map
 	if (g_GameAttributes.map == "random")
-	{
-		let victoryScriptsSelected = g_GameAttributes.settings.VictoryScripts;
-		let gameTypeSelected = g_GameAttributes.settings.GameType;
 		selectMap(pickRandom(g_Dropdowns.mapSelection.ids().slice(1)));
-		g_GameAttributes.settings.VictoryScripts = victoryScriptsSelected;
-		g_GameAttributes.settings.GameType = gameTypeSelected;
-	}
 
 	if (g_GameAttributes.settings.Biome == "random")
 		g_GameAttributes.settings.Biome = pickRandom(
 			typeof g_GameAttributes.settings.SupportedBiomes == "string" ?
 				g_BiomeList.Id.slice(1).filter(biomeID => biomeID.startsWith(g_GameAttributes.settings.SupportedBiomes)) :
 				g_GameAttributes.settings.SupportedBiomes);
+
+	g_GameAttributes.settings.VictoryScripts = g_GameAttributes.settings.VictoryConditions.reduce(
+		(scripts, victoryConditionName) => scripts.concat(g_VictoryConditions[g_VictoryConditions.map(data =>
+			data.Name).indexOf(victoryConditionName)].Scripts.filter(script => scripts.indexOf(script) == -1)),
+		[]);
 
 	g_GameAttributes.settings.TriggerScripts = g_GameAttributes.settings.VictoryScripts.concat(g_GameAttributes.settings.TriggerScripts || []);
 
@@ -2640,7 +2649,7 @@ function sendRegisterGameStanzaImmediate()
 		"niceMapName": getMapDisplayName(g_GameAttributes.map),
 		"mapSize": g_GameAttributes.mapType == "random" ? g_GameAttributes.settings.Size : "Default",
 		"mapType": g_GameAttributes.mapType,
-		"victoryCondition": g_GameAttributes.settings.GameType,
+		"victoryConditions": g_GameAttributes.settings.VictoryConditions.join(","),
 		"nbp": clients.connectedPlayers,
 		"maxnbp": g_GameAttributes.settings.PlayerData.length,
 		"players": clients.list,

binaries/data/mods/public/gui/loadgame/load.js
@@ -113,7 +113,7 @@ function selectionChanged()
 	Engine.GetGUIObjectByName("savedPlayedTime").caption = timeToString(metadata.gui.timeElapsed ? metadata.gui.timeElapsed : 0);
 	Engine.GetGUIObjectByName("savedMapType").caption = translateMapType(metadata.initAttributes.mapType);
 	Engine.GetGUIObjectByName("savedMapSize").caption = translateMapSize(metadata.initAttributes.settings.Size);
-	Engine.GetGUIObjectByName("savedVictory").caption = translateVictoryCondition(metadata.initAttributes.settings.GameType);
+	Engine.GetGUIObjectByName("savedVictory").caption = metadata.initAttributes.settings.VictoryConditions.map(victoryConditionName => translateVictoryCondition(victoryConditionName)).join(translate(", "));
 
 	let caption = sprintf(translate("Mods: %(mods)s"), { "mods": modsToString(metadata.mods) });
 	if (!hasSameMods(metadata.mods, Engine.GetEngineInfo().mods))

binaries/data/mods/public/gui/loadgame/load.xml
@@ -97,17 +97,17 @@
 				<translatableAttribute id="caption">Map Size:</translatableAttribute>
 			</object>
 			<object name="savedMapSize" size="50% 310 100%-15 330" type="text" style="ModernLeftLabelText" />
-			<object size="0 330 50% 350" type="text" style="ModernLeftLabelText">
+			<object size="0 330 50% 370" type="text" style="ModernLeftLabelText">
 				<translatableAttribute id="caption">Victory:</translatableAttribute>
 			</object>
-			<object name="savedVictory" size="50% 330 100%-15 350" type="text" style="ModernLeftLabelText" />
+			<object name="savedVictory" size="50% 330 100%-15 370" type="text" style="ModernText" font="sans-bold-stroke-14" />
 
-			<object size="0 352 100%-15 353" type="image" sprite="ModernWhiteLine" z="25" />
+			<object size="0 372 100%-15 373" type="image" sprite="ModernWhiteLine" z="25" />
 
-			<object name="savedMods" size="0 355 100%-15 395" type="text" style="ModernLeftLabelText" />
+			<object name="savedMods" size="0 375 100%-15 415" type="text" style="ModernLeftLabelText" />
 
-			<object size="0 397 100%-15 398" type="image" sprite="ModernWhiteLine" z="25" />
-			<object name="savedPlayersNames" size="0 400 100%-10 100%-32" type="text" style="MapPlayerList" />
+			<object size="0 417 100%-15 418" type="image" sprite="ModernWhiteLine" z="25" />
+			<object name="savedPlayersNames" size="0 420 100%-10 100%-32" type="text" style="MapPlayerList" />
 			<object size="0 100%-32 100%-15 100%-31" type="image" sprite="ModernWhiteLine" z="25" />
 		</object>
 

binaries/data/mods/public/gui/replaymenu/replay_filters.js
@@ -152,8 +152,8 @@ function initSingleplayerFilter(filters)
 function initVictoryConditionFilter(filters)
 {
 	let victoryConditionFilter = Engine.GetGUIObjectByName("victoryConditionFilter");
-	victoryConditionFilter.list = [translateWithContext("victory condition", "Any gametype")].concat(g_VictoryConditions.map(vc => translateVictoryCondition(vc)));
-	victoryConditionFilter.list_data = [""].concat(g_VictoryConditions);
+	victoryConditionFilter.list = [translate("Any Victory Condition")].concat(g_VictoryConditions.map(victoryCondition => translateVictoryCondition(victoryCondition.Name)));
+	victoryConditionFilter.list_data = [""].concat(g_VictoryConditions.map(victoryCondition => victoryCondition.Name));
 
 	if (filters && filters.victoryCondition)
 		victoryConditionFilter.selected = victoryConditionFilter.list_data.indexOf(filters.victoryCondition);
@@ -243,7 +243,8 @@ function filterReplay(replay)
 
 	// Filter by victory condition
 	let victoryConditionFilter = Engine.GetGUIObjectByName("victoryConditionFilter");
-	if (victoryConditionFilter.selected > 0 && replay.attribs.settings.GameType != victoryConditionFilter.list_data[victoryConditionFilter.selected])
+	if (victoryConditionFilter.selected > 0 &&
+	    replay.attribs.settings.VictoryConditions.indexOf(victoryConditionFilter.list_data[victoryConditionFilter.selected]) == -1)
 		return false;
 
 	// Filter by rating

binaries/data/mods/public/gui/replaymenu/replay_menu.js
@@ -36,7 +36,7 @@ var g_MapNames = [];
 /**
  * Sorted list of the victory conditions occuring in the replays
  */
-var g_VictoryConditions = [];
+var g_VictoryConditions = g_Settings && g_Settings.VictoryConditions;
 
 /**
  * Directory name of the currently selected replay. Used to restore the selection after changing filters.
@@ -108,10 +108,6 @@ function loadReplays(replaySelectionData, compareFiles)
 		if (g_MapNames.indexOf(replay.attribs.settings.Name) == -1 && replay.attribs.settings.Name != "")
 			g_MapNames.push(replay.attribs.settings.Name);
 
-		// Extract victory conditions
-		if (replay.attribs.settings.GameType && g_VictoryConditions.indexOf(replay.attribs.settings.GameType) == -1)
-			g_VictoryConditions.push(replay.attribs.settings.GameType);
-
 		// Extract playernames
 		for (let playerData of replay.attribs.settings.PlayerData)
 		{
@@ -138,7 +134,6 @@ function loadReplays(replaySelectionData, compareFiles)
 	}
 
 	g_MapNames.sort();
-	g_VictoryConditions.sort();
 
 	// Reload filters (since they depend on g_Replays and its derivatives)
 	initFilters(replaySelectionData && replaySelectionData.filters);
@@ -181,9 +176,6 @@ function sanitizeGameAttributes(attribs)
 
 	if (!attribs.settings.mapType)
 		attribs.settings.mapType = "skirmish";
-
-	if (!attribs.settings.GameType)
-		attribs.settings.GameType = "conquest";
 
 	// Remove gaia
 	if (attribs.settings.PlayerData.length && attribs.settings.PlayerData[0] == null)
@@ -276,7 +268,8 @@ function displayReplayDetails()
 	Engine.GetGUIObjectByName("sgMapName").caption = translate(replay.attribs.settings.Name);
 	Engine.GetGUIObjectByName("sgMapSize").caption = translateMapSize(replay.attribs.settings.Size);
 	Engine.GetGUIObjectByName("sgMapType").caption = translateMapType(replay.attribs.settings.mapType);
-	Engine.GetGUIObjectByName("sgVictory").caption = translateVictoryCondition(replay.attribs.settings.GameType);
+	Engine.GetGUIObjectByName("sgVictory").caption = replay.attribs.settings.VictoryConditions.map(victoryConditionName =>
+		translateVictoryCondition(victoryConditionName)).join(translate(", "));
 	Engine.GetGUIObjectByName("sgNbPlayers").caption = sprintf(translate("Players: %(numberOfPlayers)s"),
 		{ "numberOfPlayers": replay.attribs.settings.PlayerData.length });
 	Engine.GetGUIObjectByName("replayFilename").caption = Engine.GetReplayDirectoryName(replay.directory);

commit 702127b8dff9b9f021d8834c5d04ce721a67edc3

Add autostart conquest default description to readme.txt and add endless support for autostart in both readme.txt and GameSetup.cpp
Reported By: elexis
Fixes: 6d54ab4c1f, 22903b09a6

This was SVN commit r22112.

source/ps/GameSetup/GameSetup.cpp
@@ -1199,7 +1199,8 @@ CStr8 LoadSettingsOfScenarioMap(const VfsPath &mapPath)
  * -autostart-nonvisual            disable any graphics and sounds
  * -autostart-victory=SCRIPTNAME   sets the victory conditions with SCRIPTNAME
  *                                 located in simulation/data/settings/victory_conditions/
- *                                 (default conquest)
+ *                                 (default conquest). When the first given SCRIPTNAME is 
+ *                                 "endless", no victory conditions will apply.
  * -autostart-wonderduration=NUM   sets the victory duration NUM for wonder victory condition
  *                                 (default 10 minutes)
  * -autostart-relicduration=NUM    sets the victory duration NUM for relic victory condition