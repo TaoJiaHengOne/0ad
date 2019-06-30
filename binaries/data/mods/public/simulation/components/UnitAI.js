TaoJiaHengOne/0ad

commit c219ee54b2443e5fd3d722bd11dc5cb31d18d87d

Move "IsInRange" family of functions to the Obstruction Manager and make the commutative.
These functions were placed in UnitMotion, which had nothing to do with
range checks and made them available only to moving entities for no
particular reason.

This patch also adds support for square-square range checks and
shape-shape distance checks.

Modified from a patch by bb on top of work from wraitii.

Differential Revision: https://code.wildfiregames.com/D981
This was SVN commit r22345.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1635,8 +1635,8 @@ UnitAI.prototype.UnitFsmSpec = {
 
 				"MoveStarted": function(msg) {
 					// Adapt the speed to the one of the target if needed
-					var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
-					if (cmpUnitMotion.IsInTargetRange(this.isGuardOf, 0, 3*this.guardRange))
+					let cmpObstructionManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ObstructionManager);
+					if (cmpObstructionManager.IsInTargetRange(this.entity, this.isGuardOf, 0, 3 * this.guardRange, true))
 					{
 						var cmpUnitAI = Engine.QueryInterface(this.isGuardOf, IID_UnitAI);
 						if (cmpUnitAI)
@@ -4308,8 +4308,8 @@ UnitAI.prototype.MoveToGarrisonRange = function(target)
 
 UnitAI.prototype.CheckPointRangeExplicit = function(x, z, min, max)
 {
-	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
-	return cmpUnitMotion.IsInPointRange(x, z, min, max);
+	let cmpObstructionManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ObstructionManager);
+	return cmpObstructionManager.IsInPointRange(this.entity, x, z, min, max, true);
 };
 
 UnitAI.prototype.CheckTargetRange = function(target, iid, type)
@@ -4319,8 +4319,8 @@ UnitAI.prototype.CheckTargetRange = function(target, iid, type)
 		return false;
 	var range = cmpRanged.GetRange(type);
 
-	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
-	return cmpUnitMotion.IsInTargetRange(target, range.min, range.max);
+	let cmpObstructionManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ObstructionManager);
+	return cmpObstructionManager.IsInTargetRange(this.entity, target, range.min, range.max, true);
 };
 
 /**
@@ -4368,14 +4368,14 @@ UnitAI.prototype.CheckTargetAttackRange = function(target, type)
 	if (maxRangeSq < 0)
 		return false;
 
-	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
-	return cmpUnitMotion.IsInTargetRange(target, range.min, Math.sqrt(maxRangeSq));
+	let cmpObstructionManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ObstructionManager);
+	return cmpObstructionManager.IsInTargetRange(this.entity, target, range.min, Math.sqrt(maxRangeSq), true);
 };
 
 UnitAI.prototype.CheckTargetRangeExplicit = function(target, min, max)
 {
-	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
-	return cmpUnitMotion.IsInTargetRange(target, min, max);
+	let cmpObstructionManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ObstructionManager);
+	return cmpObstructionManager.IsInTargetRange(this.entity, target, min, max, true);
 };
 
 UnitAI.prototype.CheckGarrisonRange = function(target)
@@ -4389,8 +4389,8 @@ UnitAI.prototype.CheckGarrisonRange = function(target)
 	if (cmpObstruction)
 		range.max += cmpObstruction.GetUnitRadius()*1.5; // multiply by something larger than sqrt(2)
 
-	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
-	return cmpUnitMotion.IsInTargetRange(target, range.min, range.max);
+	let cmpObstructionManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_ObstructionManager);
+	return cmpObstructionManager.IsInTargetRange(this.entity, target, range.min, range.max, true);
 };
 
 /**

source/simulation2/components/CCmpObstructionManager.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Wildfire Games.
+/* Copyright (C) 2019 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -466,6 +466,15 @@ public:
 	}
 
 	virtual fixed DistanceToPoint(entity_id_t ent, entity_pos_t px, entity_pos_t pz) const;
+	virtual fixed MaxDistanceToPoint(entity_id_t ent, entity_pos_t px, entity_pos_t pz) const;
+	virtual fixed DistanceToTarget(entity_id_t ent, entity_id_t target) const;
+	virtual fixed MaxDistanceToTarget(entity_id_t ent, entity_id_t target) const;
+	virtual fixed DistanceBetweenShapes(const ObstructionSquare& source, const ObstructionSquare& target) const;
+	virtual fixed MaxDistanceBetweenShapes(const ObstructionSquare& source, const ObstructionSquare& target) const;
+
+	virtual bool IsInPointRange(entity_id_t ent, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const;
+	virtual bool IsInTargetRange(entity_id_t ent, entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const;
+	virtual bool IsPointInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange) const;
 
 	virtual bool TestLine(const IObstructionTestFilter& filter, entity_pos_t x0, entity_pos_t z0, entity_pos_t x1, entity_pos_t z1, entity_pos_t r, bool relaxClearanceForUnits = false) const;
 	virtual bool TestStaticShape(const IObstructionTestFilter& filter, entity_pos_t x, entity_pos_t z, entity_pos_t a, entity_pos_t w, entity_pos_t h, std::vector<entity_id_t>* out) const;
@@ -657,18 +666,173 @@ private:
 
 REGISTER_COMPONENT_TYPE(ObstructionManager)
 
+/**
+ * DistanceTo function family, all end up in calculating a vector length, DistanceBetweenShapes or
+ * MaxDistanceBetweenShapes. The MaxFoo family calculates the opposite edge opposite edge distance.
+ * When the distance is undefined we return -1.
+ */
 fixed CCmpObstructionManager::DistanceToPoint(entity_id_t ent, entity_pos_t px, entity_pos_t pz) const
 {
+	ObstructionSquare obstruction;
+	CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), ent);
+	if (cmpObstruction && cmpObstruction->GetObstructionSquare(obstruction))
+	{
+		ObstructionSquare point;
+		point.x = px;
+		point.z = pz;
+		return DistanceBetweenShapes(obstruction, point);
+	}
+
 	CmpPtr<ICmpPosition> cmpPosition(GetSimContext(), ent);
 	if (!cmpPosition || !cmpPosition->IsInWorld())
 		return fixed::FromInt(-1);
 
-	ObstructionSquare s;
-	CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), ent);
-	if (!cmpObstruction || !cmpObstruction->GetObstructionSquare(s))
-		return (CFixedVector2D(px, pz) - cmpPosition->GetPosition2D()).Length();
+	return (CFixedVector2D(cmpPosition->GetPosition2D().X, cmpPosition->GetPosition2D().Y) - CFixedVector2D(px, pz)).Length();
+}
 
-	return Geometry::DistanceToSquare(CFixedVector2D(px - s.x, pz - s.z), s.u, s.v, CFixedVector2D(s.hw, s.hh));
+fixed CCmpObstructionManager::MaxDistanceToPoint(entity_id_t ent, entity_pos_t px, entity_pos_t pz) const
+{
+	ObstructionSquare obstruction;
+	CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), ent);
+	if (!cmpObstruction || !cmpObstruction->GetObstructionSquare(obstruction))
+	{
+		ObstructionSquare point;
+		point.x = px;
+		point.z = pz;
+		return MaxDistanceBetweenShapes(obstruction, point);
+	}
+
+	CmpPtr<ICmpPosition> cmpPosition(GetSimContext(), ent);
+	if (!cmpPosition || !cmpPosition->IsInWorld())
+		return fixed::FromInt(-1);
+
+	return (CFixedVector2D(cmpPosition->GetPosition2D().X, cmpPosition->GetPosition2D().Y) - CFixedVector2D(px, pz)).Length();
+}
+
+fixed CCmpObstructionManager::DistanceToTarget(entity_id_t ent, entity_id_t target) const
+{
+	ObstructionSquare obstruction;
+	CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), ent);
+	if (!cmpObstruction || !cmpObstruction->GetObstructionSquare(obstruction))
+	{
+		CmpPtr<ICmpPosition> cmpPosition(GetSimContext(), ent);
+		if (!cmpPosition || !cmpPosition->IsInWorld())
+			return fixed::FromInt(-1);
+		return DistanceToPoint(target, cmpPosition->GetPosition2D().X, cmpPosition->GetPosition2D().Y);
+	}
+
+	ObstructionSquare target_obstruction;
+	CmpPtr<ICmpObstruction> cmpObstructionTarget(GetSimContext(), target);
+	if (!cmpObstructionTarget || !cmpObstructionTarget->GetObstructionSquare(target_obstruction))
+	{
+		CmpPtr<ICmpPosition> cmpPositionTarget(GetSimContext(), target);
+		if (!cmpPositionTarget || !cmpPositionTarget->IsInWorld())
+			return fixed::FromInt(-1);
+		return DistanceToPoint(ent, cmpPositionTarget->GetPosition2D().X, cmpPositionTarget->GetPosition2D().Y);
+	}
+
+	return DistanceBetweenShapes(obstruction, target_obstruction);
+}
+
+fixed CCmpObstructionManager::MaxDistanceToTarget(entity_id_t ent, entity_id_t target) const
+{
+	ObstructionSquare obstruction;
+	CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), ent);
+	if (!cmpObstruction || !cmpObstruction->GetObstructionSquare(obstruction))
+	{
+		CmpPtr<ICmpPosition> cmpPosition(GetSimContext(), ent);
+		if (!cmpPosition || !cmpPosition->IsInWorld())
+			return fixed::FromInt(-1);
+		return MaxDistanceToPoint(target, cmpPosition->GetPosition2D().X, cmpPosition->GetPosition2D().Y);
+	}
+
+	ObstructionSquare target_obstruction;
+	CmpPtr<ICmpObstruction> cmpObstructionTarget(GetSimContext(), target);
+	if (!cmpObstructionTarget || !cmpObstructionTarget->GetObstructionSquare(target_obstruction))
+	{
+		CmpPtr<ICmpPosition> cmpPositionTarget(GetSimContext(), target);
+		if (!cmpPositionTarget || !cmpPositionTarget->IsInWorld())
+			return fixed::FromInt(-1);
+		return MaxDistanceToPoint(ent, cmpPositionTarget->GetPosition2D().X, cmpPositionTarget->GetPosition2D().Y);
+	}
+
+	return MaxDistanceBetweenShapes(obstruction, target_obstruction);
+}
+
+fixed CCmpObstructionManager::DistanceBetweenShapes(const ObstructionSquare& source, const ObstructionSquare& target) const
+{
+	// Sphere-sphere collision.
+	if (source.hh == fixed::Zero() && target.hh == fixed::Zero())
+		return (CFixedVector2D(target.x, target.z) - CFixedVector2D(source.x, source.z)).Length() - source.hw - target.hw;
+
+	// Square to square.
+	if (source.hh != fixed::Zero() && target.hh != fixed::Zero())
+		return Geometry::DistanceSquareToSquare(
+			CFixedVector2D(target.x, target.z) - CFixedVector2D(source.x, source.z),
+			source.u, source.v, CFixedVector2D(source.hw, source.hh),
+			target.u, target.v, CFixedVector2D(target.hw, target.hh));
+
+	// To cover both remaining cases, shape a is the square one, shape b is the circular one.
+	const ObstructionSquare& a = source.hh == fixed::Zero() ? target : source;
+	const ObstructionSquare& b = source.hh == fixed::Zero() ? source : target;
+	return Geometry::DistanceToSquare(
+		CFixedVector2D(b.x, b.z) - CFixedVector2D(a.x, a.z),
+		a.u, a.v, CFixedVector2D(a.hw, a.hh), true) - b.hw;
+}
+
+fixed CCmpObstructionManager::MaxDistanceBetweenShapes(const ObstructionSquare& source, const ObstructionSquare& target) const
+{
+	// Sphere-sphere collision.
+	if (source.hh == fixed::Zero() && target.hh == fixed::Zero())
+		return (CFixedVector2D(target.x, target.z) - CFixedVector2D(source.x, source.z)).Length() + source.hw + target.hw;
+
+	// Square to square.
+	if (source.hh != fixed::Zero() && target.hh != fixed::Zero())
+		return Geometry::MaxDistanceSquareToSquare(
+			CFixedVector2D(target.x, target.z) - CFixedVector2D(source.x, source.z),
+			source.u, source.v, CFixedVector2D(source.hw, source.hh),
+			target.u, target.v, CFixedVector2D(target.hw, target.hh));
+
+	// To cover both remaining cases, shape a is the square one, shape b is the circular one.
+	const ObstructionSquare& a = source.hh == fixed::Zero() ? target : source;
+	const ObstructionSquare& b = source.hh == fixed::Zero() ? source : target;
+	return Geometry::MaxDistanceToSquare(
+		CFixedVector2D(b.x, b.z) - CFixedVector2D(a.x, a.z),
+		a.u, a.v, CFixedVector2D(a.hw, a.hh), true) + b.hw;
+}
+
+/**
+ * IsInRange function family depending on the DistanceTo family.
+ *
+ * In range if the edge to edge distance is inferior to maxRange
+ * and if the opposite edge to opposite edge distance is greater than minRange when the opposite bool is true
+ * or when the opposite bool is false the edge to edge distance is more than minRange.
+ *
+ * Using the opposite egde for minRange means that a unit is in range of a building if it is farther than
+ * clearance-buildingsize, which is generally going to be negative (and thus this returns true).
+ * NB: from a game POV, this means units can easily fire on buildings, which is good,
+ * but it also means that buildings can easily fire on units. Buildings are usually meant
+ * to fire from the edge, not the opposite edge, so this looks odd. For this reason one can choose
+ * to set the opposite bool false and use the edge to egde distance.
+ *
+ * We don't use squares because the are likely to overflow.
+ * We use a 0.0001 margin to avoid rounding errors.
+ */
+bool CCmpObstructionManager::IsInPointRange(entity_id_t ent, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const
+{
+	fixed dist = DistanceToPoint(ent, px, pz);
+	return dist != fixed::FromInt(-1) && dist <= maxRange + fixed::FromFloat(0.0001) && (opposite ? MaxDistanceToPoint(ent, px, pz) : dist) >= minRange - fixed::FromFloat(0.0001);
+}
+
+bool CCmpObstructionManager::IsInTargetRange(entity_id_t ent, entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const
+{
+	fixed dist = DistanceToTarget(ent, target);
+	return dist != fixed::FromInt(-1) && dist <= maxRange + fixed::FromFloat(0.0001) && (opposite ? MaxDistanceToTarget(ent, target) : dist) >= minRange- fixed::FromFloat(0.0001);
+}
+bool CCmpObstructionManager::IsPointInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange) const
+{
+	entity_pos_t distance = (CFixedVector2D(x, z) - CFixedVector2D(px, pz)).Length();
+	return distance <= maxRange + fixed::FromFloat(0.0001) && distance >= minRange - fixed::FromFloat(0.0001);
 }
 
 bool CCmpObstructionManager::TestLine(const IObstructionTestFilter& filter, entity_pos_t x0, entity_pos_t z0, entity_pos_t x1, entity_pos_t z1, entity_pos_t r, bool relaxClearanceForUnits) const

source/simulation2/components/CCmpUnitMotion.cpp
@@ -503,9 +503,7 @@ public:
 	}
 
 	virtual bool MoveToPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t minRange, entity_pos_t maxRange);
-	virtual bool IsInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t minRange, entity_pos_t maxRange) const;
 	virtual bool MoveToTargetRange(entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange);
-	virtual bool IsInTargetRange(entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange) const;
 	virtual void MoveToFormationOffset(entity_id_t target, entity_pos_t x, entity_pos_t z);
 
 	virtual void FaceTowardsPoint(entity_pos_t x, entity_pos_t z);
@@ -1057,8 +1055,9 @@ void CCmpUnitMotion::Move(fixed dt)
 				else
 				{
 					// check if target was reached in case of a moving target
+					CmpPtr<ICmpObstructionManager> cmpObstructionManager(GetSystemEntity());
 					CmpPtr<ICmpUnitMotion> cmpUnitMotion(GetSimContext(), m_TargetEntity);
-					if (!cmpUnitMotion || cmpUnitMotion->IsInTargetRange(GetEntityId(), m_TargetMinRange, m_TargetMaxRange))
+					if (!cmpUnitMotion || cmpObstructionManager->IsInTargetRange(GetEntityId(), m_TargetEntity, m_TargetMinRange, m_TargetMaxRange, false))
 					{
 						// Not in formation, so just finish moving
 						StopMoving();
@@ -1471,49 +1470,6 @@ bool CCmpUnitMotion::MoveToPointRange(entity_pos_t x, entity_pos_t z, entity_pos
 	return true;
 }
 
-bool CCmpUnitMotion::IsInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t minRange, entity_pos_t maxRange) const
-{
-	CmpPtr<ICmpPosition> cmpPosition(GetEntityHandle());
-	if (!cmpPosition || !cmpPosition->IsInWorld())
-		return false;
-
-	CFixedVector2D pos = cmpPosition->GetPosition2D();
-
-	bool hasObstruction = false;
-	CmpPtr<ICmpObstructionManager> cmpObstructionManager(GetSystemEntity());
-	ICmpObstructionManager::ObstructionSquare obstruction;
-//TODO	if (cmpObstructionManager)
-//		hasObstruction = cmpObstructionManager->FindMostImportantObstruction(GetObstructionFilter(), x, z, m_Radius, obstruction);
-
-	if (minRange.IsZero() && maxRange.IsZero() && hasObstruction)
-	{
-		// Handle the non-ranged mode:
-		CFixedVector2D halfSize(obstruction.hw, obstruction.hh);
-		entity_pos_t distance = Geometry::DistanceToSquare(pos - CFixedVector2D(obstruction.x, obstruction.z), obstruction.u, obstruction.v, halfSize);
-
-		// See if we're too close to the target square
-		if (distance < minRange)
-			return false;
-
-		// See if we're close enough to the target square
-		if (maxRange < entity_pos_t::Zero() || distance <= maxRange)
-			return true;
-
-		return false;
-	}
-	else
-	{
-		entity_pos_t distance = (pos - CFixedVector2D(x, z)).Length();
-
-		if (distance < minRange)
-			return false;
-		else if (maxRange >= entity_pos_t::Zero() && distance > maxRange)
-			return false;
-		else
-			return true;
-	}
-}
-
 bool CCmpUnitMotion::ShouldTreatTargetAsCircle(entity_pos_t range, entity_pos_t circleRadius) const
 {
 	// Given a square, plus a target range we should reach, the shape at that distance
@@ -1682,76 +1638,6 @@ bool CCmpUnitMotion::MoveToTargetRange(entity_id_t target, entity_pos_t minRange
 	BeginPathing(pos, goal);
 
 	return true;
-}
-
-bool CCmpUnitMotion::IsInTargetRange(entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange) const
-{
-	// This function closely mirrors MoveToTargetRange - it needs to return true
-	// after that Move has completed
-
-	CmpPtr<ICmpPosition> cmpPosition(GetEntityHandle());
-	if (!cmpPosition || !cmpPosition->IsInWorld())
-		return false;
-
-	CFixedVector2D pos = cmpPosition->GetPosition2D();
-
-	CmpPtr<ICmpObstructionManager> cmpObstructionManager(GetSystemEntity());
-	if (!cmpObstructionManager)
-		return false;
-
-	bool hasObstruction = false;
-	ICmpObstructionManager::ObstructionSquare obstruction;
-	CmpPtr<ICmpObstruction> cmpObstruction(GetSimContext(), target);
-	if (cmpObstruction)
-		hasObstruction = cmpObstruction->GetObstructionSquare(obstruction);
-
-	if (hasObstruction)
-	{
-		CFixedVector2D halfSize(obstruction.hw, obstruction.hh);
-		entity_pos_t distance = Geometry::DistanceToSquare(pos - CFixedVector2D(obstruction.x, obstruction.z), obstruction.u, obstruction.v, halfSize, true);
-
-		// Compare with previous obstruction
-		ICmpObstructionManager::ObstructionSquare previousObstruction;
-		cmpObstruction->GetPreviousObstructionSquare(previousObstruction);
-		entity_pos_t previousDistance = Geometry::DistanceToSquare(pos - CFixedVector2D(previousObstruction.x, previousObstruction.z), obstruction.u, obstruction.v, halfSize, true);
-
-		// See if we're too close to the target square
-		bool inside = distance.IsZero() && !Geometry::DistanceToSquare(pos - CFixedVector2D(obstruction.x, obstruction.z), obstruction.u, obstruction.v, halfSize).IsZero();
-		if ((distance < minRange && previousDistance < minRange) || inside)
-			return false;
-
-		// See if we're close enough to the target square
-		if (maxRange < entity_pos_t::Zero() || distance <= maxRange || previousDistance <= maxRange)
-			return true;
-
-		entity_pos_t circleRadius = halfSize.Length();
-
-		if (ShouldTreatTargetAsCircle(maxRange, circleRadius))
-		{
-			// The target is small relative to our range, so pretend it's a circle
-			// and see if we're close enough to that.
-			// Also check circle around previous position.
-			entity_pos_t circleDistance = (pos - CFixedVector2D(obstruction.x, obstruction.z)).Length() - circleRadius;
-			entity_pos_t previousCircleDistance = (pos - CFixedVector2D(previousObstruction.x, previousObstruction.z)).Length() - circleRadius;
-
-			return circleDistance <= maxRange || previousCircleDistance <= maxRange;
-		}
-
-		// take minimal clearance required in MoveToTargetRange into account, multiplying by 3/2 for diagonals
-		entity_pos_t maxDist = std::max(maxRange, (m_Clearance + entity_pos_t::FromInt(TERRAIN_TILE_SIZE)/16)*3/2);
-		return distance <= maxDist || previousDistance <= maxDist;
-	}
-	else
-	{
-		CmpPtr<ICmpPosition> cmpTargetPosition(GetSimContext(), target);
-		if (!cmpTargetPosition || !cmpTargetPosition->IsInWorld())
-			return false;
-
-		CFixedVector2D targetPos = cmpTargetPosition->GetPreviousPosition2D();
-		entity_pos_t distance = (pos - targetPos).Length();
-
-		return minRange <= distance && (maxRange < entity_pos_t::Zero() || distance <= maxRange);
-	}
 }
 
 void CCmpUnitMotion::MoveToFormationOffset(entity_id_t target, entity_pos_t x, entity_pos_t z)

source/simulation2/components/ICmpObstructionManager.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Wildfire Games.
+/* Copyright (C) 2019 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -53,6 +53,16 @@ class IObstructionTestFilter;
 class ICmpObstructionManager : public IComponent
 {
 public:
+	/**
+	 * Standard representation for all types of shapes, for use with geometry processing code.
+	 */
+	struct ObstructionSquare
+	{
+		entity_pos_t x, z; // position of center
+		CFixedVector2D u, v; // 'horizontal' and 'vertical' orthogonal unit vectors, representing orientation
+		entity_pos_t hw, hh; // half width, half height of square
+	};
+
 	/**
 	 * External identifiers for shapes.
 	 * (This is a struct rather than a raw u32 for type-safety.)
@@ -165,6 +175,46 @@ public:
 	virtual fixed DistanceToPoint(entity_id_t ent, entity_pos_t px, entity_pos_t pz) const = 0;
 
 	/**
+	 * Calculate the largest straight line distance between the entity and the point.
+	 */
+	virtual fixed MaxDistanceToPoint(entity_id_t ent, entity_pos_t px, entity_pos_t pz) const = 0;
+
+	/**
+	 * Calculate the shortest distance between the entity and the target.
+	 */
+	virtual fixed DistanceToTarget(entity_id_t ent, entity_id_t target) const = 0;
+
+	/**
+	 * Calculate the largest straight line distance between the entity and the target.
+	 */
+	virtual fixed MaxDistanceToTarget(entity_id_t ent, entity_id_t target) const = 0;
+
+	/**
+	 * Calculate the shortest straight line distance between the source and the target
+	 */
+	virtual fixed DistanceBetweenShapes(const ObstructionSquare& source, const ObstructionSquare& target) const = 0;
+
+	/**
+	 * Calculate the largest straight line distance between the source and the target
+	 */
+	virtual fixed MaxDistanceBetweenShapes(const ObstructionSquare& source, const ObstructionSquare& target) const = 0;
+
+	/**
+	 * Check if the given entity is in range of the other point given those parameters.
+	 */
+	virtual bool IsInPointRange(entity_id_t ent, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const = 0;
+
+	/**
+	 * Check if the given entity is in range of the target given those parameters.
+	 */
+	virtual bool IsInTargetRange(entity_id_t ent, entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const = 0;
+
+	/**
+	 * Check if the given point is in range of the other point given those parameters.
+	 */
+	virtual bool IsPointInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange) const = 0;
+
+	/**
 	 * Collision test a flat-ended thick line against the current set of shapes.
 	 * The line caps extend by @p r beyond the end points.
 	 * Only intersections going from outside to inside a shape are counted.
@@ -223,16 +273,6 @@ public:
 	 * The return value is false if an update is unnecessary.
 	 */
 	virtual void UpdateInformations(GridUpdateInformation& informations) = 0;
-
-	/**
-	 * Standard representation for all types of shapes, for use with geometry processing code.
-	 */
-	struct ObstructionSquare
-	{
-		entity_pos_t x, z; // position of center
-		CFixedVector2D u, v; // 'horizontal' and 'vertical' orthogonal unit vectors, representing orientation
-		entity_pos_t hw, hh; // half width, half height of square
-	};
 
 	/**
 	 * Find all the obstructions that are inside (or partially inside) the given range.

commit f42d97c6f36d2aa85f39f57824dcd20dd4c42012

Fix some animals "roaming" state not working correctly (units seem stuck in place)
Due to an issue in 4a15cc3b9f, animals incorrectly tried going towards
the roaming point instead of away from it.

With that fixed, MovementUpdate still did not trigger as the min and max
range were the same.
Use -1 as max range (= infinite) instead since we want to move
arbitrarily away.

Having an infinite max range was broken in c219ee54b2, this
re-implements that.

Further, other calls with equal min and max range have been changed
likewise.

This does not entirely fix whales, which run in other problems because
of their large roaming range.

Differential Revision: https://code.wildfiregames.com/D1980
This was SVN commit r22413.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1232,7 +1232,7 @@ UnitAI.prototype.UnitFsmSpec = {
 			}
 			// Move a tile outside the building
 			let range = 4;
-			if (this.CheckTargetRangeExplicit(msg.data.target, range, range))
+			if (this.CheckTargetRangeExplicit(msg.data.target, range, -1))
 			{
 				// We are already at the target, or can't move at all
 				this.FinishOrder();
@@ -2963,17 +2963,14 @@ UnitAI.prototype.UnitFsmSpec = {
 
 		"Order.LeaveFoundation": function(msg) {
 			// Move a tile outside the building
-			var range = 4;
-			if (this.CheckTargetRangeExplicit(msg.data.target, range, range))
+			let range = 4;
+			if (this.CheckTargetRangeExplicit(msg.data.target, range, -1))
 			{
 				this.FinishOrder();
 				return;
 			}
-			else
-			{
-				this.order.data.min = range;
-				this.SetNextState("WALKING");
-			}
+			this.order.data.min = range;
+			this.SetNextState("WALKING");
 		},
 
 		"IDLE": {
@@ -5785,7 +5782,7 @@ UnitAI.prototype.MoveRandomly = function(distance)
 	// Then second half of the rotation
 	ang += halfDelta;
 	let dist = randFloat(0.5, 1.5) * distance;
-	cmpUnitMotion.MoveToPointRange(pos.x - 0.5 * Math.sin(ang), pos.z - 0.5 * Math.cos(ang), dist, dist);
+	cmpUnitMotion.MoveToPointRange(pos.x - 0.5 * Math.sin(ang), pos.z - 0.5 * Math.cos(ang), dist, -1);
 };
 
 UnitAI.prototype.SetFacePointAfterMove = function(val)

source/simulation2/components/CCmpObstructionManager.cpp
@@ -821,18 +821,26 @@ fixed CCmpObstructionManager::MaxDistanceBetweenShapes(const ObstructionSquare& 
 bool CCmpObstructionManager::IsInPointRange(entity_id_t ent, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const
 {
 	fixed dist = DistanceToPoint(ent, px, pz);
-	return dist != fixed::FromInt(-1) && dist <= maxRange + fixed::FromFloat(0.0001) && (opposite ? MaxDistanceToPoint(ent, px, pz) : dist) >= minRange - fixed::FromFloat(0.0001);
+	// Treat -1 max range as infinite
+	return dist != fixed::FromInt(-1) &&
+	      (dist <= (maxRange + fixed::FromFloat(0.0001)) || maxRange < fixed::Zero()) &&
+	      (opposite ? MaxDistanceToPoint(ent, px, pz) : dist) >= minRange - fixed::FromFloat(0.0001);
 }
 
 bool CCmpObstructionManager::IsInTargetRange(entity_id_t ent, entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const
 {
 	fixed dist = DistanceToTarget(ent, target);
-	return dist != fixed::FromInt(-1) && dist <= maxRange + fixed::FromFloat(0.0001) && (opposite ? MaxDistanceToTarget(ent, target) : dist) >= minRange- fixed::FromFloat(0.0001);
+	// Treat -1 max range as infinite
+	return dist != fixed::FromInt(-1) &&
+	      (dist <= (maxRange + fixed::FromFloat(0.0001)) || maxRange < fixed::Zero()) &&
+	      (opposite ? MaxDistanceToTarget(ent, target) : dist) >= minRange - fixed::FromFloat(0.0001);
 }
 bool CCmpObstructionManager::IsPointInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange) const
 {
 	entity_pos_t distance = (CFixedVector2D(x, z) - CFixedVector2D(px, pz)).Length();
-	return distance <= maxRange + fixed::FromFloat(0.0001) && distance >= minRange - fixed::FromFloat(0.0001);
+	// Treat -1 max range as infinite
+	return (distance <= (maxRange + fixed::FromFloat(0.0001)) || maxRange < fixed::Zero()) &&
+	        distance >= minRange - fixed::FromFloat(0.0001);
 }
 
 bool CCmpObstructionManager::TestLine(const IObstructionTestFilter& filter, entity_pos_t x0, entity_pos_t z0, entity_pos_t x1, entity_pos_t z1, entity_pos_t r, bool relaxClearanceForUnits) const

source/simulation2/components/CCmpUnitMotion.cpp
@@ -1081,7 +1081,8 @@ bool CCmpUnitMotion::ComputeTargetPosition(CFixedVector2D& out) const
 bool CCmpUnitMotion::TryGoingStraightToGoalPoint(const CFixedVector2D& from)
 {
 	// Make sure the goal is a point (and not a point-like target like a formation controller)
-	if (m_MoveRequest.m_Type != MoveRequest::POINT)
+	if (m_MoveRequest.m_Type != MoveRequest::POINT ||
+		m_MoveRequest.m_MinRange > fixed::Zero())
 		return false;
 
 	// Fail if the goal is too far away

source/simulation2/components/ICmpObstructionManager.h
@@ -201,16 +201,19 @@ public:
 
 	/**
 	 * Check if the given entity is in range of the other point given those parameters.
+	 * @param maxRange - if -1, treated as infinite.
 	 */
 	virtual bool IsInPointRange(entity_id_t ent, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const = 0;
 
 	/**
 	 * Check if the given entity is in range of the target given those parameters.
+	 * @param maxRange - if -1, treated as infinite.
 	 */
 	virtual bool IsInTargetRange(entity_id_t ent, entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange, bool opposite) const = 0;
 
 	/**
 	 * Check if the given point is in range of the other point given those parameters.
+	 * @param maxRange - if -1, treated as infinite.
 	 */
 	virtual bool IsPointInPointRange(entity_pos_t x, entity_pos_t z, entity_pos_t px, entity_pos_t pz, entity_pos_t minRange, entity_pos_t maxRange) const = 0;
 