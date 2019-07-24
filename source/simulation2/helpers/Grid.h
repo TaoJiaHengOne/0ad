TaoJiaHengOne/0ad

commit 3a839517ce22f85eea167dadc41031afeffaa459

Fix the -Wclass-memaccess compiler warning in the pathfinders Grid.h, refs #5294.
Differential Revision: https://code.wildfiregames.com/D2099
Reviewed By: historic_bruno, wraitii
This was SVN commit r22511.

source/simulation2/helpers/Grid.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Wildfire Games.
+/* Copyright (C) 2019 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -190,8 +190,7 @@ class SparseGrid
 		size_t b = (j >> BucketBits) * m_BW + (i >> BucketBits);
 		if (!m_Data[b])
 		{
-			m_Data[b] = new T[BucketSize*BucketSize];
-			memset(m_Data[b], 0, BucketSize*BucketSize*sizeof(T));
+			m_Data[b] = new T[BucketSize*BucketSize]();
 		}
 		return m_Data[b];
 	}
@@ -204,8 +203,7 @@ public:
 		m_BW = (u16)((m_W + BucketSize-1) >> BucketBits);
 		m_BH = (u16)((m_H + BucketSize-1) >> BucketBits);
 
-		m_Data = new T*[m_BW*m_BH];
-		memset(m_Data, 0, m_BW*m_BH*sizeof(T*));
+		m_Data = new T*[m_BW*m_BH]();
 	}
 
 	~SparseGrid()
@@ -219,7 +217,7 @@ public:
 		for (size_t i = 0; i < (size_t)(m_BW*m_BH); ++i)
 			delete[] m_Data[i];
 
-		memset(m_Data, 0, m_BW*m_BH*sizeof(T*));
+		m_Data = new T*[m_BW*m_BH]();
 	}
 
 	void set(int i, int j, const T& value)

commit 2cc071f306eb66cba6320ba89055d202cdf89c47

Fix memory leak introduced by 3a839517ce in SparseGrid
Following 3a839517ce, SparseGrid::Reset() replaces the value of m_Data
(a C-style dynamically sized array) with a new value-initialised array,
by using the new operator. Previous code simply did a memset.
This means that when m_Data was not null, it leaked memory as the
previous array was not deleted.

SparseGrid::Reset is called when destroying the sparse grid, and a
SparseGrid is used by the long pathfinder when computing JPS paths. As
that is called rather often, it resulted in a relatively serious memory
leak that could make very long games use several GB of memory.

This fixes the leak by using placement new, which re-uses the memory,
effectively doing the same as the previously existing memset.

Profiling by historic_bruno and elexis showed that performance from
value-initialising with placement new was at worst similar to memset,
and testing shows this was compiled in a memset call in several cases
anyways.

Reviewed By: elexis
Fixes #5522

Differential Revision: https://code.wildfiregames.com/D2121
This was SVN commit r22545.

source/simulation2/helpers/Grid.h
@@ -217,7 +217,8 @@ public:
 		for (size_t i = 0; i < (size_t)(m_BW*m_BH); ++i)
 			delete[] m_Data[i];
 
-		m_Data = new T*[m_BW*m_BH]();
+		// Reset m_Data by value-constructing in place with placement new.
+		m_Data = new (m_Data) T*[m_BW*m_BH]();
 	}
 
 	void set(int i, int j, const T& value)