TaoJiaHengOne/0ad

commit 3698c134b5e3b4e6081e0ce50119a4198a8a9648

Make sure dropdowns fit in the screen by allowing the list to be above when there is not enough space
Reviewed By: Vladislav
Differential Revision: D1061
fixes #4857

This was SVN commit r21379.

source/gui/scripting/JSInterface_IGUIObject.cpp
@@ -156,6 +156,17 @@ bool JSI_IGUIObject::getProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 			break;
 		}
 
+		case GUIST_uint:
+		{
+			u32 value;
+			GUI<u32>::GetSetting(e, propName, value);
+			if (value >= std::numeric_limits<u32>::max())
+				LOGERROR("Integer overflow on converting to GUIST_uint");
+			else
+				vp.set(JS::Int32Value(value));
+			break;
+		}
+
 		case GUIST_float:
 		{
 			float value;
@@ -447,6 +458,19 @@ bool JSI_IGUIObject::setProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 		else
 		{
 			JS_ReportError(cx, "Cannot convert value to int");
+			return false;
+		}
+		break;
+	}
+
+	case GUIST_uint:
+	{
+		u32 value;
+		if (ScriptInterface::FromJSVal(cx, vp, value))
+			GUI<u32>::SetSetting(e, propName, value);
+		else
+		{
+			JS_ReportError(cx, "Cannot convert value to u32");
 			return false;
 		}
 		break;

binaries/data/mods/mod/gui/common/modern/styles.xml
@@ -77,6 +77,7 @@
 		sprite2_pressed="ModernDropDownArrowHighlight"
 		buffer_zone="8"
 		dropdown_size="225"
+		minimum_visible_items="3"
 		sprite_list="BlackBorderOnGray"
 		sprite_selectarea="ModernDarkBoxWhite"
 		textcolor_selected="white"

binaries/data/mods/mod/gui/gui.rnc
@@ -55,6 +55,7 @@ ex_settings =
   attribute clip { bool }?&
   attribute dropdown_size { xsd:decimal }?&
   attribute dropdown_buffer { xsd:decimal }?&
+  attribute minimum_visible_items { xsd:nonNegativeInteger }?&
   attribute enabled { bool }?&
   attribute font { text }?&
   attribute fov_wedge_color { ccolor }?&

binaries/data/mods/mod/gui/gui.rng
@@ -215,6 +215,11 @@
         </attribute>
       </optional>
       <optional>
+        <attribute name="minimum_visible_items">
+          <data type="nonNegativeInteger"/>
+        </attribute>
+      </optional>
+      <optional>
         <attribute name="enabled">
           <ref name="bool"/>
         </attribute>

source/gui/CDropDown.cpp
@@ -31,6 +31,7 @@ CDropDown::CDropDown()
 	AddSetting(GUIST_float,					"button_width");
 	AddSetting(GUIST_float,					"dropdown_size");
 	AddSetting(GUIST_float,					"dropdown_buffer");
+	AddSetting(GUIST_uint,					"minimum_visible_items");
 //	AddSetting(GUIST_CStrW,					"font");
 	AddSetting(GUIST_CStrW,					"sound_closed");
 	AddSetting(GUIST_CStrW,					"sound_disabled");
@@ -85,6 +86,7 @@ void CDropDown::HandleMessage(SGUIMessage& Message)
 			Message.value == "absolute" ||
 			Message.value == "dropdown_size" ||
 			Message.value == "dropdown_buffer" ||
+			Message.value == "minimum_visible_items" ||
 			Message.value == "scrollbar_style" ||
 			Message.value == "button_width")
 		{
@@ -389,23 +391,63 @@ InReaction CDropDown::ManuallyHandleEvent(const SDL_Event_* ev)
 
 void CDropDown::SetupListRect()
 {
-	float size, buffer;
+	extern int g_yres;
+	extern float g_GuiScale;
+	float size, buffer, yres;
+	yres = g_yres / g_GuiScale;
+	u32 minimumVisibleItems;
 	GUI<float>::GetSetting(this, "dropdown_size", size);
 	GUI<float>::GetSetting(this, "dropdown_buffer", buffer);
+	GUI<u32>::GetSetting(this, "minimum_visible_items", minimumVisibleItems);
 
-	if (m_ItemsYPositions.empty() || m_ItemsYPositions.back() > size)
+	if (m_ItemsYPositions.empty())
 	{
-		m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom+buffer,
-								 m_CachedActualSize.right, m_CachedActualSize.bottom+buffer + size);
+		m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom + buffer,
+		                         m_CachedActualSize.right, m_CachedActualSize.bottom + buffer + size);
+		m_HideScrollBar = false;
+	}
+	// Too many items so use a scrollbar
+	else if (m_ItemsYPositions.back() > size)
+	{
+		// Place items below if at least some items can be placed below
+		if (m_CachedActualSize.bottom + buffer + size <= yres)
+			m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom + buffer,
+			                         m_CachedActualSize.right, m_CachedActualSize.bottom + buffer + size);
+		else if ((m_ItemsYPositions.size() > minimumVisibleItems && yres - m_CachedActualSize.bottom - buffer >= m_ItemsYPositions[minimumVisibleItems]) ||
+		         m_CachedActualSize.top < yres - m_CachedActualSize.bottom)
+			m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom + buffer,
+			                         m_CachedActualSize.right, yres);
+		// Not enough space below, thus place items above
+		else
+			m_CachedListRect = CRect(m_CachedActualSize.left, std::max(0.f, m_CachedActualSize.top - buffer - size),
+			                         m_CachedActualSize.right, m_CachedActualSize.top - buffer);
 
 		m_HideScrollBar = false;
 	}
 	else
 	{
-		m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom+buffer,
-								 m_CachedActualSize.right, m_CachedActualSize.bottom+buffer + m_ItemsYPositions.back());
-
-		m_HideScrollBar = true;
+		// Enough space below, no scrollbar needed
+		if (m_CachedActualSize.bottom + buffer + m_ItemsYPositions.back() <= yres)
+		{
+			m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom + buffer,
+			                         m_CachedActualSize.right, m_CachedActualSize.bottom + buffer + m_ItemsYPositions.back());
+			m_HideScrollBar = true;
+		}
+		// Enough space below for some items, but not all, so place items below and use a scrollbar
+		else if ((m_ItemsYPositions.size() > minimumVisibleItems && yres - m_CachedActualSize.bottom - buffer >= m_ItemsYPositions[minimumVisibleItems]) ||
+		         m_CachedActualSize.top < yres - m_CachedActualSize.bottom)
+		{
+			m_CachedListRect = CRect(m_CachedActualSize.left, m_CachedActualSize.bottom + buffer,
+			                         m_CachedActualSize.right, yres);
+			m_HideScrollBar = false;
+		}
+		// Not enough space below, thus place items above. Hide the scrollbar accordingly
+		else
+		{
+			m_CachedListRect = CRect(m_CachedActualSize.left, std::max(0.f, m_CachedActualSize.top - buffer - m_ItemsYPositions.back()),
+			                         m_CachedActualSize.right, m_CachedActualSize.top - buffer);
+			m_HideScrollBar = m_CachedActualSize.top > m_ItemsYPositions.back() + buffer;
+		}
 	}
 }
 
@@ -421,9 +463,8 @@ bool CDropDown::MouseOver()
 
 	if (m_Open)
 	{
-		CRect rect(m_CachedActualSize.left, m_CachedActualSize.top,
-				   m_CachedActualSize.right, GetListRect().bottom);
-
+		CRect rect(m_CachedActualSize.left, std::min(m_CachedActualSize.top, GetListRect().top),
+		           m_CachedActualSize.right, std::max(m_CachedActualSize.bottom, GetListRect().bottom));
 		return rect.PointInside(GetMousePos());
 	}
 	else

source/gui/GUItypes.h
@@ -27,6 +27,7 @@ to handle every possible type.
 
 TYPE(bool)
 TYPE(int)
+TYPE(uint)
 TYPE(float)
 TYPE(CColor)
 TYPE(CClientArea)

source/gui/GUIutil.cpp
@@ -47,6 +47,13 @@ bool __ParseString<int>(const CStrW& Value, int& Output)
 }
 
 template <>
+bool __ParseString<u32>(const CStrW& Value, u32& Output)
+{
+	Output = Value.ToUInt();
+	return true;
+}
+
+template <>
 bool __ParseString<float>(const CStrW& Value, float& Output)
 {
 	Output = Value.ToFloat();

commit 636c71911098cd615739bc0e0c183e08b66b48f3

Use FromJSVal ToJSVal for primitive types to remove the duplicate logic and make JSInterface_IGUIObject case handling more equal.
Remove broken uint overflow check from 3698c134b5 and just use the
native JS uint32 conversion.

This was SVN commit r22540.

source/gui/scripting/GuiScriptConversions.cpp
@@ -134,7 +134,7 @@ template<> void ScriptInterface::ToJSVal<CGUIString>(JSContext* cx, JS::MutableH
 template<> bool ScriptInterface::FromJSVal<CGUIString>(JSContext* cx, JS::HandleValue v, CGUIString& out)
 {
 	std::wstring val;
-	if (!ScriptInterface::FromJSVal(cx, v, val))
+	if (!FromJSVal(cx, v, val))
 		return false;
 	out.SetValue(val);
 	return true;

source/gui/scripting/JSInterface_IGUIObject.cpp
@@ -138,9 +138,9 @@ bool JSI_IGUIObject::getProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 
 		case GUIST_int:
 		{
-			int value;
-			GUI<int>::GetSetting(e, propName, value);
-			vp.set(JS::Int32Value(value));
+			i32 value;
+			GUI<i32>::GetSetting(e, propName, value);
+			ScriptInterface::ToJSVal(cx, vp, value);
 			break;
 		}
 
@@ -148,10 +148,7 @@ bool JSI_IGUIObject::getProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 		{
 			u32 value;
 			GUI<u32>::GetSetting(e, propName, value);
-			if (value >= std::numeric_limits<u32>::max())
-				LOGERROR("Integer overflow on converting to GUIST_uint");
-			else
-				vp.set(JS::Int32Value(value));
+			ScriptInterface::ToJSVal(cx, vp, value);
 			break;
 		}
 
@@ -159,9 +156,8 @@ bool JSI_IGUIObject::getProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 		{
 			float value;
 			GUI<float>::GetSetting(e, propName, value);
-			// Create a garbage-collectable double
-			vp.set(JS::NumberValue(value));
-			return !vp.isNull();
+			ScriptInterface::ToJSVal(cx, vp, value);
+			break;
 		}
 
 		case GUIST_CColor:
@@ -205,7 +201,7 @@ bool JSI_IGUIObject::getProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 		{
 			CGUIString value;
 			GUI<CGUIString>::GetSetting(e, propName, value);
-			ScriptInterface::ToJSVal(cx, vp, value.GetOriginalString());
+			ScriptInterface::ToJSVal(cx, vp, value);
 			break;
 		}
 
@@ -227,7 +223,7 @@ bool JSI_IGUIObject::getProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 
 		case GUIST_CGUISpriteInstance:
 		{
-			CGUISpriteInstance *value;
+			CGUISpriteInstance* value;
 			GUI<CGUISpriteInstance>::GetSettingPointer(e, propName, value);
 			ScriptInterface::ToJSVal(cx, vp, value->GetName());
 			break;
@@ -346,7 +342,7 @@ bool JSI_IGUIObject::setProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 	{
 	case GUIST_CStr:
 	{
-		std::string value;
+		CStr value;
 		if (!ScriptInterface::FromJSVal(cx, vp, value))
 			return false;
 
@@ -356,7 +352,7 @@ bool JSI_IGUIObject::setProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 
 	case GUIST_CStrW:
 	{
-		std::wstring value;
+		CStrW value;
 		if (!ScriptInterface::FromJSVal(cx, vp, value))
 			return false;
 
@@ -376,13 +372,11 @@ bool JSI_IGUIObject::setProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 
 	case GUIST_CGUIString:
 	{
-		std::wstring value;
+		CGUIString value;
 		if (!ScriptInterface::FromJSVal(cx, vp, value))
 			return false;
 
-		CGUIString str;
-		str.SetValue(value);
-		GUI<CGUIString>::SetSetting(e, propName, str);
+		GUI<CGUIString>::SetSetting(e, propName, value);
 		break;
 	}
 
@@ -452,9 +446,9 @@ bool JSI_IGUIObject::setProperty(JSContext* cx, JS::HandleObject obj, JS::Handle
 
 	case GUIST_float:
 	{
-		double value;
-		if (JS::ToNumber(cx, vp, &value) == true)
-			GUI<float>::SetSetting(e, propName, (float)value);
+		float value;
+		if (ScriptInterface::FromJSVal(cx, vp, value))
+			GUI<float>::SetSetting(e, propName, value);
 		else
 		{
 			JS_ReportError(cx, "Cannot convert value to float");