TaoJiaHengOne/0ad

commit b807c39e6a48930c2ab02c357f6bf41e014b0ffe

Restore previous formation behaviour of targeting nearby enemies when idle / aborting orders
This restores A23 formation behaviour, changed by 4ca448a686. Units will
now start attacking nearby enemies when the formation goes idle (such as
when the abort order button is clicked).

This introduces a change that formation units that actually go idle will
try to move back in formation shape, so it looks more like they're still
in formation.

Fixes #5519.

Differential Revision: https://code.wildfiregames.com/D2097
This was SVN commit r22506.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1266,13 +1266,11 @@ UnitAI.prototype.UnitFsmSpec = {
 			this.SetDefaultAnimationVariant();
 		},
 
-		"IDLE": {
-			// Formation members do nothing while Idle, but we need the state
-			// so that they keep the formation variant.
-		},
+		"IDLE": "INDIVIDUAL.IDLE",
 
 		"WALKING": {
 			"enter": function() {
+				this.formationOffset = { "x": this.order.data.x, "z": this.order.data.z };
 				let cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
 				cmpUnitMotion.MoveToFormationOffset(this.order.data.target, this.order.data.x, this.order.data.z);
 			},
@@ -1402,12 +1400,6 @@ UnitAI.prototype.UnitFsmSpec = {
 				// get stuck with an incorrect animation
 				this.SelectAnimation("idle");
 
-				if (this.formationController)
-				{
-					this.SetNextState("FORMATIONMEMBER.IDLE");
-					return true;
-				}
-
 				// Idle is the default state. If units try, from the IDLE.enter sub-state, to
 				// begin another order, and that order fails (calling FinishOrder), they might
 				// end up in an infinite loop. To avoid this, all methods that could put the unit in
@@ -1467,6 +1459,16 @@ UnitAI.prototype.UnitFsmSpec = {
 				// (If anyone approaches later, it'll be handled via LosRangeUpdate.)
 				if (this.FindNewTargets())
 					return; // (abort the FSM transition since we may have already switched state)
+
+				if (this.formationOffset && this.formationController)
+				{
+					this.PushOrder("FormationWalk", {
+						"target": this.formationController,
+						"x": this.formationOffset.x,
+						"z": this.formationOffset.z,
+					});
+					return;
+				}
 
 				if (!this.isIdle)
 				{

commit 86acba17f0a5c12d56f43a032a3b09fd2d55a2b0

Fix formation-walking infinite loop introduced in b807c39e6a
b807c39e6a Introduced a infinite loop in which formation members keep
receiving the order to formation walk to their proper position. This was
due to this.formationOffset is not deleted when member gets in offset
position, what means he recieved motion update and it was not because of
failure.

Patch by: @Freagarach
Differential Revision: https://code.wildfiregames.com/D2424
This was SVN commit r23217.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -1332,6 +1332,8 @@ UnitAI.prototype.UnitFsmSpec = {
 				let cmpFormation = Engine.QueryInterface(this.formationController, IID_Formation);
 				if (cmpFormation)
 					cmpFormation.SetInPosition(this.entity);
+
+				delete this.formationOffset;
 			},
 		},
 