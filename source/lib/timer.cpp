TaoJiaHengOne/0ad

commit 953d2621dd7a7622933923d70fb46d0f44636563

timer: improved timer_add_client - no longer has fixed-size storage. btw, name will change here - not happy with SUM_TIMER.
GUI/input code: moved g_active, g_keys etc. into ps/Globals; it has its
own input handler as well

all users of fopen(): add file_make_full_native_path to get correct path
even though current dir = system/

This was SVN commit r2954.

source/lib/timer.cpp
@@ -242,40 +242,30 @@ void calc_fps()
 // this supplements in-game profiling by providing low-overhead,
 // high resolution time accounting.
 
-struct TimerClient
-{
-	double sum;	// total bill [s]
-
-	// only store a pointer for efficiency.
-	const char* name;
-};
 
 // use static storage to ensure clients can be added at any time,
 // especially before the heap has been initialized.
-// that means there's a fixed limit, but that's not a problem since
-// these timers are added manually for performance measuring.
 static uint num_clients;
-static const uint MAX_CLIENTS = 16;
-static TimerClient clients[MAX_CLIENTS];
+static TimerClient* clients;
 
 
 // allocate a new TimerClient whose total (added to by timer_bill_client)
 // will be displayed by timer_display_client_totals.
 // notes:
-// - uses static data; there is a fixed limit. rationale: see clients[].
 // - may be called at any time;
+// - always succeeds (there's no fixed limit);
 // - free() is not needed nor possible.
-// - name must remain valid until exit; passing a string literal is safest.
-TimerClient* timer_add_client(const char* name)
+// - description must remain valid until exit; a string literal is safest.
+TimerClient* timer_add_client(TimerClient* tc, const char* description)
 {
-	if(num_clients == MAX_CLIENTS)
-	{
-		debug_warn("timer: increase MAX_CLIENTS");
-		return 0;
-	}
-	TimerClient* tc = &clients[num_clients++];
-	tc->name = name;
-	// note: tc->sum is already 0 (static data)
+	tc->sum = 0.0;
+	tc->description = description;
+
+	// insert at front of list
+	tc->next = clients;
+	clients = tc;
+	num_clients++;
+
 	return tc;
 }
 
@@ -293,9 +283,16 @@ void timer_display_client_totals()
 {
 	debug_printf("TIMER TOTALS (%d clients)\n", num_clients);
 	debug_printf("-----------------------------------------------------\n");
-	for(uint i = 0; i < num_clients; i++)
+
+	while(clients)
 	{
-		const double sum = clients[i].sum;
+		// (make sure list and count are consistent)
+		debug_assert(num_clients != 0);
+		TimerClient* tc = clients;
+		clients = tc->next;
+		num_clients--;
+
+		const double sum = tc->sum;
 		// determine scale factor for pretty display
 		double scale = 1e6;
 		const char* unit = "us";
@@ -304,7 +301,8 @@ void timer_display_client_totals()
 		else if(sum > 1e-3)
 			scale = 1e3, unit = "ms";
 
-		debug_printf("  %s: %g %s\n", clients[i].name, sum*scale, unit);
+		debug_printf("  %s: %g %s\n", tc->description, sum*scale, unit);
 	}
+
 	debug_printf("-----------------------------------------------------\n");
 }

source/graphics/TextureEntry.cpp
@@ -77,7 +77,7 @@ void CTextureEntry::LoadTexture()
 	}
 }
 
-static TimerClient* tc_mipmap_basecolor = timer_add_client("mipmap BuildBaseColor");
+TIMER_ADD_CLIENT(tc_mipmap_basecolor);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // BuildBaseColor: calculate the root colour of the texture, used for coloring minimap, and store

source/gui/CGUI.cpp
@@ -38,6 +38,7 @@ gee@pyro.nu
 
 #include "scripting/ScriptingHost.h"
 #include "Hotkey.h"
+#include "ps/Globals.h"
 
 // namespaces used
 using namespace std;
@@ -53,9 +54,6 @@ JSClass GUIClass = {
 	JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub,
 };
 
-// Globals used.
-extern int g_xres, g_yres;
-extern bool g_keys[];
 
 //-------------------------------------------------------------------
 //	called from main loop when (input) events are received.

source/gui/CInput.cpp
@@ -16,12 +16,10 @@ gee@pyro.nu
 #include "lib/res/graphics/unifont.h"
 
 #include "ps/Hotkey.h"
-
 #include "ps/CLogger.h"
-#define LOG_CATEGORY "gui"
+#include "ps/Globals.h"
 
-extern bool g_keys[];
-extern bool g_mouse_buttons[];
+#define LOG_CATEGORY "gui"
 
 using namespace std;
 

source/gui/MiniMap.cpp
@@ -16,11 +16,10 @@
 #include "Terrain.h"
 #include "Profile.h"
 #include "LOSManager.h"
+#include "ps/Globals.h"
 
 
 
-extern bool g_mouse_buttons[5];
-extern int g_mouse_x, g_mouse_y;
 extern float g_MaxZoomHeight, g_YMinOffset;
 bool HasClicked=false;
 

source/lib/debug.cpp
@@ -24,10 +24,12 @@
 #include "debug.h"
 #include "debug_stl.h"
 #include "posix.h"
+// some functions here are called from within mmgr; disable its hooks
+// so that our allocations don't cause infinite recursion.
 #include "nommgr.h"
-	// some functions here are called from within mmgr; disable its hooks
-	// so that our allocations don't cause infinite recursion.
 #include "self_test.h"
+// file_make_full_native_path is needed when bundling game data files.
+#include "lib/res/file/file.h"
 
 // needed when writing crashlog
 static const size_t LOG_CHARS = 16384;
@@ -84,7 +86,7 @@ static void cat_atow(FILE* out, const char* in_filename)
 
 	while(!feof(in))
 	{
-		size_t bytes_read = fread(buf, 1,buf_size, in);
+		size_t bytes_read = fread(buf, 1, buf_size, in);
 		if(!bytes_read)
 			break;
 		buf[bytes_read] = 0;	// 0-terminate
@@ -109,15 +111,15 @@ int debug_write_crashlog(const wchar_t* text)
 	fwprintf(f, L"%ls\n", text);
 	WRITE_DIVIDER
 
-
 	// for user convenience, bundle all logs into this file:
-
+	char N_path[PATH_MAX];
 	fwprintf(f, L"System info:\n\n");
-	cat_atow(f, "../logs/system_info.txt");
+	(void)file_make_full_native_path("../logs/system_info.txt", N_path);
+	cat_atow(f, N_path);
 	WRITE_DIVIDER
-
 	fwprintf(f, L"Main log:\n\n");
-	cat_atow(f, "../logs/mainlog.html");
+	(void)file_make_full_native_path("../logs/mainlog.html", N_path);
+	cat_atow(f, N_path);
 	WRITE_DIVIDER
 
 	fwprintf(f, L"Last known activity:\n\n %ls\n", debug_log);

source/lib/res/file/file.cpp
@@ -26,7 +26,6 @@
 #include "adts.h"
 #include "sysdep/sysdep.h"
 #include "byte_order.h"
-#include "timer.h"
 #include "lib/allocators.h"
 
 #include <vector>
@@ -431,8 +430,6 @@ static bool dirent_less(const DirEnt* d1, const DirEnt* d2)
 }
 
 
-static TimerClient* tc_file_enum_malloc = timer_add_client("file_enum_malloc");
-
 // call <cb> for each file and subdirectory in <dir> (alphabetical order),
 // passing the entry name (not full path!), stat info, and <user>.
 //
@@ -476,9 +473,7 @@ int file_enum(const char* P_path, const FileCB cb, const uintptr_t user)
 			stat_err = ret;
 
 		const size_t size = sizeof(DirEnt)+strlen(ent.name)+1;
-		DirEnt* p_ent;
-		{SUM_TIMER(tc_file_enum_malloc);
-		p_ent = (DirEnt*)malloc(size);}
+		DirEnt* p_ent = (DirEnt*)malloc(size);
 		if(!p_ent)
 		{
 			stat_err = ERR_NO_MEM;

source/lib/res/file/vfs.cpp
@@ -279,7 +279,9 @@ static void file_listing_add(const char* v_fn)
 
 	if(!file_list)
 	{
-		file_list = fopen("../logs/filelist.txt", "w");
+		char N_path[PATH_MAX];
+		(void)file_make_full_native_path("../logs/filelist.txt", N_path);
+		file_list = fopen(N_path, "w");
 		if(!file_list)
 			return;
 	}

commit b2665437fce55bcb5279c34474fa9315c92c7392

Allow calling timer_DisplayClientTotals multiple times.
According to the `timer_DisplayClientTotals` comment 953d2621dd broke
correctness of `clients` after the function call.

Patch By: echotangoecho
Reviewed By: wraitii
Differential Revision: https://code.wildfiregames.com/D812
This was SVN commit r23240.

source/lib/timer.cpp
@@ -183,14 +183,8 @@ void timer_DisplayClientTotals()
 	debug_printf("TIMER TOTALS (%lu clients)\n", (unsigned long)numClients);
 	debug_printf("-----------------------------------------------------\n");
 
-	while(clients)
+	for(TimerClient* tc = clients; tc; tc = tc->next)
 	{
-		// (make sure list and count are consistent)
-		ENSURE(numClients != 0);
-		TimerClient* tc = clients;
-		clients = tc->next;
-		numClients--;
-
 		const std::string duration = tc->sum.ToString();
 		debug_printf("  %s: %s (%lux)\n", utf8_from_wstring(tc->description).c_str(), duration.c_str(), (unsigned long)tc->num_calls);
 	}