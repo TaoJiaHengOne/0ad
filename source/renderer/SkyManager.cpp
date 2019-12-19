TaoJiaHengOne/0ad

commit 779a33ee30c3b9654393b899bad40296b9a71ff1

Water and waterfall shaders, materials and examples.
This was SVN commit r12643.

source/renderer/SkyManager.cpp
@@ -67,6 +67,8 @@ SkyManager::SkyManager()
 	m_SkySet = L"";
 
 	m_HorizonHeight = -150.0f;
+	
+	m_SkyCubeMap = 0;
 }
 
 
@@ -84,6 +86,77 @@ void SkyManager::LoadSkyTextures()
 		texture->Prefetch();
 		m_SkyTexture[i] = texture;
 	}
+	
+	glGenTextures(1, &m_SkyCubeMap);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, m_SkyCubeMap);
+	
+	int types[] = {
+		GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Y
+	};
+	
+	const wchar_t* images[numTextures+1] = {
+		L"front",
+		L"back",
+		L"right",
+		L"left",
+		L"top",
+		L"top"
+	};
+	
+	for (size_t i = 0; i < numTextures+1; ++i)
+	{
+		VfsPath path = VfsPath("art/textures/skies") / m_SkySet / (Path::String(images[i])+L".dds");
+		
+		shared_ptr<u8> file;
+		size_t fileSize;
+		g_VFS->LoadFile(path, file, fileSize);
+		
+		Tex tex;
+		tex_decode(file, fileSize, &tex);
+		
+		tex_transform_to(&tex, (tex.flags | TEX_BOTTOM_UP | TEX_ALPHA) & ~(TEX_DXT | TEX_MIPMAPS));
+		
+		u8* data = tex_get_data(&tex);
+		
+		if (types[i] == GL_TEXTURE_CUBE_MAP_NEGATIVE_Y || types[i] == GL_TEXTURE_CUBE_MAP_POSITIVE_Y)
+		{
+			std::vector<u8> rotated(tex.dataSize);
+		
+			for (size_t y = 0; y < tex.h; ++y)
+			{
+				for (size_t x = 0; x < tex.w; ++x)
+				{
+					size_t invx = y, invy = tex.w-x-1;
+					
+					rotated[(y*tex.w + x) * 4 + 0] = data[(invy*tex.w + invx) * 4 + 0];
+					rotated[(y*tex.w + x) * 4 + 1] = data[(invy*tex.w + invx) * 4 + 1];
+					rotated[(y*tex.w + x) * 4 + 2] = data[(invy*tex.w + invx) * 4 + 2];
+					rotated[(y*tex.w + x) * 4 + 3] = data[(invy*tex.w + invx) * 4 + 3];
+				}
+			}
+			
+			glTexImage2D(types[i], 0, GL_RGB, tex.w, tex.h, 0, GL_RGBA, GL_UNSIGNED_BYTE, &rotated[0]);
+		}
+		else
+		{
+			glTexImage2D(types[i], 0, GL_RGB, tex.w, tex.h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		}
+		
+		tex_free(&tex);
+	}
+	
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 
@@ -93,6 +166,13 @@ void SkyManager::SetSkySet( const CStrW& newSet )
 {
 	if(newSet == m_SkySet)
 		return;
+	
+	if (m_SkyCubeMap)
+	{
+		glDeleteTextures(1, &m_SkyCubeMap);
+		m_SkyCubeMap = 0;
+	}
+	
 	m_SkySet = newSet;
 
 	LoadSkyTextures();

binaries/data/mods/public/art/actors/temp/reservoir.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<actor version="1">
+	
+	<castshadow/>
+	
+	<group>
+		<variant frequency="100" name="Base">
+			<animations/>
+			<mesh>test/reservoir.dae</mesh>
+			<props>
+				<prop actor="temp/reservoir_water.xml" attachpoint="root"/>
+			</props>
+			<textures>
+				<texture file="structural/rome_struct.png" name="baseTex"/>
+			</textures>
+		</variant>
+	</group>
+	
+</actor>

binaries/data/mods/public/art/actors/temp/reservoir_water.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<actor version="1">
+	
+	<group>
+		<variant frequency="100" name="Base">
+			<animations/>
+			<mesh>test/reservoir-water.dae</mesh>
+			<props/>
+			<textures>
+				<texture file="structural/rome_struct.png" name="baseTex"/>
+			</textures>
+		</variant>
+	</group>
+	<material>player_water.xml</material>
+	
+</actor>

binaries/data/mods/public/art/actors/temp/water_plane.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<actor version="1">
+	
+	<group>
+		<variant frequency="100" name="water_plane">
+			<animations/>
+			<mesh>test/waterplane.dae</mesh>
+			<props/>
+			<textures>
+				<texture file="structural/rome_struct.png" name="baseTex"/>
+			</textures>
+		</variant>
+	</group>
+	<material>player_water.xml</material>
+	
+</actor>

binaries/data/mods/public/art/actors/temp/waterfall.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<actor version="1">
+	
+	<group>
+		<variant frequency="100" name="waterfall">
+			<animations/>
+			<mesh>test/waterfall.dae</mesh>
+			<props/>
+			<textures>
+				<texture file="temp/waterfall.png" name="baseTex"/>
+			</textures>
+		</variant>
+	</group>
+	<material>waterfall.xml</material>
+	
+</actor>

binaries/data/mods/public/art/materials/basic_trans_wind.xml
@@ -3,7 +3,7 @@
     <shader effect="model_transparent"/>
     <alpha_blending/>
     <define name="USE_TRANSPARENT" value="1"/>
-    <renderquery name="time"/>
+    <renderquery name="sim_time"/>
     <define name="USE_WIND" value="1"/>
     <uniform name="windData" value="1.0 1.0"/>
 </material>

binaries/data/mods/public/art/materials/player_water.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="utf-8"?>
+<material>
+    <shader effect="model_water"/>
+    <renderquery name="water_tex"/>
+    <renderquery name="sky_cube"/>
+    <renderquery name="sim_time"/>
+    <uniform name="translation" value="0.05 -0.05"/>
+    
+    <uniform name="shininess" value="32.0"/>
+    <uniform name="specularStrength" value="1.5"/>
+    <uniform name="waviness" value="0.5"/>
+    <uniform name="waterTint" value="0.2 0.3 0.2"/>
+    <uniform name="murkiness" value="0.6"/>
+    <uniform name="reflectionTint" value="0.3 0.4 0.3"/>
+    <uniform name="reflectionTintStrength" value="0.1"/>
+</material>

binaries/data/mods/public/art/materials/trans_wind.xml
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <material>
     <shader effect="model"/>
-    <renderquery name="time"/>
+    <renderquery name="sim_time"/>
     <define name="USE_WIND" value="1"/>
+    <uniform name="windData" value="1.0 1.0"/>
 </material>

commit e1cf1e16b924bce044f5d2114e3ed299d92afc6e

Unbind the correct texture slot in SkyManager
Wrong binding pair was introduced in 779a33ee30.

Patch By: linkmauve
Differential Revision: https://code.wildfiregames.com/D2487
This was SVN commit r23263.

source/renderer/SkyManager.cpp
@@ -143,7 +143,7 @@ void SkyManager::LoadSkyTextures()
 #else
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 #endif
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
 	///////////////////////////////////////////////////////////////////////////
 }
 