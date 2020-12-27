TaoJiaHengOne/0ad

commit b2842e8021a19cbf8f710bbde68556f890ae04eb

Rewrite Structure Tree and Template Viewer to use OOP principles
Commenters: elexis, Stan, Angen
Refs.: #5387
Differential Revision: https://code.wildfiregames.com/D2734
This was SVN commit r23808.

binaries/data/mods/public/gui/reference/structree/Boxes/ProductionRowManager.js
@@ -1,0 +1,79 @@
+class ProductionRowManager
+{
+	constructor(page, guiName, sortByPhase)
+	{
+		this.page = page;
+		this.width = 0;
+		this.sortProductionsByPhase = sortByPhase;
+
+		this.productionRows = [];
+		for (let row of Engine.GetGUIObjectByName(guiName).children)
+			this.productionRows.push(new ProductionRow(this.page, row, this.productionRows.length));
+	}
+
+	draw(template, civCode, phaseIdx=0)
+	{
+		this.width = 0;
+
+		if (this.sortProductionsByPhase)
+			for (let r = 0; r < this.page.TemplateParser.phaseList.length; ++r)
+				this.productionRows[r].startDraw(this.page.TemplateParser.phaseList.length - phaseIdx);
+		else
+			this.productionRows[0].startDraw(1);
+
+		// (Want to draw Units before Techs)
+		for (let prodType of Object.keys(template.production).reverse())
+			for (let prod of template.production[prodType])
+			{
+				let pIdx = 0;
+				switch (prodType)
+				{
+
+				case "units":
+					prod = this.page.TemplateParser.getEntity(prod, civCode);
+					pIdx = this.page.TemplateParser.phaseList.indexOf(prod.phase);
+					break;
+
+				case "techs":
+					pIdx = this.page.TemplateParser.phaseList.indexOf(this.page.TemplateParser.getPhaseOfTechnology(prod, civCode));
+					prod = clone(this.page.TemplateParser.getTechnology(prod, civCode));
+					for (let res in template.techCostMultiplier)
+						if (prod.cost[res])
+							prod.cost[res] *= template.techCostMultiplier[res];
+					break;
+
+				default:
+					continue;
+				}
+
+				let rowIdx = this.sortProductionsByPhase ? Math.max(0, pIdx - phaseIdx) : 0;
+				this.productionRows[rowIdx].drawIcon(prod, civCode)
+			}
+
+		if (template.upgrades)
+			for (let upgrade of template.upgrades)
+			{
+				let pIdx = 0;
+				if (this.phaseSort)
+					pIdx = this.page.TemplateParser.phaseList.indexOf(upgrade.phase);
+				let rowIdx = Math.max(0, pIdx - phaseIdx);
+				this.productionRows[rowIdx].drawIcon(upgrade, civCode);
+			}
+
+		if (template.wallset)
+			this.productionRows[0].drawIcon(template.wallset.tower, civCode);
+
+		let r = 0;
+
+		// Tell the production rows used we've finished
+		if (this.sortProductionsByPhase)
+			for (; r < this.page.TemplateParser.phaseList.length; ++r)
+				this.width = Math.max(this.width, this.productionRows[r].finishDraw());
+		else
+			this.width = this.productionRows[r++].finishDraw();
+
+		// Hide any remaining phase rows
+		for (; r < this.productionRows.length; ++r)
+			this.productionRows[r].hide();
+	}
+}

binaries/data/mods/public/gui/common/tooltips.js
@@ -621,6 +621,26 @@ function getGatherTooltip(template)
 	});
 }
 
+/**
+ * Returns the resources this entity supplies in the specified entity's tooltip
+ */
+function getResourceSupplyTooltip(template)
+{
+	if (!template.supply)
+		return "";
+
+	let supply = template.supply;
+	let type = supply.type[0] == "treasure" ? supply.type[1] : supply.type[0];
+
+	// Translation: Label in tooltip showing the resource type and quantity of a given resource supply.
+	return sprintf(translate("%(label)s %(component)s %(amount)s"), {
+		"label": headerFont(translate("Resource Supply:")),
+		"component": resourceIcon(type),
+		// Translation: Marks that a resource supply entity has an unending, infinite, supply of its resource.
+		"amount": Number.isFinite(+supply.amount) ? supply.amount : translate("∞")
+	});
+}
+
 function getResourceTrickleTooltip(template)
 {
 	if (!template.resourceTrickle)

binaries/data/mods/public/gui/reference/common/Buttons/CivInfoButton.js
@@ -1,0 +1,27 @@
+class CivInfoButton
+{
+	constructor(parentPage)
+	{
+		this.parentPage = parentPage;
+
+		this.civInfoButton = Engine.GetGUIObjectByName("civInfoButton");
+		this.civInfoButton.onPress = this.onPress.bind(this);
+		this.civInfoButton.caption = this.Caption;
+		this.civInfoButton.tooltip = colorizeHotkey(this.Tooltip, this.Hotkey);
+	}
+
+	onPress()
+	{
+		Engine.PopGuiPage({ "civ": this.parentPage.activeCiv, "nextPage": "page_civinfo.xml" });
+	}
+
+}
+
+CivInfoButton.prototype.Caption =
+	translate("Civilization Overview");
+
+CivInfoButton.prototype.Hotkey =
+	"civinfo";
+
+CivInfoButton.prototype.Tooltip =
+	translate("%(hotkey)s: Switch to Civilization Overview.");

binaries/data/mods/public/gui/reference/common/Buttons/CivInfoButton.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<object
+	name="civInfoButton"
+	type="button"
+	style="StoneButton"
+	size="100%-421 100%-44 100%-221 100%-16"
+	hotkey="civinfo"
+/>

binaries/data/mods/public/gui/reference/common/Buttons/CloseButton.js
@@ -1,0 +1,16 @@
+class CloseButton
+{
+	constructor(parentPage)
+	{
+		this.closeButton = Engine.GetGUIObjectByName("closeButton");
+		this.closeButton.onPress = parentPage.closePage.bind(parentPage);
+		this.closeButton.caption = this.Caption;
+		this.closeButton.tooltip = colorizeHotkey(parentPage.CloseButtonTooltip, this.Hotkey);
+	}
+}
+
+CloseButton.prototype.Caption =
+	translate("Close");
+
+CloseButton.prototype.Hotkey =
+	"close";

binaries/data/mods/public/gui/reference/common/Buttons/CloseButton.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<object
+	name="closeButton"
+	type="button"
+	style="StoneButton"
+	size="100%-216 100%-44 100%-16 100%-16"
+	hotkey="cancel"
+/>

binaries/data/mods/public/gui/reference/common/Dropdowns/CivSelectDropdown.js
@@ -1,0 +1,63 @@
+class CivSelectDropdown
+{
+	constructor(civData)
+	{
+		this.handlers = new Set();
+
+		let civList = Object.keys(civData).map(civ => ({
+			"name": civData[civ].Name,
+			"code": civ,
+		})).sort(sortNameIgnoreCase);
+
+		this.civSelectionHeading = Engine.GetGUIObjectByName("civSelectionHeading");
+		this.civSelectionHeading.caption = this.Caption;
+
+		this.civSelection = Engine.GetGUIObjectByName("civSelection");
+		this.civSelection.list = civList.map(c => c.name);
+		this.civSelection.list_data = civList.map(c => c.code);
+		this.civSelection.onSelectionChange = () => this.onSelectionChange(this);
+	}
+
+	onSelectionChange()
+	{
+		let civCode = this.civSelection.list_data[this.civSelection.selected];
+
+		for (let handler of this.handlers)
+			handler(civCode);
+	}
+
+	registerHandler(handler)
+	{
+		this.handlers.add(handler);
+	}
+
+	unregisterHandler(handler)
+	{
+		this.handlers.delete(handler);
+	}
+
+	hasCivs()
+	{
+		return this.civSelection.list.length != 0;
+	}
+
+	selectCiv(civCode)
+	{
+		if (!civCode)
+			return;
+
+		let index = this.civSelection.list_data.indexOf(civCode);
+		if (index == -1)
+			return;
+
+		this.civSelection.selected = index;
+	}
+
+	selectFirstCiv()
+	{
+		this.civSelection.selected = 0;
+	}
+}
+
+CivSelectDropdown.prototype.Caption =
+	translate("Civilization:");

binaries/data/mods/public/gui/reference/common/Dropdowns/CivSelectDropdown.xml
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<object size="16 10 100%-16 30">
+	<object
+		name="civSelectionHeading"
+		type="text"
+		font="sans-bold-16"
+		textcolor="white"
+		text_align="right"
+		size="0 10 100%-188 48"
+	/>
+	<object name="civSelection" type="dropdown" style="ModernDropDown" size="100%-180 8 100% 34" dropdown_size="424"/>
+</object>

commit 1432343eeb73fe5a513ea01ba366daab3d3d43df

Fix upgrades not being sorted by phase in structuretree
Currenlty there is regression in structuretree from b2842e8021, when
upgrade for sentry tower is not in the correct phase row.
Thix is fixing that.
Probably typo in condition.

Differential Revision: D3252
Reviewed by: bb
This was SVN commit r24461.

binaries/data/mods/public/gui/reference/structree/Boxes/ProductionRowManager.js
@@ -54,7 +54,7 @@ class ProductionRowManager
 			for (let upgrade of template.upgrades)
 			{
 				let pIdx = 0;
-				if (this.phaseSort)
+				if (this.sortProductionsByPhase)
 					pIdx = this.page.TemplateParser.phaseList.indexOf(upgrade.phase);
 				let rowIdx = Math.max(0, pIdx - phaseIdx);
 				this.productionRows[rowIdx].drawIcon(upgrade, civCode);