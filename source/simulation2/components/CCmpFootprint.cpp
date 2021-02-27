TaoJiaHengOne/0ad

commit 52fcee9c91817fc187cbc2d489f9439bb2bce581

Fix division by 0 error in PickSpawnPoint
The spawn code should not assume that obstructions will never be
0-sized.

Reported by: Zack
Fixes #6039

Differential Revision: https://code.wildfiregames.com/D3583
This was SVN commit r24945.

source/simulation2/components/CCmpFootprint.cpp
@@ -170,6 +170,9 @@ public:
 		if (cmpSpawnedObstruction)
 		{
 			spawnedRadius = cmpSpawnedObstruction->GetSize();
+			// Force a positive radius to avoid division by zero errors.
+			if (spawnedRadius == 0)
+				spawnedRadius = 1;
 			spawnedTag = cmpSpawnedObstruction->GetObstruction();
 		}
 

commit 20b1d98a84bd4f18d533b8678aa6039bc5ac175b

Fix compilation following 52fcee9c91
52fcee9c91 broke compilation because spawnedRadius is not an integer.

This was SVN commit r24948.

source/simulation2/components/CCmpFootprint.cpp
@@ -163,7 +163,7 @@ public:
 			return error;
 
 		// If no spawned obstruction, use a positive radius to avoid division by zero errors.
-		entity_pos_t spawnedRadius = fixed::FromInt(1);
+		entity_pos_t spawnedRadius = entity_pos_t::FromInt(1);
 		ICmpObstructionManager::tag_t spawnedTag;
 
 		CmpPtr<ICmpObstruction> cmpSpawnedObstruction(GetSimContext(), spawned);
@@ -171,8 +171,8 @@ public:
 		{
 			spawnedRadius = cmpSpawnedObstruction->GetSize();
 			// Force a positive radius to avoid division by zero errors.
-			if (spawnedRadius == 0)
-				spawnedRadius = 1;
+			if (spawnedRadius == entity_pos_t::Zero())
+				spawnedRadius = entity_pos_t::FromInt(1);
 			spawnedTag = cmpSpawnedObstruction->GetObstruction();
 		}
 