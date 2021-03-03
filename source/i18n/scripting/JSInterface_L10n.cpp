TaoJiaHengOne/0ad

commit cb346e207b288ba319aec8d929591eb9ff2ce9e0

Refactor all usage of RegisterFunction to ScriptFunction::Register
- Replace ScriptInterface::RegisterFunction with
ScriptFunction::Register
 - Mostly removing unused cmpPrivate*
 - Some usage introduces specific getters (mapgenerator, AIWorker,
XmppClient,...)
 - Several passthrough functions are simply removed in favour of calling
the original, reducing duplication
 - Make use of ScriptRequest/ScriptInterface capabilities where
relevant.
- Make JSI_* headers only expose necessary functions, lightening them
considerably and reducing duplication
 - Reuse namespaces in JSI_* implementations directly, reducing visual
noise there

Follows f3aedf88a6

Differential Revision: https://code.wildfiregames.com/D3626
This was SVN commit r24983.

source/i18n/scripting/JSInterface_L10n.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -21,169 +21,81 @@
 
 #include "i18n/L10n.h"
 #include "lib/utf8.h"
+#include "scriptinterface/FunctionWrapper.h"
 #include "scriptinterface/ScriptInterface.h"
 
-// Returns a translation of the specified English string into the current language.
-std::wstring JSI_L10n::Translate(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::wstring& sourceString)
+namespace JSI_L10n
 {
-	return wstring_from_utf8(g_L10n.Translate(utf8_from_wstring(sourceString)));
+L10n* L10nGetter(const ScriptRequest&, JS::CallArgs&)
+{
+	if (!g_L10n.IsInitialised())
+	{
+		LOGERROR("Trying to access g_L10n when it's not initialized!");
+		return nullptr;
+	}
+	return &g_L10n.GetSingleton();
 }
 
-// Returns a translation of the specified English string, for the specified context.
-std::wstring JSI_L10n::TranslateWithContext(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& context, const std::wstring& sourceString)
+std::vector<std::string> TranslateArray(const std::vector<std::string>& sourceArray)
 {
-	return wstring_from_utf8(g_L10n.TranslateWithContext(context, utf8_from_wstring(sourceString)));
-}
-
-// Return a translated version of the given strings (singular and plural) depending on an integer value.
-std::wstring JSI_L10n::TranslatePlural(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::wstring& singularSourceString, const std::wstring& pluralSourceString, int number)
-{
-	return wstring_from_utf8(g_L10n.TranslatePlural(utf8_from_wstring(singularSourceString), utf8_from_wstring(pluralSourceString), number));
-}
-
-// Return a translated version of the given strings (singular and plural) depending on an integer value, for the specified context.
-std::wstring JSI_L10n::TranslatePluralWithContext(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& context, const std::wstring& singularSourceString, const std::wstring& pluralSourceString, int number)
-{
-	return wstring_from_utf8(g_L10n.TranslatePluralWithContext(context, utf8_from_wstring(singularSourceString), utf8_from_wstring(pluralSourceString), number));
-}
-
-// Return a translated version of the given string, localizing it line by line.
-std::wstring JSI_L10n::TranslateLines(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::wstring& sourceString)
-{
-	return wstring_from_utf8(g_L10n.TranslateLines(utf8_from_wstring(sourceString)));
-}
-
-// Return a translated version of the items in the specified array.
-std::vector<std::wstring> JSI_L10n::TranslateArray(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::vector<std::wstring>& sourceArray)
-{
-	std::vector<std::wstring> translatedArray;
-	for (const std::wstring& elem : sourceArray)
-		translatedArray.push_back(wstring_from_utf8(g_L10n.Translate(utf8_from_wstring(elem))));
+	std::vector<std::string> translatedArray;
+	if (g_L10n.IsInitialised())
+		for (const std::string& elem : sourceArray)
+			translatedArray.push_back(g_L10n.Translate(elem));
 
 	return translatedArray;
 }
 
-std::wstring JSI_L10n::GetFallbackToAvailableDictLocale(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.GetFallbackToAvailableDictLocale(locale);
-}
-
 // Return a localized version of a time given in milliseconds.
-std::wstring JSI_L10n::FormatMillisecondsIntoDateStringLocal(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), UDate milliseconds, const std::wstring& formatString)
+std::string FormatMillisecondsIntoDateStringLocal(UDate milliseconds, const std::string& formatString)
 {
-	return wstring_from_utf8(g_L10n.FormatMillisecondsIntoDateString(milliseconds, utf8_from_wstring(formatString), true));
+	return g_L10n.FormatMillisecondsIntoDateString(milliseconds, formatString, true);
 }
 
 // Return a localized version of a duration or a time in GMT given in milliseconds.
-std::wstring JSI_L10n::FormatMillisecondsIntoDateStringGMT(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), UDate milliseconds, const std::wstring& formatString)
+std::string FormatMillisecondsIntoDateStringGMT(UDate milliseconds, const std::string& formatString)
 {
-	return wstring_from_utf8(g_L10n.FormatMillisecondsIntoDateString(milliseconds, utf8_from_wstring(formatString), false));
+	return g_L10n.FormatMillisecondsIntoDateString(milliseconds, formatString, false);
 }
 
-// Return a localized version of the given decimal number.
-std::wstring JSI_L10n::FormatDecimalNumberIntoString(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), double number)
+void RegisterScriptFunctions(const ScriptRequest& rq)
 {
-	return wstring_from_utf8(g_L10n.FormatDecimalNumberIntoString(number));
+#define REGISTER_L10N(name) \
+	ScriptFunction::Register<&L10n::name, &L10nGetter>(rq, #name);
+#define REGISTER_L10N_FUNC(func, name) \
+	ScriptFunction::Register<func, &L10nGetter>(rq, name);
+
+	REGISTER_L10N(Translate)
+	REGISTER_L10N(TranslateWithContext)
+	REGISTER_L10N(TranslatePlural)
+	REGISTER_L10N(TranslatePluralWithContext)
+	REGISTER_L10N(TranslateLines)
+	ScriptFunction::Register<&TranslateArray>(rq, "TranslateArray");
+	ScriptFunction::Register<&FormatMillisecondsIntoDateStringLocal>(rq, "FormatMillisecondsIntoDateStringLocal");
+	ScriptFunction::Register<&FormatMillisecondsIntoDateStringGMT>(rq, "FormatMillisecondsIntoDateStringGMT");
+	REGISTER_L10N(FormatDecimalNumberIntoString)
+
+	REGISTER_L10N(GetSupportedLocaleBaseNames)
+	REGISTER_L10N(GetSupportedLocaleDisplayNames)
+	REGISTER_L10N_FUNC(&L10n::GetCurrentLocaleString, "GetCurrentLocale");
+	REGISTER_L10N(GetAllLocales)
+	// Select the appropriate overload.
+	REGISTER_L10N_FUNC(static_cast<std::string(L10n::*)(const std::string&) const>(&L10n::GetDictionaryLocale), "GetDictionaryLocale");
+	REGISTER_L10N(GetDictionariesForLocale)
+
+	REGISTER_L10N(UseLongStrings)
+	REGISTER_L10N(GetLocaleLanguage)
+	REGISTER_L10N(GetLocaleBaseName)
+	REGISTER_L10N(GetLocaleCountry)
+	REGISTER_L10N(GetLocaleScript)
+	// Select the appropriate overload.
+	REGISTER_L10N_FUNC(static_cast<std::wstring(L10n::*)(const std::string&) const>(&L10n::GetFallbackToAvailableDictLocale), "GetFallbackToAvailableDictLocale");
+
+	// Select the appropriate overloads.
+	REGISTER_L10N_FUNC(static_cast<bool(L10n::*)(const std::string&) const>(&L10n::ValidateLocale), "ValidateLocale");
+	REGISTER_L10N_FUNC(static_cast<bool(L10n::*)(const std::string&) const>(&L10n::SaveLocale), "SaveLocale");
+	REGISTER_L10N(ReevaluateCurrentLocaleAndReload)
+#undef REGISTER_L10N
+#undef REGISTER_L10N_FUNC
 }
-
-std::vector<std::string> JSI_L10n::GetSupportedLocaleBaseNames(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
-{
-	return g_L10n.GetSupportedLocaleBaseNames();
-}
-
-std::vector<std::wstring> JSI_L10n::GetSupportedLocaleDisplayNames(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
-{
-	return g_L10n.GetSupportedLocaleDisplayNames();
-}
-
-std::string JSI_L10n::GetCurrentLocale(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
-{
-	return g_L10n.GetCurrentLocaleString();
-}
-
-bool JSI_L10n::UseLongStrings(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
-{
-	return g_L10n.UseLongStrings();
-}
-
-std::vector<std::string> JSI_L10n::GetAllLocales(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
-{
-	return g_L10n.GetAllLocales();
-}
-
-std::string JSI_L10n::GetDictionaryLocale(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& configLocale)
-{
-	return g_L10n.GetDictionaryLocale(configLocale);
-}
-
-std::vector<std::wstring> JSI_L10n::GetDictionariesForLocale(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.GetDictionariesForLocale(locale);
-}
-
-std::string JSI_L10n::GetLocaleLanguage(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.GetLocaleLanguage(locale);
-}
-
-std::string JSI_L10n::GetLocaleBaseName(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.GetLocaleBaseName(locale);
-}
-
-std::string JSI_L10n::GetLocaleCountry(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.GetLocaleCountry(locale);
-}
-
-std::string JSI_L10n::GetLocaleScript(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.GetLocaleScript(locale);
-}
-
-bool JSI_L10n::ValidateLocale(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.ValidateLocale(locale);
-}
-
-bool JSI_L10n::SaveLocale(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate), const std::string& locale)
-{
-	return g_L10n.SaveLocale(locale);
-}
-
-void JSI_L10n::ReevaluateCurrentLocaleAndReload(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
-{
-	g_L10n.ReevaluateCurrentLocaleAndReload();
-}
-
-
-void JSI_L10n::RegisterScriptFunctions(const ScriptInterface& scriptInterface)
-{
-	scriptInterface.RegisterFunction<std::wstring, std::wstring, &Translate>("Translate");
-	scriptInterface.RegisterFunction<std::wstring, std::string, std::wstring, &TranslateWithContext>("TranslateWithContext");
-	scriptInterface.RegisterFunction<std::wstring, std::wstring, std::wstring, int, &TranslatePlural>("TranslatePlural");
-	scriptInterface.RegisterFunction<std::wstring, std::string, std::wstring, std::wstring, int, &TranslatePluralWithContext>("TranslatePluralWithContext");
-	scriptInterface.RegisterFunction<std::wstring, std::wstring, &TranslateLines>("TranslateLines");
-	scriptInterface.RegisterFunction<std::vector<std::wstring>, std::vector<std::wstring>, &TranslateArray>("TranslateArray");
-	scriptInterface.RegisterFunction<std::wstring, UDate, std::wstring, &FormatMillisecondsIntoDateStringLocal>("FormatMillisecondsIntoDateStringLocal");
-	scriptInterface.RegisterFunction<std::wstring, UDate, std::wstring, &FormatMillisecondsIntoDateStringGMT>("FormatMillisecondsIntoDateStringGMT");
-	scriptInterface.RegisterFunction<std::wstring, double, &FormatDecimalNumberIntoString>("FormatDecimalNumberIntoString");
-
-	scriptInterface.RegisterFunction<std::vector<std::string>, &GetSupportedLocaleBaseNames>("GetSupportedLocaleBaseNames");
-	scriptInterface.RegisterFunction<std::vector<std::wstring>, &GetSupportedLocaleDisplayNames>("GetSupportedLocaleDisplayNames");
-	scriptInterface.RegisterFunction<std::string, &GetCurrentLocale>("GetCurrentLocale");
-	scriptInterface.RegisterFunction<std::vector<std::string>, &GetAllLocales>("GetAllLocales");
-	scriptInterface.RegisterFunction<std::string, std::string, &GetDictionaryLocale>("GetDictionaryLocale");
-	scriptInterface.RegisterFunction<std::vector<std::wstring>, std::string, &GetDictionariesForLocale>("GetDictionariesForLocale");
-
-	scriptInterface.RegisterFunction<bool, &UseLongStrings>("UseLongStrings");
-	scriptInterface.RegisterFunction<std::string, std::string, &GetLocaleLanguage>("GetLocaleLanguage");
-	scriptInterface.RegisterFunction<std::string, std::string, &GetLocaleBaseName>("GetLocaleBaseName");
-	scriptInterface.RegisterFunction<std::string, std::string, &GetLocaleCountry>("GetLocaleCountry");
-	scriptInterface.RegisterFunction<std::string, std::string, &GetLocaleScript>("GetLocaleScript");
-	scriptInterface.RegisterFunction<std::wstring, std::string, &GetFallbackToAvailableDictLocale>("GetFallbackToAvailableDictLocale");
-
-	scriptInterface.RegisterFunction<bool, std::string, &ValidateLocale>("ValidateLocale");
-	scriptInterface.RegisterFunction<bool, std::string, &SaveLocale>("SaveLocale");
-	scriptInterface.RegisterFunction<void, &ReevaluateCurrentLocaleAndReload>("ReevaluateCurrentLocaleAndReload");
 }

source/ps/VisualReplay.h
@@ -15,14 +15,15 @@
  * along with 0 A.D.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef INCLUDED_REPlAY
-#define INCLUDED_REPlAY
+#ifndef INCLUDED_VISUAL_REPLAY
+#define INCLUDED_VISUAL_REPLAY
 
 #include "lib/os_path.h"
-#include "scriptinterface/ScriptInterface.h"
 
 class CSimulation2;
 class CGUIManager;
+
+class ScriptInterface;
 
 /**
  * Contains functions for visually replaying past games.
@@ -104,7 +105,7 @@ bool DeleteReplay(const OsPath& replayFile);
 /**
  * Returns the parsed header of the replay file (commands.txt).
  */
-JS::Value GetReplayAttributes(ScriptInterface::CmptPrivate* pCmptPrivate, const OsPath& directoryName);
+JS::Value GetReplayAttributes(const ScriptInterface& scriptInterface, const OsPath& directoryName);
 
 /**
  * Returns whether or not the metadata / summary screen data has been saved properly when the game ended.
@@ -114,7 +115,7 @@ bool HasReplayMetadata(const OsPath& directoryName);
 /**
  * Returns the metadata of a replay.
  */
-JS::Value GetReplayMetadata(ScriptInterface::CmptPrivate* pCmptPrivate, const OsPath& directoryName);
+JS::Value GetReplayMetadata(const ScriptInterface& scriptInterface, const OsPath& directoryName);
 
 /**
 * Adds a replay to the replayCache.

source/graphics/CameraController.cpp
@@ -21,7 +21,6 @@
 
 #include "graphics/HFTracer.h"
 #include "graphics/Terrain.h"
-#include "graphics/scripting/JSInterface_GameView.h"
 #include "lib/input.h"
 #include "lib/timer.h"
 #include "maths/MathUtil.h"

source/graphics/GameView.cpp
@@ -35,7 +35,6 @@
 #include "graphics/TerritoryTexture.h"
 #include "graphics/Unit.h"
 #include "graphics/UnitManager.h"
-#include "graphics/scripting/JSInterface_GameView.h"
 #include "lib/input.h"
 #include "lib/timer.h"
 #include "lobby/IXmppClient.h"

source/graphics/MapGenerator.cpp
@@ -32,6 +32,7 @@
 #include "ps/Profile.h"
 #include "ps/Threading.h"
 #include "ps/scripting/JSInterface_VFS.h"
+#include "scriptinterface/FunctionWrapper.h"
 #include "scriptinterface/ScriptContext.h"
 #include "scriptinterface/ScriptConversions.h"
 #include "scriptinterface/ScriptInterface.h"
@@ -160,6 +161,11 @@ bool CMapGeneratorWorker::Run()
 	return true;
 }
 
+#define REGISTER_MAPGEN_FUNC(func) \
+	ScriptFunction::Register<&CMapGeneratorWorker::func, ScriptFunction::ObjectFromCBData<CMapGeneratorWorker>>(rq, #func);
+#define REGISTER_MAPGEN_FUNC_NAME(func, name) \
+	ScriptFunction::Register<&CMapGeneratorWorker::func, ScriptFunction::ObjectFromCBData<CMapGeneratorWorker>>(rq, name);
+
 void CMapGeneratorWorker::InitScriptInterface(const u32 seed)
 {
 	m_ScriptInterface->SetCallbackData(static_cast<void*>(this));
@@ -174,9 +180,10 @@ void CMapGeneratorWorker::InitScriptInterface(const u32 seed)
 	m_ScriptInterface->LoadGlobalScripts();
 
 	// File loading
-	m_ScriptInterface->RegisterFunction<bool, VfsPath, CMapGeneratorWorker::LoadLibrary>("LoadLibrary");
-	m_ScriptInterface->RegisterFunction<JS::Value, VfsPath, CMapGeneratorWorker::LoadHeightmap>("LoadHeightmapImage");
-	m_ScriptInterface->RegisterFunction<JS::Value, VfsPath, CMapGeneratorWorker::LoadMapTerrain>("LoadMapTerrain");
+	ScriptRequest rq(m_ScriptInterface);
+	REGISTER_MAPGEN_FUNC_NAME(LoadScripts, "LoadLibrary");
+	REGISTER_MAPGEN_FUNC_NAME(LoadHeightmap, "LoadHeightmapImage");
+	REGISTER_MAPGEN_FUNC(LoadMapTerrain);
 
 	// Engine constants
 
@@ -190,17 +197,22 @@ void CMapGeneratorWorker::InitScriptInterface(const u32 seed)
 
 void CMapGeneratorWorker::RegisterScriptFunctions_MapGenerator()
 {
+	ScriptRequest rq(m_ScriptInterface);
+
 	// Template functions
-	m_ScriptInterface->RegisterFunction<CParamNode, std::string, CMapGeneratorWorker::GetTemplate>("GetTemplate");
-	m_ScriptInterface->RegisterFunction<bool, std::string, CMapGeneratorWorker::TemplateExists>("TemplateExists");
-	m_ScriptInterface->RegisterFunction<std::vector<std::string>, std::string, bool, CMapGeneratorWorker::FindTemplates>("FindTemplates");
-	m_ScriptInterface->RegisterFunction<std::vector<std::string>, std::string, bool, CMapGeneratorWorker::FindActorTemplates>("FindActorTemplates");
+	REGISTER_MAPGEN_FUNC(GetTemplate);
+	REGISTER_MAPGEN_FUNC(TemplateExists);
+	REGISTER_MAPGEN_FUNC(FindTemplates);
+	REGISTER_MAPGEN_FUNC(FindActorTemplates);
 
 	// Progression and profiling
-	m_ScriptInterface->RegisterFunction<void, int, CMapGeneratorWorker::SetProgress>("SetProgress");
-	m_ScriptInterface->RegisterFunction<double, CMapGeneratorWorker::GetMicroseconds>("GetMicroseconds");
-	m_ScriptInterface->RegisterFunction<void, JS::HandleValue, CMapGeneratorWorker::ExportMap>("ExportMap");
+	REGISTER_MAPGEN_FUNC(SetProgress);
+	REGISTER_MAPGEN_FUNC(GetMicroseconds);
+	REGISTER_MAPGEN_FUNC(ExportMap);
 }
+
+#undef REGISTER_MAPGEN_FUNC
+#undef REGISTER_MAPGEN_FUNC_NAME
 
 int CMapGeneratorWorker::GetProgress()
 {
@@ -208,7 +220,7 @@ int CMapGeneratorWorker::GetProgress()
 	return m_Progress;
 }
 
-double CMapGeneratorWorker::GetMicroseconds(ScriptInterface::CmptPrivate* UNUSED(pCmptPrivate))
+double CMapGeneratorWorker::GetMicroseconds()
 {
 	return JS_Now();
 }
@@ -219,61 +231,47 @@ ScriptInterface::StructuredClone CMapGeneratorWorker::GetResults()
 	return m_MapData;
 }
 
-bool CMapGeneratorWorker::LoadLibrary(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& name)
+void CMapGeneratorWorker::ExportMap(JS::HandleValue data)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->LoadScripts(name);
-}
-
-void CMapGeneratorWorker::ExportMap(ScriptInterface::CmptPrivate* pCmptPrivate, JS::HandleValue data)
-{
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-
 	// Copy results
-	std::lock_guard<std::mutex> lock(self->m_WorkerMutex);
-	self->m_MapData = self->m_ScriptInterface->WriteStructuredClone(data);
-	self->m_Progress = 0;
+	std::lock_guard<std::mutex> lock(m_WorkerMutex);
+	m_MapData = m_ScriptInterface->WriteStructuredClone(data);
+	m_Progress = 0;
 }
 
-void CMapGeneratorWorker::SetProgress(ScriptInterface::CmptPrivate* pCmptPrivate, int progress)
+void CMapGeneratorWorker::SetProgress(int progress)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-
 	// Copy data
-	std::lock_guard<std::mutex> lock(self->m_WorkerMutex);
+	std::lock_guard<std::mutex> lock(m_WorkerMutex);
 
-	if (progress >= self->m_Progress)
-		self->m_Progress = progress;
+	if (progress >= m_Progress)
+		m_Progress = progress;
 	else
-		LOGWARNING("The random map script tried to reduce the loading progress from %d to %d", self->m_Progress, progress);
+		LOGWARNING("The random map script tried to reduce the loading progress from %d to %d", m_Progress, progress);
 }
 
-CParamNode CMapGeneratorWorker::GetTemplate(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName)
+CParamNode CMapGeneratorWorker::GetTemplate(const std::string& templateName)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	const CParamNode& templateRoot = self->m_TemplateLoader.GetTemplateFileData(templateName).GetChild("Entity");
+	const CParamNode& templateRoot = m_TemplateLoader.GetTemplateFileData(templateName).GetChild("Entity");
 	if (!templateRoot.IsOk())
 		LOGERROR("Invalid template found for '%s'", templateName.c_str());
 
 	return templateRoot;
 }
 
-bool CMapGeneratorWorker::TemplateExists(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName)
+bool CMapGeneratorWorker::TemplateExists(const std::string& templateName)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->m_TemplateLoader.TemplateExists(templateName);
+	return m_TemplateLoader.TemplateExists(templateName);
 }
 
-std::vector<std::string> CMapGeneratorWorker::FindTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories)
+std::vector<std::string> CMapGeneratorWorker::FindTemplates(const std::string& path, bool includeSubdirectories)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->m_TemplateLoader.FindTemplates(path, includeSubdirectories, SIMULATION_TEMPLATES);
+	return m_TemplateLoader.FindTemplates(path, includeSubdirectories, SIMULATION_TEMPLATES);
 }
 
-std::vector<std::string> CMapGeneratorWorker::FindActorTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories)
+std::vector<std::string> CMapGeneratorWorker::FindActorTemplates(const std::string& path, bool includeSubdirectories)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	return self->m_TemplateLoader.FindTemplates(path, includeSubdirectories, ACTOR_TEMPLATES);
+	return m_TemplateLoader.FindTemplates(path, includeSubdirectories, ACTOR_TEMPLATES);
 }
 
 bool CMapGeneratorWorker::LoadScripts(const VfsPath& libraryName)
@@ -314,7 +312,7 @@ bool CMapGeneratorWorker::LoadScripts(const VfsPath& libraryName)
 	return true;
 }
 
-JS::Value CMapGeneratorWorker::LoadHeightmap(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& filename)
+JS::Value CMapGeneratorWorker::LoadHeightmap(const VfsPath& filename)
 {
 	std::vector<u16> heightmap;
 	if (LoadHeightmapImageVfs(filename, heightmap) != INFO::OK)
@@ -323,18 +321,16 @@ JS::Value CMapGeneratorWorker::LoadHeightmap(ScriptInterface::CmptPrivate* pCmpt
 		return JS::UndefinedValue();
 	}
 
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	ScriptRequest rq(self->m_ScriptInterface);
+	ScriptRequest rq(m_ScriptInterface);
 	JS::RootedValue returnValue(rq.cx);
 	ToJSVal_vector(rq, &returnValue, heightmap);
 	return returnValue;
 }
 
 // See CMapReader::UnpackTerrain, CMapReader::ParseTerrain for the reordering
-JS::Value CMapGeneratorWorker::LoadMapTerrain(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& filename)
+JS::Value CMapGeneratorWorker::LoadMapTerrain(const VfsPath& filename)
 {
-	CMapGeneratorWorker* self = static_cast<CMapGeneratorWorker*>(pCmptPrivate->pCBData);
-	ScriptRequest rq(self->m_ScriptInterface);
+	ScriptRequest rq(m_ScriptInterface);
 
 	if (!VfsFileExists(filename))
 	{

source/graphics/MapGenerator.h
@@ -134,54 +134,49 @@ private:
 	bool LoadScripts(const VfsPath& libraryName);
 
 	/**
-	 * Recursively load all script files in the given folder.
-	 */
-	static bool LoadLibrary(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& name);
-
-	/**
 	 * Finalize map generation and pass results from the script to the engine.
 	 */
-	static void ExportMap(ScriptInterface::CmptPrivate* pCmptPrivate, JS::HandleValue data);
+	void ExportMap(JS::HandleValue data);
 
 	/**
 	 * Load an image file and return it as a height array.
 	 */
-	static JS::Value LoadHeightmap(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& src);
+	JS::Value LoadHeightmap(const VfsPath& src);
 
 	/**
 	 * Load an Atlas terrain file (PMP) returning textures and heightmap.
 	 */
-	static JS::Value LoadMapTerrain(ScriptInterface::CmptPrivate* pCmptPrivate, const VfsPath& filename);
+	JS::Value LoadMapTerrain(const VfsPath& filename);
 
 	/**
 	 * Sets the map generation progress, which is one of multiple stages determining the loading screen progress.
 	 */
-	static void SetProgress(ScriptInterface::CmptPrivate* pCmptPrivate, int progress);
+	void SetProgress(int progress);
 
 	/**
 	 * Microseconds since the epoch.
 	 */
-	static double GetMicroseconds(ScriptInterface::CmptPrivate* pCmptPrivate);
+	double GetMicroseconds();
 
 	/**
 	 * Return the template data of the given template name.
 	 */
-	static CParamNode GetTemplate(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName);
+	CParamNode GetTemplate(const std::string& templateName);
 
 	/**
 	 * Check whether the given template exists.
 	 */
-	static bool TemplateExists(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& templateName);
+	bool TemplateExists(const std::string& templateName);
 
 	/**
 	 * Returns all template names of simulation entity templates.
 	 */
-	static std::vector<std::string> FindTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories);
+	std::vector<std::string> FindTemplates(const std::string& path, bool includeSubdirectories);
 
 	/**
 	 * Returns all template names of actors.
 	 */
-	static std::vector<std::string> FindActorTemplates(ScriptInterface::CmptPrivate* pCmptPrivate, const std::string& path, bool includeSubdirectories);
+	std::vector<std::string> FindActorTemplates(const std::string& path, bool includeSubdirectories);
 
 	/**
 	 * Perform map generation in an independent thread.

commit a4223c87f692e17754e11ba55eda0c78ca81b0b3

Fix NoPCH following cb346e207b
Broken in cb346e207b

Differential Revision: https://code.wildfiregames.com/D3629
This was SVN commit r24994.

source/i18n/scripting/JSInterface_L10n.cpp
@@ -21,6 +21,7 @@
 
 #include "i18n/L10n.h"
 #include "lib/utf8.h"
+#include "ps/CLogger.h"
 #include "scriptinterface/FunctionWrapper.h"
 #include "scriptinterface/ScriptInterface.h"
 

source/ps/VisualReplay.h
@@ -19,9 +19,11 @@
 #define INCLUDED_VISUAL_REPLAY
 
 #include "lib/os_path.h"
+#include "scriptinterface/ScriptTypes.h"
 
 class CSimulation2;
 class CGUIManager;
+class CStrW;
 
 class ScriptInterface;
 