TaoJiaHengOne/0ad

commit 293cd95ccb2f13e9b1b8c845eaace575a9c3c9e0

Flush order queue when given an order to perform a action on the same entity.
Follows a4c698ac48.

Fixes: #6046
Differential revision: D3587
Reviewed by: @wraitii
This was SVN commit r25100.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5565,6 +5565,8 @@ UnitAI.prototype.Attack = function(target, allowCapture = true, queued = false, 
 	{
 		this.order.data.lastPos = order.lastPos;
 		this.order.data.force = order.force;
+		if (order.force)
+			this.orderQueue = [this.order];
 		return;
 	}
 
@@ -5650,6 +5652,8 @@ UnitAI.prototype.PerformGather = function(target, queued, force, pushFront = fal
 	{
 		this.order.data.lastPos = order.lastPos;
 		this.order.data.force = order.force;
+		if (order.force)
+			this.orderQueue = [this.order];
 		return;
 	}
 
@@ -5687,6 +5691,7 @@ UnitAI.prototype.Heal = function(target, queued, pushFront)
 		this.order.data.target === target)
 	{
 		this.order.data.force = true;
+		this.orderQueue = [this.order];
 		return;
 	}
 
@@ -5910,6 +5915,7 @@ UnitAI.prototype.Repair = function(target, autocontinue, queued, pushFront)
 		this.order.data.autocontinue === autocontinue)
 	{
 		this.order.data.force = true;
+		this.orderQueue = [this.order];
 		return;
 	}
 

commit 3548feb16a8c91f736386a4bfc41cabd2ec05a6f

Fix entities receiving double hunt order only attacking.
Caused by: 293cd95ccb.
Noticed by @Asger and @Kate.
Special case hunting.

Differential revision: https://code.wildfiregames.com/D3986
Comment by: @wraitii
This was SVN commit r25509.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5548,7 +5548,12 @@ UnitAI.prototype.PerformGather = function(target, queued, force, pushFront = fal
 		this.order.data.lastPos = order.lastPos;
 		this.order.data.force = order.force;
 		if (order.force)
-			this.orderQueue = [this.order];
+		{
+			if (this.orderQueue[1]?.type === "Gather")
+				this.orderQueue = [this.order, this.orderQueue[1]];
+			else
+				this.orderQueue = [this.order];
+		}
 		return;
 	}
 