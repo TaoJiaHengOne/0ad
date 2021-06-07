TaoJiaHengOne/0ad

commit 72f0fdb41b9257d3956a86ca601d715b0c9770d3

GameSetup GUI cleanup - Renaming.
- rename 'Controls' class to 'Controllers' to avoid ambiguity and mimic
MVC lingo.
- rename GameRegisterStanza into LobbyGameRegistration, and make it
explicitly a controller.
- rename GameSettingsFile to PersistentMatchSettings & move it to its
own folder (since it could be reused independently of the controllers)
- remove the StartGameController & move `launchGame` to the
GameSettingsController (simplifies the control flow)
- the GUI Object "setupWindow" was actually the GameSettingsPage, this
is corrected.
- the LoadingPage was showing the GameSettingsPage -> in the current
code, it makes more sense to have both handle their own visibility.
- make the NetMessages class independent of other gamesetup logic, so
that it can be reused by other code. Remove the custom XML object.

Differential Revision: https://code.wildfiregames.com/D3719
This was SVN commit r25101.

binaries/data/mods/public/gui/gamesetup/Controllers/GameSettingsController.js
@@ -1,20 +1,24 @@
 /**
- * 'Controller' for the GUI handling of gamesettings.
+ * Controller for the GUI handling of gamesettings.
  */
-class GameSettingsControl
+class GameSettingsController
 {
-	constructor(setupWindow, netMessages, startGameControl, playerAssignmentsControl, mapCache)
+	constructor(setupWindow, netMessages, playerAssignmentsController, mapCache)
 	{
 		this.setupWindow = setupWindow;
-		this.startGameControl = startGameControl;
 		this.mapCache = mapCache;
-		this.gameSettingsFile = new GameSettingsFile(this);
+		this.persistentMatchSettings = new PersistentMatchSettings(this);
 
 		this.guiData = new GameSettingsGuiData();
 
 		// When joining a game, the complete set of attributes
 		// may not have been received yet.
 		this.loading = true;
+
+		// If this is true, the ready controller won't reset readiness.
+		// TODO: ideally the ready controller would be somewhat independent from this one,
+		// possibly by listening to gamesetup messages itself.
+		this.gameStarted = false;
 
 		this.updateLayoutHandlers = new Set();
 		this.settingsChangeHandlers = new Set();
@@ -23,15 +27,18 @@ class GameSettingsControl
 		setupWindow.registerLoadHandler(this.onLoad.bind(this));
 		setupWindow.registerGetHotloadDataHandler(this.onGetHotloadData.bind(this));
 
-		startGameControl.registerLaunchGameHandler(this.onLaunchGame.bind(this));
-
 		setupWindow.registerClosePageHandler(this.onClose.bind(this));
 
-		if (g_IsController && g_IsNetworked)
-			playerAssignmentsControl.registerClientJoinHandler(this.onClientJoin.bind(this));
-
 		if (g_IsNetworked)
+		{
+			if (g_IsController)
+				playerAssignmentsController.registerClientJoinHandler(this.onClientJoin.bind(this));
+			else
+				// In MP, the host launches the game and switches right away,
+				// clients switch when they receive the appropriate message.
+				netMessages.registerNetMessageHandler("start", this.switchToLoadingPage.bind(this));
 			netMessages.registerNetMessageHandler("gamesetup", this.onGamesetupMessage.bind(this));
+		}
 	}
 
 	/**
@@ -63,9 +70,9 @@ class GameSettingsControl
 	{
 		if (hotloadData)
 			this.parseSettings(hotloadData.initAttributes);
-		else if (g_IsController && this.gameSettingsFile.enabled)
+		else if (g_IsController && this.persistentMatchSettings.enabled)
 		{
-			let settings = this.gameSettingsFile.loadFile();
+			let settings = this.persistentMatchSettings.loadFile();
 			if (settings)
 				this.parseSettings(settings);
 		}
@@ -76,11 +83,6 @@ class GameSettingsControl
 		// If we are the controller, we are done loading.
 		if (hotloadData || !g_IsNetworked || g_IsController)
 			this.setLoading(false);
-	}
-
-	onClose()
-	{
-		this.gameSettingsFile.saveFile();
 	}
 
 	onClientJoin()
@@ -226,10 +228,52 @@ class GameSettingsControl
 			});
 	}
 
-	onLaunchGame()
+	/**
+	 * Cheat prevention:
+	 *
+	 * 1. Ensure that the host cannot start the game unless all clients agreed on the game settings using the ready system.
+	 *
+	 * TODO:
+	 * 2. Ensure that the host cannot start the game with InitAttributes different from the agreed ones.
+	 * This may be achieved by:
+	 * - Determining the seed collectively.
+	 * - passing the agreed game settings to the engine when starting the game instance
+	 * - rejecting new game settings from the server after the game launch event
+	 */
+	launchGame()
 	{
 		// Save the file before random settings are resolved.
-		this.gameSettingsFile.saveFile();
+		this.savePersistentMatchSettings();
+
+		// Mark the game as started so the readyController won't reset state.
+		this.gameStarted = true;
+
+		// This will resolve random settings & send game start messages.
+		// TODO: this will trigger observers, which is somewhat wasteful.
+		g_GameSettings.launchGame(g_PlayerAssignments);
+
+		// Switch to the loading page right away,
+		// the GUI will otherwise show the unrandomised settings.
+		this.switchToLoadingPage();
+	}
+
+	switchToLoadingPage()
+	{
+		Engine.SwitchGuiPage("page_loading.xml", {
+			"attribs": g_GameSettings.toInitAttributes(),
+			"playerAssignments": g_PlayerAssignments
+		});
+	}
+
+	onClose()
+	{
+		this.savePersistentMatchSettings();
+	}
+
+	savePersistentMatchSettings()
+	{
+		// TODO: ought to only save a subset of settings.
+		this.persistentMatchSettings.saveFile(this.getSettings());
 	}
 }
 
@@ -237,4 +281,4 @@ class GameSettingsControl
 /**
  * Wait (at most) this many milliseconds before sending network messages.
  */
-GameSettingsControl.prototype.Timeout = 400;
+GameSettingsController.prototype.Timeout = 400;

commit 7e9bb4d4e80f6dd922e86f47718c93f67aed8518

Only save MP match settings if the player is the controller
Broken in 72f0fdb41b.

Differential Revision: https://code.wildfiregames.com/D4112
This was SVN commit r25732.

binaries/data/mods/public/gui/gamesetup/Controllers/GameSettingsController.js
@@ -296,8 +296,9 @@ class GameSettingsController
 
 	savePersistentMatchSettings()
 	{
-		// TODO: ought to only save a subset of settings.
-		this.persistentMatchSettings.saveFile(this.getSettings());
+		if (g_IsController)
+			// TODO: ought to only save a subset of settings.
+			this.persistentMatchSettings.saveFile(this.getSettings());
 	}
 }
 