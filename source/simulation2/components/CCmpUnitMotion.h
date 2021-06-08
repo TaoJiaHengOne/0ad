TaoJiaHengOne/0ad

commit 40cbde1925dc6b48e4100da1ce32aaa4eb8d0129

Further Pushing tweaks: more customisable, longer ranges.
This overall decreases the deathball effect from units walking to each
other a bit.
- Fix formations - this cleans up a UnitMotion hack for formations,
making it possible to increase pushing ranges without breaking closely
knit formations like testudo.
- Make MINIMAL_PUSHING and the MOVE_EXTENSION configurable, and add a
STATIC_EXTENSION as well.
- Increase the pushing range significantly, making units sparser.

Differential Revision: https://code.wildfiregames.com/D4098
This was SVN commit r25708.

source/simulation2/components/CCmpUnitMotion.h
@@ -143,7 +143,7 @@ public:
 
 	// Template state:
 
-	bool m_FormationController;
+	bool m_IsFormationController;
 
 	fixed m_TemplateWalkSpeed, m_TemplateRunMultiplier;
 	pass_class_t m_PassClass;
@@ -209,6 +209,9 @@ public:
 		MoveRequest(entity_id_t target, CFixedVector2D offset) : m_Type(OFFSET), m_Entity(target), m_Position(offset) {};
 	} m_MoveRequest;
 
+	// If this is not INVALID_ENTITY, the unit is a formation member.
+	entity_id_t m_FormationController = INVALID_ENTITY;
+
 	// If the entity moves, it will do so at m_WalkSpeed * m_SpeedMultiplier.
 	fixed m_SpeedMultiplier;
 	// This caches the resulting speed from m_WalkSpeed * m_SpeedMultiplier for convenience.
@@ -253,7 +256,7 @@ public:
 
 	virtual void Init(const CParamNode& paramNode)
 	{
-		m_FormationController = paramNode.GetChild("FormationController").ToBool();
+		m_IsFormationController = paramNode.GetChild("FormationController").ToBool();
 
 		m_FacePointAfterMove = true;
 
@@ -307,6 +310,8 @@ public:
 		serialize.NumberFixed_Unbounded("target min range", m_MoveRequest.m_MinRange);
 		serialize.NumberFixed_Unbounded("target max range", m_MoveRequest.m_MaxRange);
 
+		serialize.NumberU32_Unbounded("formation controller", m_FormationController);
+
 		serialize.NumberFixed_Unbounded("speed multiplier", m_SpeedMultiplier);
 
 		serialize.NumberFixed_Unbounded("current speed", m_CurSpeed);
@@ -358,7 +363,7 @@ public:
 		case MT_Create:
 		{
 			if (!ENTITY_IS_LOCAL(GetEntityId()))
-				CmpPtr<ICmpUnitMotionManager>(GetSystemEntity())->Register(this, GetEntityId(), m_FormationController);
+				CmpPtr<ICmpUnitMotionManager>(GetSystemEntity())->Register(this, GetEntityId(), m_IsFormationController);
 			break;
 		}
 		case MT_Destroy:
@@ -390,7 +395,7 @@ public:
 		{
 			OnValueModification();
 			if (!ENTITY_IS_LOCAL(GetEntityId()))
-				CmpPtr<ICmpUnitMotionManager>(GetSystemEntity())->Register(this, GetEntityId(), m_FormationController);
+				CmpPtr<ICmpUnitMotionManager>(GetSystemEntity())->Register(this, GetEntityId(), m_IsFormationController);
 			break;
 		}
 		}
@@ -501,9 +506,16 @@ public:
 		return MoveTo(MoveRequest(target, minRange, maxRange));
 	}
 
-	virtual void MoveToFormationOffset(entity_id_t target, entity_pos_t x, entity_pos_t z)
+	virtual void MoveToFormationOffset(entity_id_t controller, entity_pos_t x, entity_pos_t z)
 	{
-		MoveTo(MoveRequest(target, CFixedVector2D(x, z)));
+		SetMemberOfFormation(controller);
+		// Pass the controller to the move request anyways.
+		MoveTo(MoveRequest(controller, CFixedVector2D(x, z)));
+	}
+
+	virtual void SetMemberOfFormation(entity_id_t controller)
+	{
+		m_FormationController = controller;
 	}
 
 	virtual bool IsTargetRangeReachable(entity_id_t target, entity_pos_t minRange, entity_pos_t maxRange);
@@ -542,19 +554,18 @@ public:
 private:
 	bool IsFormationMember() const
 	{
-		// TODO: this really shouldn't be what we are checking for.
-		return m_MoveRequest.m_Type == MoveRequest::OFFSET;
+		return m_FormationController != INVALID_ENTITY;
 	}
 
 	bool IsFormationControllerMoving() const
 	{
-		CmpPtr<ICmpUnitMotion> cmpControllerMotion(GetSimContext(), m_MoveRequest.m_Entity);
+		CmpPtr<ICmpUnitMotion> cmpControllerMotion(GetSimContext(), m_FormationController);
 		return cmpControllerMotion && cmpControllerMotion->IsMoveRequested();
 	}
 
 	entity_id_t GetGroup() const
 	{
-		return IsFormationMember() ? m_MoveRequest.m_Entity : GetEntityId();
+		return IsFormationMember() ? m_FormationController : GetEntityId();
 	}
 
 	void SetParticipateInPushing(bool pushing)
@@ -975,7 +986,7 @@ void CCmpUnitMotion::PreMove(CCmpUnitMotionManager::MotionState& state)
 	if (!m_BlockMovement)
 		return;
 
-	state.controlGroup = IsFormationMember() ? m_MoveRequest.m_Entity : INVALID_ENTITY;
+	state.controlGroup = IsFormationMember() ? m_FormationController : INVALID_ENTITY;
 
 	// Update moving flag, this is an internal construct used for pushing,
 	// so it does not really reflect whether the unit is actually moving or not.

binaries/data/mods/public/simulation/components/UnitAI.js
@@ -5107,7 +5107,7 @@ UnitAI.prototype.SetFormationController = function(ent)
 
 	// Set obstruction group, so we can walk through members
 	// of our own formation (or ourself if not in formation)
-	var cmpObstruction = Engine.QueryInterface(this.entity, IID_Obstruction);
+	const cmpObstruction = Engine.QueryInterface(this.entity, IID_Obstruction);
 	if (cmpObstruction)
 	{
 		if (ent == INVALID_ENTITY)
@@ -5115,6 +5115,10 @@ UnitAI.prototype.SetFormationController = function(ent)
 		else
 			cmpObstruction.SetControlGroup(ent);
 	}
+
+	const cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
+	if (cmpUnitMotion)
+		cmpUnitMotion.SetMemberOfFormation(ent);
 
 	// If we were removed from a formation, let the FSM switch back to INDIVIDUAL
 	if (ent == INVALID_ENTITY)

binaries/data/mods/public/simulation/components/UnitMotionFlying.js
@@ -296,6 +296,11 @@ UnitMotionFlying.prototype.MoveToTargetRange = function(target, minRange, maxRan
 	return true;
 };
 
+UnitMotionFlying.prototype.SetMemberOfFormation = function()
+{
+	// Ignored.
+};
+
 UnitMotionFlying.prototype.GetWalkSpeed = function()
 {
 	return +this.template.MaxSpeed;

binaries/data/mods/public/simulation/components/tests/test_UnitAI.js
@@ -170,6 +170,7 @@ function TestFormationExiting(mode)
 		"GetWalkSpeed": () => 1,
 		"MoveToFormationOffset": (target, x, z) => {},
 		"MoveToTargetRange": (target, min, max) => true,
+		"SetMemberOfFormation": () => {},
 		"StopMoving": () => {},
 		"SetFacePointAfterMove": () => {},
 		"GetFacePointAfterMove": () => true,
@@ -349,6 +350,7 @@ function TestMoveIntoFormationWhileAttacking()
 			"GetWalkSpeed": () => 1,
 			"MoveToFormationOffset": (target, x, z) => {},
 			"MoveToTargetRange": (target, min, max) => true,
+			"SetMemberOfFormation": () => {},
 			"StopMoving": () => {},
 			"SetFacePointAfterMove": () => {},
 			"GetFacePointAfterMove": () => true,

binaries/data/mods/public/simulation/data/pathfinder.rng
@@ -4,8 +4,21 @@
     <element name="MaxSameTurnMoves">
       <data type="nonNegativeInteger"/>
     </element>
-    <element name="PushingRadius">
-      <data type="decimal"/>
+    <element name="Pushing">
+      <interleave>
+        <element name="Radius">
+          <data type="decimal"/>
+        </element>
+        <element name="StaticExtension">
+          <data type="decimal"/>
+        </element>
+        <element name="MovingExtension">
+          <data type="decimal"/>
+        </element>
+        <element name="MinimalForce">
+          <data type="decimal"/>
+        </element>
+      </interleave>
     </element>
     <element name="PassabilityClasses">
       <oneOrMore>

binaries/data/mods/public/simulation/data/pathfinder.xml
@@ -4,11 +4,30 @@
   <!-- Setting the value to 0 disable this functionality -->
   <MaxSameTurnMoves>20</MaxSameTurnMoves>
 
-  <!-- Multiplier for the distance at which units push each other. -->
-  <!-- Setting the value to 0 disables unit pushing entirely. -->
-  <!-- Note that values above 2-3 are likely to start behaving weirdly -->
-  <!-- (in particular, formations offsets may need adjusting). -->
-  <PushingRadius>1.6</PushingRadius>
+  <Pushing>
+    <!-- Units push each other if they are within 'clearance * radius' meters. -->
+    <!-- Setting the value to 0 disables unit pushing entirely. -->
+    <!-- Note that values above 2-3 are likely to start behaving weirdly. -->
+    <!-- You can also tweaks extensions below. -->
+    <Radius>1.6</Radius>
+
+    <!-- Actual pushing radius for non-moving units is: -->
+    <!-- Clearance * PushingRadius + StaticPushExtension -->
+    <!-- NB: Once idle units start being pushed, they become moving units, -->
+    <!-- so this should be understood as the maximum unit density. -->
+    <StaticExtension>2</StaticExtension>
+
+    <!-- Actual pushing radius for moving units is: -->
+    <!-- Clearance * PushingRadius + MovingPushExtension -->
+    <!-- This is the factor that has the largest influence on actual sparsity. -->
+    <MovingExtension>2.5</MovingExtension>
+
+    <!-- After the combined pushing force of all neighboring units is calculated, -->
+    <!-- if the value is below this number, treat it as effectively zero. -->
+    <!-- This nullifies very small pushes, and makes things look and behave nicer. -->
+    <!-- NB: This impacts the pushing max distance. -->
+    <MinimalForce>0.2</MinimalForce>
+  </Pushing>
 
   <PassabilityClasses>
 

binaries/data/mods/public/simulation/templates/special/formations/testudo.xml
@@ -7,7 +7,7 @@
     <SortingClasses>Hero Champion Elite Advanced Basic</SortingClasses>
     <FormationName>Testudo</FormationName>
     <FormationShape>square</FormationShape>
-    <UnitSeparationWidthMultiplier>0.50</UnitSeparationWidthMultiplier>
+    <UnitSeparationWidthMultiplier>0.5</UnitSeparationWidthMultiplier>
     <UnitSeparationDepthMultiplier>0.4</UnitSeparationDepthMultiplier>
     <SortingOrder>fillFromTheSides</SortingOrder>
     <WidthDepthRatio>0.8</WidthDepthRatio>

source/simulation2/components/CCmpUnitMotionManager.h
@@ -81,9 +81,15 @@ public:
 		bool isMoving = false;
 	};
 
-	// Multiplier for the pushing radius. Pre-multiplied by the circle-square correction factor.
 	// "Template" state, not serialized (cannot be changed mid-game).
+
+	// Multiplier for the pushing radius. Pre-multiplied by the circle-square correction factor.
 	entity_pos_t m_PushingRadius;
+	// Additive modifiers to the pushing radius for moving units and idle units respectively.
+	entity_pos_t m_MovingPushExtension;
+	entity_pos_t m_StaticPushExtension;
+	// Pushing forces below this value are ignored - this prevents units moving forever by very small increments.
+	entity_pos_t m_MinimalPushing;
 
 	// These vectors are reconstructed on deserialization.
 

commit 83703992c9b1db85fb4db05e8b40e2c257123a3e

Fix speed glitch
Introduced in 40cbde1925
MoveToFormationOffset may be called after the formation controller is
reset, leading to issues. It seems best to trust only
SetMemberOfFormation.

This causes the speed glitch experienced by wow & Valihrant.

Differential Revision: https://code.wildfiregames.com/D4128
This was SVN commit r25747.

source/simulation2/components/CCmpUnitMotion.h
@@ -508,7 +508,6 @@ public:
 
 	virtual void MoveToFormationOffset(entity_id_t controller, entity_pos_t x, entity_pos_t z)
 	{
-		SetMemberOfFormation(controller);
 		// Pass the controller to the move request anyways.
 		MoveTo(MoveRequest(controller, CFixedVector2D(x, z)));
 	}