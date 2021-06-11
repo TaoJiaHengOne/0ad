TaoJiaHengOne/0ad

commit 8ee6774d120886636b830b2bff7e20a38263524d

[Gameplay] - Move infantry from fortress to barracks only.
Having some civs able to train their champions at the barracks, others
at (not too expensive) specific structures and some only at the (still
expensive) fortress is neither consistent nor balanced.

Patch by: @Nescio
Differential revision: D3384
Reviewed by: @borg-
Idea approved by: @wraitii
This was SVN commit r24657.

binaries/data/mods/public/maps/random/wild_lake.js
@@ -147,7 +147,7 @@ var mercenaryCampGuards = {
 		{ "Template" : "units/mace/infantry_javelineer_b", "Count" : 4 },
 		{ "Template" : "units/mace/cavalry_spearman_e", "Count" : 3 },
 		{ "Template" : "units/mace/infantry_archer_a", "Count" : 4 },
-		{ "Template" : "units/mace/champion_infantry_a", "Count" : 3 }
+		{ "Template" : "units/mace/champion_infantry_spearman", "Count" : 3 }
 	],
 	"generic/snowy": [
 		{ "Template" : "structures/ptol/mercenary_camp" },

binaries/data/mods/public/maps/random/jebel_barkal.js
@@ -86,7 +86,7 @@ const oPalisadeGate = "structures/palisades_gate";
 const oPalisadeTower = "structures/palisades_tower";
 const oKushCitizenArcher = "units/kush/infantry_archer_b";
 const oKushHealer = "units/kush/support_healer_b";
-const oKushChampionArcher = "units/kush/champion_infantry";
+const oKushChampionArcher = "units/kush/champion_infantry_archer";
 const oKushChampions = [
 	oKushChampionArcher,
 	"units/kush/champion_infantry_amun",

binaries/data/mods/public/maps/scenarios/combat_demo.xml
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f589551de1a8f1b29e3c5b20d863bf8caee682a7c2dc90dea5c0b21523fb4bb8
-size 43590
+oid sha256:3a5dd3359f0f24ee87865bcaafdfbe8a01907797e5be12dd519dd7ff8b681eb9
+size 43640

binaries/data/mods/public/maps/scenarios/death_canyon_invasion_force.xml
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0a3f10cc8e7e34496563add5ae1a2cd33d307ca2a7d154255b40467ce7d5a41
-size 598764
+oid sha256:cef7f27fdf991aa93e62a4c1f79a734d85835112a6894e1d3a41d31f6f2dc936
+size 598964

binaries/data/mods/public/maps/scenarios/laconia.xml
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b69fedf6f429e294ffb6f4bda22298bbd4ac3a8712061f761b316bb10b8756d6
-size 191421
+oid sha256:32feb40bcc3b9c57215d64aab9e684eeba316ee74b0dd812ae25aa9838ed4803
+size 191501

binaries/data/mods/public/maps/scenarios/road_demo.xml
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35e99142242b9f2f24a0fd3a22f657261ffd64de76c5fc253c2fb058cd1b8901
-size 26497
+oid sha256:073f20bd1e7f341c86f4837995f9bf530f2eec6f7ac088c7f71fe1e34d02e5b1
+size 26507

binaries/data/mods/public/maps/scenarios/sandbox_britons.xml
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:044d8414c0fed52bc5ead8f6d98e5714f827834741e84ea09ffa21445e4ecd08
-size 674843
+oid sha256:cc22b54cab67f50fbedbd68618a8f8e5ff0e21f8785f6006adb3e8699a17adc8
+size 674953

binaries/data/mods/public/maps/scenarios/sandbox_carthaginians.xml
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5accf2ca6aeb7f6be15bd4e771a4fcc714e61e0ccdfb63ea5811dd21dbf1cc38
-size 172823
+oid sha256:bb562308c9bfcf3dafedc9531efc7a12e4f49194639cced2e692d957eae51f26
+size 172903

commit ded4859e6ec44cef2872703d8ffc1efec832d733

Fix missing templates on wild_lake.js.
Broken in 8ee6774d12

Reported by: @Nobbi
This was SVN commit r25773.

binaries/data/mods/public/maps/random/wild_lake.js
@@ -154,7 +154,7 @@ var mercenaryCampGuards = {
 		{ "Template": "units/brit/infantry_javelineer_b", "Count": 4 },
 		{ "Template": "units/brit/cavalry_swordsman_e", "Count": 3 },
 		{ "Template": "units/brit/infantry_slinger_a", "Count": 4 },
-		{ "Template": "units/brit/champion_infantry", "Count": 3 }
+		{ "Template": "units/brit/champion_infantry_swordsman", "Count": 3 }
 	],
 	"generic/sahara": [
 		{ "Template": "structures/ptol/mercenary_camp" },
@@ -175,7 +175,7 @@ var mercenaryCampGuards = {
 		{ "Template": "units/iber/infantry_javelineer_b", "Count": 4 },
 		{ "Template": "units/iber/cavalry_spearman_e", "Count": 3 },
 		{ "Template": "units/iber/infantry_slinger_a", "Count": 4 },
-		{ "Template": "units/iber/champion_infantry", "Count": 3 }
+		{ "Template": "units/iber/champion_infantry_swordsman", "Count": 3 }
 	],
 	"generic/savanna": [
 		{ "Template": "structures/merc_camp_egyptian" },
@@ -196,7 +196,7 @@ var mercenaryCampGuards = {
 		{ "Template": "units/gaul/infantry_javelineer_b", "Count": 4 },
 		{ "Template": "units/gaul/cavalry_swordsman_e", "Count": 3 },
 		{ "Template": "units/gaul/infantry_slinger_a", "Count": 4 },
-		{ "Template": "units/gaul/champion_infantry", "Count": 3 }
+		{ "Template": "units/gaul/champion_infantry_swordsman", "Count": 3 }
 	]
 };
 