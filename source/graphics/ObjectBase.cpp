TaoJiaHengOne/0ad

commit 76acc4e146bc192dd9201e208ea61185d9525ee6

Implement quality levels for actors & corresponding setting.
An actor file, as referenced by the VisualActor, can now define
different actors for different "quality level" setting.
In this initial version, the quality is handled directly by the object
manager.

Actor format impact:
- '<qualitylevels>' may be used as the root node, containing actor nodes
as children.
  - such actor nodes can refer to a file, or to an inline actor, or
simply be inlined.
  - such actor nodes may have a 'quality' attribute, specifying the
maximum quality level of this actor. By default, 255 (the maximum) is
implied.
- The actor format remains valid, but 'groups', 'variants', 'material',
'castshadow' and 'float' can be given a [minquality, maxquality[ range
via XML attributes. Outside of this range, the XML node is ignored
(making it possible to define, in a single actor file, several quality
levels).

Quality is a 0-255 value, with:
- Range 0-99 intended for lower level-of-detail actors (billboards,
etc.)
- Range 100-200 the 'normal' range for models. 100 is "low", 150
"medium", and 200 "high".
- Range 201-255 used for higher quality actors that might be used for
e.g. cinematics.
The range is wide to make it easier to add intermediate levels in the
future and it seemed easier given that an integer value of some kind was
required anyways.

Engine impacts:
- A new CActorDef class is introduced, wrapping an art/actors XML file
and its different quality levels. ObjectBase remains the definition of a
given 'actor', now at a given quality level.
- CActorDef imposes a maximal # of quality level for a particular actor
definition (5 currently).
- CUnit is made to refer to an Actor Definition explicitly, not a
particular ObjectBase.
- As a minor optimisation, variation keys are calculated on
pointer-to-sets-of-selections, instead of raw sets-of-selections, as
this reduces copying.
- some refactoring, including better const-correctness and hotloading
support via std::shared_ptr.

source/graphics/ObjectBase.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -22,7 +22,9 @@
 #include "ps/CStr.h"
 #include "ps/CStrIntern.h"
 
+class CActorDef;
 class CModel;
+class CObjectEntry;
 class CObjectManager;
 class CSkeletonAnim;
 class CXeromyces;
@@ -30,12 +32,24 @@ class XMBElement;
 
 #include <boost/random/mersenne_twister.hpp>
 #include <map>
+#include <memory>
 #include <set>
 #include <unordered_set>
 #include <vector>
 
+/**
+ * Maintains the tree of possible objects from a specific actor definition at a given quality level.
+ * An Object Base is made of:
+ *  - a material
+ *  - a few properties (float on water / casts shadow / ...)
+ *  - a number of variant groups.
+ * Any actual object in game will pick a variant from each group (see ObjectEntry).
+ */
 class CObjectBase
 {
+	friend CActorDef;
+
+	// See CopyWithQuality() below.
 	NONCOPYABLE(CObjectBase);
 public:
 	struct Anim
@@ -118,32 +132,109 @@ public:
 		std::multimap<CStr, Samp> samplers;
 	};
 
-	CObjectBase(CObjectManager& objectManager);
+	CObjectBase(CObjectManager& objectManager, CActorDef& actorDef, u8 QualityLevel);
+
+	// Returns a set of selection such that, added to initialSelections, CalculateVariationKey can proceed.
+	std::set<CStr> CalculateRandomRemainingSelections(uint32_t seed, const std::vector<std::set<CStr>>& initialSelections) const;
 
 	// Get the variation key (indices of chosen variants from each group)
-	// based on the selection strings
-	std::vector<u8> CalculateVariationKey(const std::vector<std::set<CStr> >& selections);
+	// based on the selection strings.
+	// Should not have to make a random choice: the selections should be complete.
+	std::vector<u8> CalculateVariationKey(const std::vector<const std::set<CStr>*>& selections) const;
 
 	// Get the final actor data, combining all selected variants
-	const Variation BuildVariation(const std::vector<u8>& variationKey);
-
-	// Get a set of selection strings that are complete enough to specify an
-	// exact variation of the actor, using the initial selections wherever possible
-	// and choosing randomly where a choice is necessary.
-	std::set<CStr> CalculateRandomVariation(uint32_t seed, const std::set<CStr>& initialSelections);
-
-	// Given a prioritized vector of selection string sets that partially specify
-	// a variation, calculates a remaining set of selection strings such that the resulting
-	// set merged with the initial selections fully specifies an exact variation of
-	// the actor. The resulting selections are selected randomly, but only where a choice
-	// is necessary (i.e. where there are multiple variants but the initial selections,
-	// applied in priority order, fail to select one).
-	std::set<CStr> CalculateRandomRemainingSelections(uint32_t seed, const std::vector<std::set<CStr> >& initialSelections);
+	const Variation BuildVariation(const std::vector<u8>& variationKey) const;
 
 	// Get a list of variant groups for this object, plus for all possible
 	// props. Duplicated groups are removed, if several props share the same
 	// variant names.
 	std::vector<std::vector<CStr> > GetVariantGroups() const;
+
+	// Return a string identifying this actor uniquely (includes quality level information);
+	const CStr& GetIdentifier() const;
+
+	/**
+	 * Returns whether this object (including any possible props)
+	 * uses the given file. (This is used for hotloading.)
+	 */
+	bool UsesFile(const VfsPath& pathname) const;
+
+
+	struct {
+		// cast shadows from this object
+		bool m_CastShadows;
+		// float on top of water
+		bool m_FloatOnWater;
+	} m_Properties;
+
+	// the material file
+	VfsPath m_Material;
+
+	// Quality level - part of the data resource path.
+	u8 m_QualityLevel;
+
+private:
+	// Private interface for CActorDef/ObjectEntry
+
+	/**
+	 * Acts as an explicit copy constructor, for a new quality level.
+	 * Note that this does not reload the actor, so this setting will only change props.
+	 */
+	std::unique_ptr<CObjectBase> CopyWithQuality(u8 newQualityLevel) const;
+
+	// A low-quality RNG like rand48 causes visible non-random patterns (particularly
+	// in large grids of the same actor with consecutive seeds, e.g. forests),
+	// so use a better one that appears to avoid those patterns
+	using rng_t = boost::mt19937;
+	std::set<CStr> CalculateRandomRemainingSelections(rng_t& rng, const std::vector<std::set<CStr>>& initialSelections) const;
+
+	/**
+	 * Get all quality levels at which this object changes (includes props).
+	 * Intended to be called by CActorFef.
+	 * @param splits - a sorted vector of unique quality splits.
+	 */
+	void GetQualitySplits(std::vector<u8>& splits) const;
+
+	void Load(const CXeromyces& XeroFile, const XMBElement& base);
+	void LoadVariant(const CXeromyces& XeroFile, const XMBElement& variant, Variant& currentVariant);
+
+private:
+	// Backref to the owning actor.
+	CActorDef& m_ActorDef;
+
+	// Used to identify this actor uniquely in the ObjectManager (and for debug).
+	CStr m_Identifier;
+
+	std::vector< std::vector<Variant> > m_VariantGroups;
+	CObjectManager& m_ObjectManager;
+};
+
+/**
+ * Represents an actor file. Actors can contain various quality levels.
+ * An ActorDef maintains a CObjectBase for each specified quality level, and provides access to it.
+ */
+class CActorDef
+{
+	// Friend these three so they can use GetBase.
+	friend class CObjectManager;
+	friend class CObjectBase;
+	friend class CObjectEntry;
+
+	NONCOPYABLE(CActorDef);
+public:
+
+	CActorDef(CObjectManager& objectManager);
+
+	std::vector<u8> QualityLevels() const;
+
+	VfsPath GetPathname() const { return m_Pathname; }
+
+// Interface accessible from CObjectManager / CObjectBase
+protected:
+	/**
+	 * Return the Object base matching the given quality level.
+	 */
+	const std::shared_ptr<CObjectBase>& GetBase(u8 QualityLevel) const;
 
 	/**
 	 * Initialise this object by loading from the given file.
@@ -158,41 +249,21 @@ public:
 	bool Reload();
 
 	/**
-	 * Returns whether this object (including any possible props)
+	 * Returns whether this actor (including any possible props)
 	 * uses the given file. (This is used for hotloading.)
 	 */
-	bool UsesFile(const VfsPath& pathname);
+	bool UsesFile(const VfsPath& pathname) const;
 
 	// filename that this was loaded from
 	VfsPath m_Pathname;
 
-	// short human-readable name
-	CStrW m_ShortName;
-
-	struct {
-		// cast shadows from this object
-		bool m_CastShadows;
-		// float on top of water
-		bool m_FloatOnWater;
-	} m_Properties;
-
-	// the material file
-	VfsPath m_Material;
-
 private:
-	// A low-quality RNG like rand48 causes visible non-random patterns (particularly
-	// in large grids of the same actor with consecutive seeds, e.g. forests),
-	// so use a better one that appears to avoid those patterns
-	using rng_t = boost::mt19937;
-
-	std::set<CStr> CalculateRandomRemainingSelections(rng_t& rng, const std::vector<std::set<CStr> >& initialSelections);
-
-	std::vector< std::vector<Variant> > m_VariantGroups;
 	CObjectManager& m_ObjectManager;
 
-	std::unordered_set<VfsPath> m_UsedFiles;
+	// std::shared_ptr to avoid issues during hotloading.
+	std::vector<std::shared_ptr<CObjectBase>> m_ObjectBases;
 
-	void LoadVariant(const CXeromyces& XeroFile, const XMBElement& variant, Variant& currentVariant);
+	std::unordered_set<VfsPath> m_UsedFiles;
 };
 
 #endif

source/graphics/Unit.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -26,16 +26,11 @@
 #include "SkeletonAnimDef.h"
 #include "UnitAnimation.h"
 
-CUnit::CUnit(CObjectEntry* object, CObjectManager& objectManager,
-			 const std::set<CStr>& actorSelections, uint32_t seed)
-: m_Object(object), m_Model(object->m_Model->Clone()),
-  m_ID(INVALID_ENTITY), m_ActorSelections(actorSelections),
-  m_ObjectManager(objectManager), m_Seed(seed)
+#include "ps/CLogger.h"
+
+CUnit::CUnit(CObjectManager& objectManager, const CActorDef& actor, uint32_t seed)
+: m_ID(INVALID_ENTITY), m_ObjectManager(objectManager), m_Actor(actor), m_Seed(seed), m_Animation(nullptr)
 {
-	if (m_Model->ToCModel())
-		m_Animation = new CUnitAnimation(m_ID, m_Model->ToCModel(), m_Object);
-	else
-		m_Animation = NULL;
 }
 
 CUnit::~CUnit()
@@ -46,22 +41,19 @@ CUnit::~CUnit()
 
 CUnit* CUnit::Create(const CStrW& actorName, uint32_t seed, const std::set<CStr>& selections, CObjectManager& objectManager)
 {
-	CObjectBase* base = objectManager.FindObjectBase(actorName);
+	CActorDef* actor = objectManager.FindActorDef(actorName);
 
-	if (! base)
-		return NULL;
+	if (!actor)
+		return nullptr;
 
-	std::set<CStr> actorSelections = base->CalculateRandomVariation(seed, selections);
-
-	std::vector<std::set<CStr> > selectionsVec;
-	selectionsVec.push_back(actorSelections);
-
-	CObjectEntry* obj = objectManager.FindObjectVariation(base, selectionsVec);
-
-	if (! obj)
-		return NULL;
-
-	return new CUnit(obj, objectManager, actorSelections, seed);
+	CUnit* unit = new CUnit(objectManager, *actor, seed);
+	unit->SetActorSelections(selections); // Calls ReloadObject().
+	if (!unit->m_Model)
+	{
+		delete unit;
+		return nullptr;
+	}
+	return unit;
 }
 
 void CUnit::UpdateModel(float frameTime)
@@ -107,7 +99,7 @@ void CUnit::ReloadObject()
 	std::set<CStr> entitySelections;
 	for (const std::pair<const CStr, CStr>& selection : m_EntitySelections)
 		entitySelections.insert(selection.second);
-	std::vector<std::set<CStr> > selections;
+	std::vector<std::set<CStr>> selections;
 	selections.push_back(entitySelections);
 	selections.push_back(m_ActorSelections);
 
@@ -116,14 +108,24 @@ void CUnit::ReloadObject()
 	// expects the selectors passed to it to be complete.
 	// see http://trac.wildfiregames.com/ticket/979
 
-	// Use the entity ID as randomization seed (same as when the unit was first created)
-	std::set<CStr> remainingSelections = m_Object->m_Base->CalculateRandomRemainingSelections(m_Seed, selections);
-	if (!remainingSelections.empty())
-		selections.push_back(remainingSelections);
-
 	// If these selections give a different object, change this unit to use it
-	CObjectEntry* newObject = m_ObjectManager.FindObjectVariation(m_Object->m_Base, selections);
-	if (newObject && newObject != m_Object)
+	// Use the entity ID as randomization seed (same as when the unit was first created)
+	CObjectEntry* newObject = m_ObjectManager.FindObjectVariation(&m_Actor, selections, m_Seed);
+	if (!newObject)
+	{
+		LOGERROR("Error loading object variation (actor: %s)", m_Actor.GetPathname().string8());
+		// Don't delete the unit, don't override our current (valid) state.
+		return;
+	}
+
+	if (!m_Object)
+	{
+		m_Object = newObject;
+		m_Model = newObject->m_Model->Clone();
+		if (m_Model->ToCModel())
+			m_Animation = new CUnitAnimation(m_ID, m_Model->ToCModel(), m_Object);
+	}
+	else if (m_Object && newObject != m_Object)
 	{
 		// Clone the new object's base (non-instance) model
 		CModelAbstract* newModel = newObject->m_Model->Clone();

source/graphics/Unit.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -24,6 +24,7 @@
 #include "ps/CStr.h"
 #include "simulation2/system/Entity.h"	// entity_id_t
 
+class CActorDef;
 class CModelAbstract;
 class CObjectEntry;
 class CObjectManager;
@@ -38,8 +39,7 @@ class CUnit
 	NONCOPYABLE(CUnit);
 private:
 	// Private constructor. Needs complete list of selections for the variation.
-	CUnit(CObjectEntry* object, CObjectManager& objectManager,
-		const std::set<CStr>& actorSelections, uint32_t seed);
+	CUnit(CObjectManager& objectManager, const CActorDef& actor, uint32_t seed);
 
 public:
 	// Attempt to create a unit with the given actor, with a set of
@@ -80,12 +80,14 @@ public:
 	void SetActorSelections(const std::set<CStr>& selections);
 
 private:
-	// object from which unit was created; never NULL
-	CObjectEntry* m_Object;
-	// object model representation; never NULL
-	CModelAbstract* m_Model;
+	// Actor for the unit
+	const CActorDef& m_Actor;
+	// object from which unit was created; never NULL once fully created.
+	CObjectEntry* m_Object = nullptr;
+	// object model representation; never NULL once fully created.
+	CModelAbstract* m_Model = nullptr;
 
-	CUnitAnimation* m_Animation;
+	CUnitAnimation* m_Animation = nullptr;
 
 	// unique (per map) ID number for units created in the editor, as a
 	// permanent way of referencing them.

source/simulation2/components/CCmpVisualActor.cpp
@@ -385,13 +385,6 @@ public:
 		return m_Unit->GetModel().GetTransform().GetTranslation();
 	}
 
-	virtual std::wstring GetActorShortName() const
-	{
-		if (!m_Unit)
-			return L"";
-		return m_Unit->GetObject().m_Base->m_ShortName;
-	}
-
 	virtual std::wstring GetProjectileActor() const
 	{
 		if (!m_Unit)
@@ -556,7 +549,7 @@ public:
 		if (!m_Unit)
 			return;
 
-		if (name != m_ActorName)
+		if (!name.empty() && name != m_ActorName)
 			return;
 
 		ReloadActor();

binaries/data/config/default.cfg
@@ -128,6 +128,9 @@ antialiasing = "disabled"
 sharpening = "disabled"
 sharpness = 0.3
 
+; Quality used for actors.
+max_actor_quality=200
+
 ; Quality level of shader effects (set to 10 to display all effects)
 materialmgr.quality = 2.0
 

binaries/data/mods/public/art/actors/actor.rnc
@@ -1,71 +1,0 @@
-namespace a = "http://relaxng.org/ns/compatibility/annotations/1.0"
-##
-# NOTE: To modify this Relax NG grammar, edit the Relax NG Compact (.rnc) file
-#   and use a converter tool like trang to generate the Relax NG XML (.rng) file
-##
-
-element actor {
-    attribute version { xsd:positiveInteger }, (
-    element group {
-        element variant {
-            attribute name { text }? &
-            attribute file { text }? &
-            attribute frequency { xsd:nonNegativeInteger }? &
-            element mesh {
-                text
-            }? &
-            element textures {
-                element texture {
-                    attribute file { text }? &
-                    attribute name { text }
-                }*
-            }? &
-            element decal {
-                attribute width { xsd:float } &  # X
-                attribute depth { xsd:float } &  # Z
-                attribute angle { xsd:float } &
-                attribute offsetx { xsd:float } &
-                attribute offsetz { xsd:float }
-            }? &
-            element particles {
-                attribute file { text }
-            }? &
-            element color { list {
-                xsd:nonNegativeInteger, # R
-                xsd:nonNegativeInteger, # G
-                xsd:nonNegativeInteger # B
-            } }? &
-            element animations {
-                element animation {
-                    attribute name { text } &
-                    attribute id { text }? &
-                    attribute frequency { xsd:nonNegativeInteger }? &
-                    attribute file { text }? &
-                    attribute speed { xsd:nonNegativeInteger } &
-                    attribute event { xsd:decimal { minInclusive = "0" maxInclusive = "1" } }? &
-                    attribute load { xsd:decimal { minInclusive = "0" maxInclusive = "1" } }? &
-                    attribute sound { xsd:decimal { minInclusive = "0" maxInclusive = "1" } }?
-                }*
-            }? &
-            element props {
-                element prop {
-                    (attribute actor { text }? &
-                    attribute attachpoint { text } &
-                    attribute minheight { xsd:float }? &
-                    attribute maxheight { xsd:float }? &
-                    attribute selectable { "true" | "false" }?)
-                }*
-            }?
-        }*
-    }* &
-    element castshadow {    # flag; true if present
-        empty
-    }? &
-    element float {         # flag; true if present
-        empty
-    }? &
-    element material {
-        text
-    }?
-    )
-}

commit 0ba9cbef74419b5d196354a35e85ef0d034e9385

Fix units changing appearance when switching animation.
Bug introduced in 76acc4e146.
The previous CUnit code had logic to select random aesthetic variants
once initially. The new code removed that, as I completely missed its
purpose, assuming that the random selection, being based on a seed,
would pick the same variants every time. This is incorrect because
entity selections can change the RNG calls, thus the variants, and thus
entity appearance can change when the animation changes (typically, a
horse will change color when walking and running).

The solution is to re-introduce the choice of actor selections on CUnit
creation. This makes sure that units don't change their purely-aesthetic
selections when e.g. animations change.

Reported by: Wowgetoffyourcellphone
Tested By: Stan
Differential Revision: https://code.wildfiregames.com/D4205
This was SVN commit r25844.

source/graphics/ObjectBase.cpp
@@ -781,6 +781,14 @@ CActorDef::CActorDef(CObjectManager& objectManager) : m_ObjectManager(objectMana
 {
 }
 
+std::set<CStr> CActorDef::PickSelectionsAtRandom(uint32_t seed) const
+{
+	// Use the selections from the highest quality actor - this lets artists maintain compatibility (or not)
+	// when going to lower quality levels.
+	std::vector<std::set<CStr>> noSelections;
+	return GetBase(255)->CalculateRandomRemainingSelections(seed, noSelections);
+}
+
 std::vector<u8> CActorDef::QualityLevels() const
 {
 	std::vector<u8> splits;

source/graphics/ObjectBase.h
@@ -229,6 +229,11 @@ public:
 
 	VfsPath GetPathname() const { return m_Pathname; }
 
+	/**
+	 * Return a list of selections specifying a particular variant in all groups, based on the seed.
+	 */
+	std::set<CStr> PickSelectionsAtRandom(uint32_t seed) const;
+
 // Interface accessible from CObjectManager / CObjectBase
 protected:
 	/**

source/graphics/Unit.cpp
@@ -31,6 +31,15 @@
 CUnit::CUnit(CObjectManager& objectManager, const CActorDef& actor, uint32_t seed)
 : m_ID(INVALID_ENTITY), m_ObjectManager(objectManager), m_Actor(actor), m_Seed(seed), m_Animation(nullptr)
 {
+	/**
+	 * When entity selections change, we might end up with a different layout in terms of variants/groups,
+	 * which means the random key calculation might end up with different results for the same seed.
+	 * This is bad, as it means entities randomly change appearence when changing e.g. animation.
+	 * To fix this, we'll initially pick a random and complete specification based on our seed,
+	 * and then pass that as the lowest priority selections. Thus, if the actor files are properly specified,
+	 * we can ensure that the entities will look the same no matter what happens.
+	 */
+	SetActorSelections(m_Actor.PickSelectionsAtRandom(m_Seed)); // Calls ReloadObject().
 }
 
 CUnit::~CUnit()
@@ -39,14 +48,13 @@ CUnit::~CUnit()
 	delete m_Model;
 }
 
-CUnit* CUnit::Create(const CStrW& actorName, uint32_t seed, const std::set<CStr>& selections, CObjectManager& objectManager)
+CUnit* CUnit::Create(const CStrW& actorName, uint32_t seed, CObjectManager& objectManager)
 {
 	auto [success, actor] = objectManager.FindActorDef(actorName);
 	
 	UNUSED2(success);
 
 	CUnit* unit = new CUnit(objectManager, actor, seed);
-	unit->SetActorSelections(selections); // Calls ReloadObject().
 	if (!unit->m_Model)
 	{
 		delete unit;

source/graphics/Unit.h
@@ -42,11 +42,10 @@ private:
 	CUnit(CObjectManager& objectManager, const CActorDef& actor, uint32_t seed);
 
 public:
-	// Attempt to create a unit with the given actor, with a set of
-	// suggested selections (with the rest being randomised using the
-	// given random seed).
+	// Attempt to create a unit with the given actor.
+	// If specific selections are wanted, call SetEntitySelection or SetActorSelections after creation.
 	// Returns NULL on failure.
- 	static CUnit* Create(const CStrW& actorName, uint32_t seed, const std::set<CStr>& selections, CObjectManager& objectManager);
+	static CUnit* Create(const CStrW& actorName, uint32_t seed, CObjectManager& objectManager);
 
 	// destructor
 	~CUnit();
@@ -77,6 +76,9 @@ public:
 
 	const std::set<CStr>& GetActorSelections() const { return m_ActorSelections; }
 
+	/**
+	 * Overwrite the seed-selected actor selections. Likely only useful for Atlas or debugging.
+	 */
 	void SetActorSelections(const std::set<CStr>& selections);
 
 private:
@@ -96,9 +98,11 @@ private:
 	// seed used when creating unit
 	uint32_t m_Seed;
 
-	// actor-level selections for this unit
+	// Actor-level selections for this unit. This is normally set at init time,
+	// so that we always re-use the same aesthetic variants.
+	// These have lower priority than entity-level selections.
 	std::set<CStr> m_ActorSelections;
-	// entity-level selections for this unit
+	// Entity-level selections for this unit (used for e.g. animation variants).
 	std::map<CStr, CStr> m_EntitySelections;
 
 	// object manager which looks after this unit's objectentry

source/graphics/UnitManager.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -87,12 +87,12 @@ void CUnitManager::DeleteAll()
 
 ///////////////////////////////////////////////////////////////////////////////
 // CreateUnit: create a new unit and add it to the world
-CUnit* CUnitManager::CreateUnit(const CStrW& actorName, uint32_t seed, const std::set<CStr8>& selections)
+CUnit* CUnitManager::CreateUnit(const CStrW& actorName, uint32_t seed)
 {
 	if (! m_ObjectManager)
 		return NULL;
 
-	CUnit* unit = CUnit::Create(actorName, seed, selections, *m_ObjectManager);
+	CUnit* unit = CUnit::Create(actorName, seed, *m_ObjectManager);
 	if (unit)
 		AddUnit(unit);
 	return unit;

source/graphics/UnitManager.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Wildfire Games.
+/* Copyright (C) 2021 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -50,7 +50,7 @@ public:
 	void DeleteAll();
 
 	// creates a new unit and adds it to the world
-	CUnit* CreateUnit(const CStrW& actorName, uint32_t seed, const std::set<CStr8>& selections);
+	CUnit* CreateUnit(const CStrW& actorName, uint32_t seed);
 
 	// return the units
 	const std::vector<CUnit*>& GetUnits() const { return m_Units; }

source/simulation2/components/CCmpProjectileManager.cpp
@@ -202,8 +202,7 @@ uint32_t CCmpProjectileManager::LaunchProjectile(CFixedVector3D launchPoint, CFi
 
 	projectile.origin = launchPoint;
 
-	std::set<CStr> selections;
-	projectile.unit = GetSimContext().GetUnitManager().CreateUnit(actorName, m_ActorSeed++, selections);
+	projectile.unit = GetSimContext().GetUnitManager().CreateUnit(actorName, m_ActorSeed++);
 	if (!projectile.unit) // The error will have already been logged
 		return currentId;
 
@@ -302,8 +301,7 @@ void CCmpProjectileManager::Interpolate(float frameTime)
 			quat.ToMatrix(transform);
 			transform.Translate(m_Projectiles[i].pos);
 
-			std::set<CStr> selections;
-			CUnit* unit = GetSimContext().GetUnitManager().CreateUnit(m_Projectiles[i].impactActorName, m_ActorSeed++, selections);
+			CUnit* unit = GetSimContext().GetUnitManager().CreateUnit(m_Projectiles[i].impactActorName, m_ActorSeed++);
 			unit->GetModel().SetTransform(transform);
 
 			ProjectileImpactAnimation projectileImpactAnimation;

source/simulation2/components/CCmpVisualActor.cpp
@@ -598,11 +598,10 @@ void CCmpVisualActor::InitModel()
 	if (!GetSimContext().HasUnitManager())
 		return;
 
-	std::set<CStr> selections;
 	std::wstring actorName = m_ActorName;
 	if (actorName.find(L".xml") == std::wstring::npos)
 		actorName += L".xml";
-	m_Unit = GetSimContext().GetUnitManager().CreateUnit(actorName, GetActorSeed(), selections);
+	m_Unit = GetSimContext().GetUnitManager().CreateUnit(actorName, GetActorSeed());
 	if (!m_Unit)
 		return;
 