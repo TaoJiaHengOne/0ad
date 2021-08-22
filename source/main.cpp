TaoJiaHengOne/0ad

commit 6400a4a0c54989481e4f2e9e65f4a4adf4473bb1

Update Available mods when installing them
Differential revision: D4211
Since 498f0d420b available mods where cached and not updated when new
where installed.
Fixing above.

This was SVN commit r25850.

source/main.cpp
@@ -79,6 +79,7 @@ that of Atlas depending on commandline parameters.
 #include "rlinterface/RLInterface.h"
 #include "scriptinterface/ScriptContext.h"
 #include "scriptinterface/ScriptEngine.h"
+#include "scriptinterface/ScriptInterface.h"
 #include "scriptinterface/JSON.h"
 #include "simulation2/Simulation2.h"
 #include "simulation2/system/TurnManager.h"
@@ -665,6 +666,9 @@ static void RunGameOrAtlas(int argc, const char* argv[])
 				installer.Install(modPath, g_ScriptContext, true);
 
 			installedMods = installer.GetInstalledMods();
+
+			ScriptInterface modInterface("Engine", "Mod", g_ScriptContext);
+			g_Mods.UpdateAvailableMods(modInterface);
 		}
 
 		if (isNonVisual)

source/ps/GameSetup/GameSetup.cpp
@@ -866,6 +866,7 @@ bool Init(const CmdLineArgs& args, int flags)
 	if (flags & INIT_MODS)
 	{
 		ScriptInterface modInterface("Engine", "Mod", g_ScriptContext);
+		g_Mods.UpdateAvailableMods(modInterface);
 		std::vector<CStr> mods;
 		if (args.Has("mod"))
 			mods = args.GetMultiple("mod");

source/ps/Mod.cpp
@@ -156,8 +156,6 @@ bool Mod::EnableMods(const ScriptInterface& scriptInterface, const std::vector<C
 	if (counts["mod"] == 0)
 		m_EnabledMods.insert(m_EnabledMods.begin(), "mod");
 
-	UpdateAvailableMods(scriptInterface);
-
 	m_IncompatibleMods = CheckForIncompatibleMods(m_EnabledMods);
 
 	for (const CStr& mod : m_IncompatibleMods)
@@ -233,6 +231,7 @@ void Mod::UpdateAvailableMods(const ScriptInterface& scriptInterface)
 {
 	PROFILE2("UpdateAvailableMods");
 
+	m_AvailableMods.clear();
 	const Paths paths(g_CmdLineArgs);
 
 	// loop over all possible paths

source/ps/Mod.h
@@ -57,6 +57,16 @@ public:
 	const std::vector<ModData>& GetAvailableMods() const;
 
 	/**
+	 * Fetches available mods and stores some metadata about them.
+	 * This may open the zipped mod archives, depending on the situation,
+	 * and/or try to write files to the user mod folder,
+	 * which can be quite slow, so should be run rarely.
+	 * TODO: if this did not need the scriptInterface to parse JSON,
+	 * we could run it in different contexts and possibly cleaner.
+	 */
+	void UpdateAvailableMods(const ScriptInterface& scriptInterface);
+
+	/**
 	 * Enables specified mods (& mods required by the engine).
 	 * @param addPublic - if true, enable the public mod.
 	 * @return whether the mods were enabled successfully. This can fail if e.g. mods are incompatible.
@@ -83,15 +93,6 @@ public:
 	 */
 	static bool AreModsPlayCompatible(const std::vector<const Mod::ModData*>& modsA, const std::vector<const Mod::ModData*>& modsB);
 private:
-	/**
-	 * Fetches available mods and stores some metadata about them.
-	 * This may open the zipped mod archives, depending on the situation,
-	 * and/or try to write files to the user mod folder,
-	 * which can be quite slow, so should be run rarely.
-	 * TODO: if this did not need the scriptInterface to parse JSON,
-	 * we could run it in different contexts and possibly cleaner.
-	 */
-	void UpdateAvailableMods(const ScriptInterface& scriptInterface);
 
 	/**
 	 * Checks a list of @a mods and returns the incompatible mods, if any.

commit 83bb6f3ed5c4605557e2c24d735e3426cc1255e3

Fix downloanded mods not showing in list until restart
since 498f0d420b
While at it, remove not used variable after 6400a4a0c5
also fix non visual replay broken in 6400a4a0c5

Differential revision: D4220
Tested by: @Stan, @Langbart
Fixes: #6288

This was SVN commit r25853.

source/main.cpp
@@ -193,6 +193,8 @@ static InReaction MainInputHandler(const SDL_Event_* ev)
 		else
 		{
 			LOGMESSAGE("Installed mod %s", installer.GetInstalledMods().front());
+			ScriptInterface modInterface("Engine", "Mod", g_ScriptContext);
+			g_Mods.UpdateAvailableMods(modInterface);
 			RestartEngine();
 		}
 		break;

source/ps/GameSetup/GameSetup.cpp
@@ -877,7 +877,7 @@ bool Init(const CmdLineArgs& args, int flags)
 			boost::split(mods, modsStr, boost::algorithm::is_space(), boost::token_compress_on);
 		}
 
-		if (!g_Mods.EnableMods(modInterface, mods, flags & INIT_MODS_PUBLIC))
+		if (!g_Mods.EnableMods(mods, flags & INIT_MODS_PUBLIC))
 		{
 			// In non-visual mode, fail entirely.
 			if (args.Has("autostart-nonvisual"))

source/ps/Mod.cpp
@@ -136,7 +136,7 @@ const std::vector<Mod::ModData>& Mod::GetAvailableMods() const
 	return m_AvailableMods;
 }
 
-bool Mod::EnableMods(const ScriptInterface& scriptInterface, const std::vector<CStr>& mods, const bool addPublic)
+bool Mod::EnableMods(const std::vector<CStr>& mods, const bool addPublic)
 {
 	m_IncompatibleMods.clear();
 	m_EnabledMods.clear();

source/ps/Mod.h
@@ -72,7 +72,7 @@ public:
 	 * @return whether the mods were enabled successfully. This can fail if e.g. mods are incompatible.
 	 * If true, GetEnabledMods() should be non-empty, GetIncompatibleMods() empty. Otherwise, GetIncompatibleMods() is non-empty.
 	 */
-	bool EnableMods(const ScriptInterface& scriptInterface, const std::vector<CStr>& mods, const bool addPublic);
+	bool EnableMods(const std::vector<CStr>& mods, const bool addPublic);
 
 	/**
 	 * Get data for the given mod.

source/ps/ModIo.cpp
@@ -486,6 +486,7 @@ bool ModIo::AdvanceRequest(const ScriptInterface& scriptInterface)
 			Paths paths(g_CmdLineArgs);
 			CModInstaller installer(paths.UserData() / "mods", paths.Cache());
 			installer.Install(m_DownloadFilePath, g_ScriptContext, false);
+			g_Mods.UpdateAvailableMods(scriptInterface);
 		}
 		break;
 	default:

source/ps/Replay.cpp
@@ -233,7 +233,8 @@ void CReplayPlayer::Replay(const bool serializationtest, const int rejointesttur
 					mods.emplace_back(data.m_Pathname);
 
 				// Ignore the return value, we check below.
-				g_Mods.EnableMods(scriptInterface, mods, false);
+				g_Mods.UpdateAvailableMods(scriptInterface);
+				g_Mods.EnableMods(mods, false);
 				CheckReplayMods(replayMods);
 
 				MountMods(Paths(g_CmdLineArgs), g_Mods.GetEnabledMods());

source/ps/scripting/JSInterface_Mod.cpp
@@ -156,9 +156,9 @@ bool AreModsPlayCompatible(const std::vector<Mod::ModData>& a, const std::vector
 	return Mod::AreModsPlayCompatible(modsA, modsB);
 }
 
-bool SetModsAndRestartEngine(const ScriptInterface& scriptInterface, const std::vector<CStr>& mods)
+bool SetModsAndRestartEngine(const std::vector<CStr>& mods)
 {
-	if (!g_Mods.EnableMods(scriptInterface, mods, false))
+	if (!g_Mods.EnableMods(mods, false))
 		return false;
 
 	RestartEngine();