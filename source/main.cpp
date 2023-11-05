TaoJiaHengOne/0ad

commit 968e55704ac3a653901130300165a11cda7a9762

Make the RLInterface local
refs: #4211

Comments By: @vladislavbelov
Differential Revision: https://code.wildfiregames.com/D5103
This was SVN commit r27908.

source/main.cpp
@@ -360,7 +360,7 @@ static void RendererIncrementalLoad()
 	while (more && timer_Time() - startTime < maxTime);
 }
 
-static void Frame()
+static void Frame(RL::Interface* rlInterface)
 {
 	g_Profiler2.RecordFrameStart();
 	PROFILE2("frame");
@@ -425,12 +425,12 @@ static void Frame()
 
 	g_GUI->TickObjects();
 
-	if (g_RLInterface)
-		g_RLInterface->TryApplyMessage();
+	if (rlInterface)
+		rlInterface->TryApplyMessage();
 
 	if (g_Game && g_Game->IsGameStarted() && needUpdate)
 	{
-		if (!g_RLInterface)
+		if (!rlInterface)
 			g_Game->Update(realTimeSinceLastFrame);
 
 		g_Game->GetView()->Update(float(realTimeSinceLastFrame));
@@ -488,16 +488,19 @@ static void MainControllerShutdown()
 	in_reset_handlers();
 }
 
-static void StartRLInterface(CmdLineArgs args)
+static std::optional<RL::Interface> CreateRLInterface(const CmdLineArgs& args)
 {
+	if (!args.Has("rl-interface"))
+		return std::nullopt;
+
 	std::string server_address;
 	CFG_GET_VAL("rlinterface.address", server_address);
 
 	if (!args.Get("rl-interface").empty())
 		server_address = args.Get("rl-interface");
 
-	g_RLInterface = std::make_unique<RL::Interface>(server_address.c_str());
 	debug_printf("RL interface listening on %s\n", server_address.c_str());
+	return std::make_optional<RL::Interface>(server_address.c_str());
 }
 
 // moved into a helper function to ensure args is destroyed before
@@ -526,7 +529,6 @@ static void RunGameOrAtlas(const PS::span<const char* const> argv)
 	const bool isVisualReplay = args.Has("replay-visual");
 	const bool isNonVisualReplay = args.Has("replay");
 	const bool isVisual = !args.Has("autostart-nonvisual");
-	const bool isUsingRLInterface = args.Has("rl-interface");
 
 	const OsPath replayFile(
 		isVisualReplay ? args.Get("replay-visual") :
@@ -676,14 +678,14 @@ static void RunGameOrAtlas(const PS::span<const char* const> argv)
 		else if (!InitNonVisual(args))
 			g_Shutdown = ShutdownType::Quit;
 
-		if (isUsingRLInterface && g_Shutdown == ShutdownType::None)
-			StartRLInterface(args);
+		std::optional<RL::Interface> rlInterface{g_Shutdown == ShutdownType::None ?
+			CreateRLInterface(args) : std::nullopt};
 
 		while (g_Shutdown == ShutdownType::None)
 			if (isVisual)
-				Frame();
-			else if(isUsingRLInterface)
-				g_RLInterface->TryApplyMessage();
+				Frame(rlInterface ? &*rlInterface : nullptr);
+			else if(rlInterface)
+				rlInterface->TryApplyMessage();
 			else
 				NonVisualFrame();
 

source/rlinterface/RLInterface.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Wildfire Games.
+/* Copyright (C) 2023 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -37,9 +37,6 @@
 #include <queue>
 #include <sstream>
 #include <tuple>
-
-// Globally accessible pointer to the RL Interface.
-std::unique_ptr<RL::Interface> g_RLInterface;
 
 namespace RL
 {

source/rlinterface/RLInterface.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Wildfire Games.
+/* Copyright (C) 2023 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -161,7 +161,5 @@ private:
 };
 
 }
-
-extern std::unique_ptr<RL::Interface> g_RLInterface;
 
 #endif // INCLUDED_RLINTERFACE

commit b38aa7d6ddcece262119dac50ee835b8ace343e2

Fix compile error introduced in 968e55704a
Change the ternary to a lambda.
Add braces around the while loop body introduced in 50cbe24cc5.

Refs: 968e55704a, 50cbe24cc5

Accepted By: @vladislavbelov
Differential Revision: https://code.wildfiregames.com/D5176
This was SVN commit r27914.

source/main.cpp
@@ -678,16 +678,24 @@ static void RunGameOrAtlas(const PS::span<const char* const> argv)
 		else if (!InitNonVisual(args))
 			g_Shutdown = ShutdownType::Quit;
 
-		std::optional<RL::Interface> rlInterface{g_Shutdown == ShutdownType::None ?
-			CreateRLInterface(args) : std::nullopt};
+		// MSVC doesn't support copy elision in ternary expressions. So we use a lambda instead.
+		std::optional<RL::Interface> rlInterface{[&]() -> std::optional<RL::Interface>
+			{
+				if (g_Shutdown == ShutdownType::None)
+					return CreateRLInterface(args);
+				else
+					return std::nullopt;
+			}()};
 
 		while (g_Shutdown == ShutdownType::None)
+		{
 			if (isVisual)
 				Frame(rlInterface ? &*rlInterface : nullptr);
 			else if(rlInterface)
 				rlInterface->TryApplyMessage();
 			else
 				NonVisualFrame();
+		}
 
 		// Do not install mods again in case of restart (typically from the mod selector)
 		modsToInstall.clear();