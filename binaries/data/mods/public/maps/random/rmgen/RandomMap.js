TaoJiaHengOne/0ad

commit 480228f9646b9efb8237ea33abcb9669604ba091

Add quadratic scaling function in rmgen library and use it for decorations.
Patch By: FeldeFeld
Reviewed By: Stan, smiley
Differential Revision: D4212
This was SVN commit r25894.

binaries/data/mods/public/maps/random/rmgen/RandomMap.js
@@ -138,6 +138,11 @@ RandomMap.prototype.getSize = function()
 	return this.size;
 };
 
+RandomMap.prototype.getArea = function(size = this.size)
+{
+	return this.isCircularMap ? diskArea(size / 2) : size * size;
+};
+
 /**
  * Returns the center tile coordinates of the map.
  */

binaries/data/mods/public/maps/random/aegean_sea.js
@@ -233,7 +233,7 @@ createDecoration(
 		]
 	],
 	[
-		scaleByMapSize(16, 262),
+		scaleByMapAreaAbsolute(16),
 		scaleByMapSize(40, 360)
 	],
 	avoidClasses(clWater, 2, clForest, 0, clPlayer, 0, clHill, 1));

binaries/data/mods/public/maps/random/african_plains.js
@@ -192,8 +192,8 @@ createDecoration(
 		[new SimpleObject(aRock, 1,2, 0,1)]
 	],
 	[
-		scaleByMapSize(8, 131),
-		scaleByMapSize(8, 131)
+		scaleByMapAreaAbsolute(8),
+		scaleByMapAreaAbsolute(8)
 	],
 	avoidClasses(clWater, 0, clForest, 0, clPlayer, 0, clHill, 0));
 Engine.SetProgress(75);

binaries/data/mods/public/maps/random/alpine_lakes.js
@@ -205,11 +205,11 @@ createDecoration(
 		[new SimpleObject(aBushMedium, 1, 2, 0, 2), new SimpleObject(aBushSmall, 2, 4, 0, 2)]
 	],
 	[
-		scaleByMapSize(16, 262),
-		scaleByMapSize(8, 131),
-		scaleByMapSize(13, 200),
-		scaleByMapSize(13, 200),
-		scaleByMapSize(13, 200)
+		scaleByMapAreaAbsolute(16),
+		scaleByMapAreaAbsolute(8),
+		scaleByMapAreaAbsolute(13),
+		scaleByMapAreaAbsolute(13),
+		scaleByMapAreaAbsolute(13)
 	],
 	avoidClasses(clWater, 0, clForest, 0, clPlayer, 0, clHill, 0));
 

binaries/data/mods/public/maps/random/archipelago.js
@@ -203,11 +203,11 @@ createDecoration(
 		[new SimpleObject(aBushMedium, 1, 2, 0, 2), new SimpleObject(aBushSmall, 2, 4, 0, 2)]
 	],
 	[
-		scaleByMapSize(16, 262),
-		scaleByMapSize(8, 131),
-		planetm * scaleByMapSize(13, 200),
-		planetm * scaleByMapSize(13, 200),
-		planetm * scaleByMapSize(13, 200)
+		scaleByMapAreaAbsolute(16),
+		scaleByMapAreaAbsolute(8),
+		planetm * scaleByMapAreaAbsolute(13),
+		planetm * scaleByMapAreaAbsolute(13),
+		planetm * scaleByMapAreaAbsolute(13)
 	],
 	[avoidClasses(clForest, 0, clPlayer, 0, clHill, 0), stayClasses(clLand, 5)]);
 Engine.SetProgress(70);

binaries/data/mods/public/maps/random/arctic_summer.js
@@ -221,8 +221,8 @@ createDecoration(
 		]
 	],
 	[
-		scaleByMapSize(16, 262),
-		scaleByMapSize(8, 131),
+		scaleByMapAreaAbsolute(16),
+		scaleByMapAreaAbsolute(8),
 	],
 	avoidClasses(
 		clWater, 0,

binaries/data/mods/public/maps/random/atlas_mountains.js
@@ -145,11 +145,11 @@ createDecoration(
 		[new SimpleObject(aBushMedium, 1, 2, 0, 2), new SimpleObject(aBushSmall, 2, 4, 0, 2)]
 	],
 	[
-		scaleByMapSize(16, 262),
-		scaleByMapSize(8, 131),
-		scaleByMapSize(13, 200),
-		scaleByMapSize(13, 200),
-		scaleByMapSize(13, 200)
+		scaleByMapAreaAbsolute(16),
+		scaleByMapAreaAbsolute(8),
+		scaleByMapAreaAbsolute(13),
+		scaleByMapAreaAbsolute(13),
+		scaleByMapAreaAbsolute(13)
 	],
 	avoidClasses(clForest, 0, clPlayer, 0, clHill, 0));
 

binaries/data/mods/public/maps/random/cantabrian_highlands.js
@@ -224,11 +224,11 @@ createDecoration(
 		[new SimpleObject(aBushMedium, 1, 2, 0, 2), new SimpleObject(aBushSmall, 2, 4, 0, 2)]
 	],
 	[
-		scaleByMapSize(16, 262),
-		scaleByMapSize(8, 131),
-		scaleByMapSize(13, 200),
-		scaleByMapSize(13, 200),
-		scaleByMapSize(13, 200)
+		scaleByMapAreaAbsolute(16),
+		scaleByMapAreaAbsolute(8),
+		scaleByMapAreaAbsolute(13),
+		scaleByMapAreaAbsolute(13),
+		scaleByMapAreaAbsolute(13)
 	],
 	avoidClasses(clWater, 0, clForest, 0, clPlayer, 0, clHill, 0));
 Engine.SetProgress(70);

commit e8908dfee85e6265abb8c7178dc0f74d4c1472ff

Fix getArea
The code didn't call `this.isCircularMap` but checks if it's defined.
The function returned the wrong values for square maps.

Broken in 480228f964

Comments by: @Stan
Differential Revision: https://code.wildfiregames.com/D5288
This was SVN commit r28122.

binaries/data/mods/public/maps/random/rmgen/RandomMap.js
@@ -140,7 +140,7 @@ RandomMap.prototype.getSize = function()
 
 RandomMap.prototype.getArea = function(size = this.size)
 {
-	return this.isCircularMap ? diskArea(size / 2) : size * size;
+	return this.isCircularMap() ? diskArea(size / 2) : size * size;
 };
 
 /**