TaoJiaHengOne/0ad

commit 72f0fdb41b9257d3956a86ca601d715b0c9770d3

GameSetup GUI cleanup - Renaming.
- rename 'Controls' class to 'Controllers' to avoid ambiguity and mimic
MVC lingo.
- rename GameRegisterStanza into LobbyGameRegistration, and make it
explicitly a controller.
- rename GameSettingsFile to PersistentMatchSettings & move it to its
own folder (since it could be reused independently of the controllers)
- remove the StartGameController & move `launchGame` to the
GameSettingsController (simplifies the control flow)
- the GUI Object "setupWindow" was actually the GameSettingsPage, this
is corrected.
- the LoadingPage was showing the GameSettingsPage -> in the current
code, it makes more sense to have both handle their own visibility.
- make the NetMessages class independent of other gamesetup logic, so
that it can be reused by other code. Remove the custom XML object.

Differential Revision: https://code.wildfiregames.com/D3719
This was SVN commit r25101.

binaries/data/mods/public/gui/gamesetup/Controllers/LobbyGameRegistration.js
@@ -3,9 +3,9 @@
  * this match is being setup so that others can join.
  * It informs of the lobby of some setting values and the participating clients.
  */
-class GameRegisterStanza
+class LobbyGameRegistrationController
 {
-	constructor(initData, setupWindow, netMessages, mapCache)
+	constructor(initData, setupWindow, netMessages, mapCache, playerAssignmentsController)
 	{
 		this.mapCache = mapCache;
 
@@ -21,6 +21,7 @@ class GameRegisterStanza
 		// Events
 		setupWindow.registerClosePageHandler(this.onClosePage.bind(this));
 		netMessages.registerNetMessageHandler("start", this.onGameStart.bind(this));
+		playerAssignmentsController.registerPlayerAssignmentsChangeHandler(this.sendImmediately.bind(this));
 
 		g_GameSettings.map.watch(() => this.onSettingsChange(), ["map", "type"]);
 		g_GameSettings.mapSize.watch(() => this.onSettingsChange(), ["size"]);
@@ -38,9 +39,6 @@ class GameRegisterStanza
 
 	onGameStart()
 	{
-		if (!g_IsController || !Engine.HasXmppClient())
-			return;
-
 		this.sendImmediately();
 		let clients = this.formatClientsForStanza();
 		Engine.SendChangeStateGame(clients.connectedPlayers, clients.list);
@@ -60,8 +58,9 @@ class GameRegisterStanza
 		if (!g_IsController || !Engine.HasXmppClient())
 			return;
 
+		// Already sending an update - do nothing.
 		if (this.timer !== undefined)
-			clearTimeout(this.timer);
+			return;
 
 		this.timer = setTimeout(this.sendImmediately.bind(this), this.Timeout);
 	}
@@ -88,6 +87,7 @@ class GameRegisterStanza
 			"name": this.serverName,
 			"hostUsername": Engine.LobbyGetNick(),
 			"mapName": g_GameSettings.map.map,
+			// TODO: if the map name was always up-to-date we wouldn't need the mapcache here.
 			"niceMapName": this.mapCache.getTranslatableMapName(g_GameSettings.map.type, g_GameSettings.map.map),
 			"mapSize": g_GameSettings.map.type == "random" ? g_GameSettings.mapSize.size : "Default",
 			"mapType": g_GameSettings.map.type,
@@ -139,4 +139,4 @@ class GameRegisterStanza
 /**
  * Send the current game settings to the lobby bot if the settings didn't change for this number of milliseconds.
  */
-GameRegisterStanza.prototype.Timeout = 2000;
+LobbyGameRegistrationController.prototype.Timeout = 2000;

commit d6bfa7dedc9c5ccde36e4ea7148d9b4716cf9763

Fix deferred sending of game updates via XMPP
72f0fdb41b broke the deferred sending of XMPP stanzas for changes to the
game settings during game setup. This change fixes it and decreases the
interval to send game setting updates from 2000ms to 500ms to have
updates faster visible in the multiplayer lobby.

One reason why this bug might have been gone unnoticed so far is that
the traffic shaping employed by ejabberd results in a similar behavior
when just looking at the multiplayer lobby through Pyrogenesis:
After exhausting a certain amount of traffic, clients are only allowed
to send a defined amount of bytes per second. This results in game
setting changes taking a moment before they show up in the multiplayer
lobby. Contrary to the desired behavior, this however leads to all
updates being received and processed by XpartaMuPP resulting in
unnecessary load on the server. With this fix Pyrogenesis doesn't
trigger the traffic shaping on ejabberd side anymore, as there is now
much less traffic being sent by Pyrogenesis.


Patch by: @Dunedan
Accepted by: @Stan
Fixes: #6740

Differential Revision: https://code.wildfiregames.com/D5217
This was SVN commit r28196.

binaries/data/mods/public/gui/gamesetup/Controllers/LobbyGameRegistration.js
@@ -21,7 +21,7 @@ class LobbyGameRegistrationController
 		// Events
 		setupWindow.registerClosePageHandler(this.onClosePage.bind(this));
 		netMessages.registerNetMessageHandler("start", this.onGameStart.bind(this));
-		playerAssignmentsController.registerPlayerAssignmentsChangeHandler(this.sendImmediately.bind(this));
+		playerAssignmentsController.registerPlayerAssignmentsChangeHandler(this.onSettingsChange.bind(this));
 
 		g_GameSettings.map.watch(() => this.onSettingsChange(), ["map", "type"]);
 		g_GameSettings.mapSize.watch(() => this.onSettingsChange(), ["size"]);
@@ -137,4 +137,4 @@ class LobbyGameRegistrationController
 /**
  * Send the current game settings to the lobby bot if the settings didn't change for this number of milliseconds.
  */
-LobbyGameRegistrationController.prototype.Timeout = 2000;
+LobbyGameRegistrationController.prototype.Timeout = 500;