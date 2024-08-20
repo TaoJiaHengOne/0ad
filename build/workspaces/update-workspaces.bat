17ae3b224d374da4c804951bd958e0617c475994
Itms <itms@wildfiregames.com>
2024-08-20 18:54:00 +0200

    Retrieve Windows binaries for the build, fixes #1814.

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -2,5 +2,5 @@
 rem ** Create Visual Studio Workspaces on Windows **
 
 cd ..\premake
-if not exist ..\workspaces\vs2017\SKIP_PREMAKE_HERE premake5\bin\release\premake5 --outpath="../workspaces/vs2017" --use-shared-glooxwrapper %* vs2017
+if not exist ..\workspaces\vs2017\SKIP_PREMAKE_HERE premake5\bin\release\premake5 --outpath="../workspaces/vs2017" --build-shared-glooxwrapper %* vs2017
 cd ..\workspaces
---
+++ libraries/get-windows-libs.bat
@@ -0,0 +1,24 @@
+rem **Download sources and binaries of libraries**
+
+rem **SVN revision to checkout for source-libs and windows-libs**
+rem **Update this line when you commit an update to source-libs or windows-libs**
+set "svnrev=28083"
+
+if exist source\.svn (
+  cd source && svn cleanup && svn up -r %svnrev% && cd ..
+) else (
+  svn co -r %svnrev% https://svn.wildfiregames.com/public/source-libs/trunk source
+)
+
+if exist win32\.svn (
+  cd win32 && svn cleanup && svn up -r %svnrev% && cd ..
+) else (
+  svn co -r %svnrev% https://svn.wildfiregames.com/public/windows-libs/trunk win32
+)
+
+rem **Copy binaries to binaries/system/**
+
+copy source\fcollada\bin\* ..\binaries\system\
+copy source\nvtt\bin\* ..\binaries\system\
+copy source\spidermonkey\bin\* ..\binaries\system\
+for /d %%l in (win32\*) do (if exist %%l\bin copy /y %%l\bin\* ..\binaries\system\)
