TaoJiaHengOne/0ad

commit 5c1195f66ac3b3d21a4e4413a21077cd277b38c6

New Linux/BSD build flow.
Getting and building libraries is now clearly separated from preparing
workspaces.
Some disk space is automatically reclaimed and cleanup is streamlined.

build/workspaces/update-workspaces.sh
@@ -11,46 +11,21 @@ die()
   exit 1
 }
 
-# Check for whitespace in absolute path; this will cause problems in the
-# SpiderMonkey build (https://bugzilla.mozilla.org/show_bug.cgi?id=459089)
-# and maybe elsewhere, so we just forbid it
-# Use perl as an alternative to readlink -f, which isn't available on BSD or OS X
-SCRIPTPATH=`perl -MCwd -e 'print Cwd::abs_path shift' "$0"`
-case "$SCRIPTPATH" in
-  *\ * )
-    die "Absolute path contains whitespace, which will break the build - move the game to a path without spaces" ;;
-esac
-
-JOBS=${JOBS:="-j2"}
-
-# Some of our makefiles depend on GNU make, so we set some sane defaults if MAKE
-# is not set.
-case "`uname -s`" in
-  "FreeBSD" | "OpenBSD" )
-    MAKE=${MAKE:="gmake"}
-    ;;
-  * )
-    MAKE=${MAKE:="make"}
-    ;;
-esac
+cd "$(dirname $0)"
+# Now in build/workspaces/ (where we assume this script resides)
 
 # Parse command-line options:
-
 premake_args=""
 
 with_system_premake5=false
-without_nvtt=false
-with_system_nvtt=false
-with_system_mozjs=false
 enable_atlas=true
+
+JOBS=${JOBS:="-j2"}
 
 for i in "$@"
 do
   case $i in
     --with-system-premake5 ) with_system_premake5=true ;;
-    --without-nvtt ) without_nvtt=true; premake_args="${premake_args} --without-nvtt" ;;
-    --with-system-nvtt ) with_system_nvtt=true; premake_args="${premake_args} --with-system-nvtt" ;;
-    --with-system-mozjs ) with_system_mozjs=true; premake_args="${premake_args} --with-system-mozjs" ;;
     --enable-atlas ) enable_atlas=true ;;
     --disable-atlas ) enable_atlas=false ;;
     -j* ) JOBS=$i ;;
@@ -75,31 +50,9 @@ if [ "$enable_atlas" = "true" ]; then
   fi
 fi
 
-cd "$(dirname $0)"
-# Now in build/workspaces/ (where we assume this script resides)
-
 if [ "`uname -s`" = "Darwin" ]; then
   # Set minimal SDK version
   export MIN_OSX_VERSION=${MIN_OSX_VERSION:="10.12"}
-fi
-
-# Don't want to build bundled libs on OS X
-# (build-osx-libs.sh is used instead)
-if [ "`uname -s`" != "Darwin" ]; then
-  echo "Updating bundled third-party dependencies..."
-  echo
-
-  # Build/update bundled external libraries
-  (cd ../../libraries/source/fcollada && MAKE=${MAKE} JOBS=${JOBS} ./build.sh) || die "FCollada build failed"
-  echo
-  if [ "$with_system_mozjs" = "false" ]; then
-    (cd ../../libraries/source/spidermonkey && MAKE=${MAKE} JOBS=${JOBS} ./build.sh) || die "SpiderMonkey build failed"
-  fi
-  echo
-  if [ "$with_system_nvtt" = "false" ] && [ "$without_nvtt" = "false" ]; then
-    (cd ../../libraries/source/nvtt && MAKE=${MAKE} JOBS=${JOBS} ./build.sh) || die "NVTT build failed"
-  fi
-  echo
 fi
 
 # Now build Premake or use system's.

build/workspaces/clean-workspaces.sh
@@ -1,83 +1,15 @@
 #!/bin/sh
 
-# Some of our makefiles depend on GNU make, so we set some sane defaults if MAKE
-# is not set.
-case "`uname -s`" in
-  "FreeBSD" | "OpenBSD" )
-    MAKE=${MAKE:="gmake"}
-    ;;
-  * )
-    MAKE=${MAKE:="make"}
-    ;;
-esac
+# This script remains for backwards compatibility, but consists of a single
+# git command.
 
-# Check the preserve-libs CL option
-preserve_libs=false
+cd "$(dirname $0)/../../"
+git clean -fx build source
 
-for i in "$@"; do
-  case "$i" in
-    --preserve-libs ) preserve_libs=true ;;
-  esac
-done
+echo "WARNING: This script has been split with libraries/clean-source-libs.sh"
+echo "If you want to fix a problem with bundled libs, it's the other script"
+echo "you want to invoke."
 
-# (We don't attempt to clean up every last file here - output in
-# binaries/system/ will still be there, etc. This is mostly just
-# to quickly fix problems in the bundled dependencies.)
-
-cd "$(dirname $0)"
-# Now in build/workspaces/ (where we assume this script resides)
-
-if [ "$preserve_libs" != "true" ]; then
-  echo "Cleaning bundled third-party dependencies..."
-
-  (cd ../../libraries/source/fcollada/src && rm -rf ./output)
-  (cd ../../libraries/source/fcollada && rm -f .already-built)
-  (cd ../../libraries/source/nvtt/src && rm -rf ./build)
-  (cd ../../libraries/source/nvtt && rm -f .already-built)
-  (cd ../../libraries/source/spidermonkey && rm -f .already-built)
-  (cd ../../libraries/source/spidermonkey && rm -rf ./lib/*.a && rm -rf ./lib/*.so)
-  (cd ../../libraries/source/spidermonkey && rm -rf ./include-unix-debug)
-  (cd ../../libraries/source/spidermonkey && rm -rf ./include-unix-release)
-  (cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-91.13.1)
-fi
-
-# Still delete the directory of previous SpiderMonkey versions to
-# avoid wasting disk space if people clean workspaces after updating.
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-78.6.0)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-68.12.1)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-62.9.1)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-52.9.1pre1)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-45.0.2)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs-38.0.0)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs31)
-(cd ../../libraries/source/spidermonkey && rm -rf ./mozjs24)
-
-# Delete former premake5 gmake.* directories
-(cd ../premake/premake5/build && rm -rf ./gmake.bsd)
-(cd ../premake/premake5/build && rm -rf ./gmake.macosx)
-(cd ../premake/premake5/build && rm -rf ./gmake.unix)
-
-# Cleanup current premake directories
-(cd ../premake/premake5/build/gmake2.bsd && ${MAKE} clean)
-(cd ../premake/premake5/build/gmake2.macosx && ${MAKE} clean)
-(cd ../premake/premake5/build/gmake2.unix && ${MAKE} clean)
-(cd ../premake/ && rm -f .already-built)
-
-echo "Removing generated stub and test files..."
-
-find ../../source -name "stub_*.cpp" -type f -exec rm {} \;
-find ../../source -name "test_*.cpp" -type f -not -name "test_setup.cpp" -exec rm {} \;
-
-echo "Cleaning build output..."
-
-# Remove workspaces/gcc if present
-rm -rf ./gcc
-# Remove workspaces/codeblocks if present
-rm -rf ./codeblocks
-# Remove workspaces/xcode3 if present
-rm -rf ./xcode3
-# Remove workspaces/xcode4 if present
-rm -rf ./xcode4
-
-echo
-echo "Done. Try running update-workspaces.sh again now."
+echo "WARNING: This script is deprecated and might be removed in a future"
+echo "release. You can run 'git clean -f build source' from the repository"
+echo "root to clean up build workspaces."

libraries/build-source-libs.sh
@@ -1,0 +1,116 @@
+#!/bin/sh
+
+die()
+{
+  echo ERROR: $*
+  exit 1
+}
+
+# SVN revision to checkout for source-libs
+# Update this line when you commit an update to source-libs
+source_svnrev="28083"
+
+if [ "`uname -s`" = "Darwin" ]; then
+ echo 'This script should not be used on macOS: use build-osx-libs.sh instead.'
+ exit 1
+fi
+
+cd "$(dirname $0)"
+# Now in libraries/ (where we assume this script resides)
+
+# Check for whitespace in absolute path; this will cause problems in the
+# SpiderMonkey build and maybe elsewhere, so we just forbid it
+# Use perl as an alternative to readlink -f, which isn't available on BSD
+SCRIPTPATH=`perl -MCwd -e 'print Cwd::abs_path shift' "$0"`
+case "$SCRIPTPATH" in
+  *\ * )
+    die "Absolute path contains whitespace, which will break the build - move the game to a path without spaces" ;;
+esac
+
+
+# Parse command-line options (download options and build options):
+source_libs_dir="source"
+
+without_nvtt=false
+with_system_nvtt=false
+with_system_mozjs=false
+
+JOBS=${JOBS:="-j2"}
+
+for i in "$@"
+do
+  case $i in
+    --source-libs-dir=* ) source_libs_dir=${1#*=} ;;
+    --source-libs-dir ) die "correct syntax is --source-libs-dir=/path/to/dir" ;;
+    --without-nvtt ) without_nvtt=true ;;
+    --with-system-nvtt ) with_system_nvtt=true ;;
+    --with-system-mozjs ) with_system_mozjs=true ;;
+    -j* ) JOBS=$i ;;
+  esac
+done
+
+# Download source libs
+echo "Downloading source libs..."
+echo
+if [ -e ${source_libs_dir}/.svn ]; then
+  (cd ${source_libs_dir} && svn cleanup && svn up -r $source_svnrev)
+else
+  svn co -r $source_svnrev https://svn.wildfiregames.com/public/source-libs/trunk ${source_libs_dir}
+fi
+
+# Build/update bundled external libraries
+echo "Building third-party dependencies..."
+echo
+
+# Some of our makefiles depend on GNU make, so we set some sane defaults if MAKE
+# is not set.
+case "`uname -s`" in
+  "FreeBSD" | "OpenBSD" )
+    MAKE=${MAKE:="gmake"}
+    ;;
+  * )
+    MAKE=${MAKE:="make"}
+    ;;
+esac
+
+(cd ${source_libs_dir}/fcollada && MAKE=${MAKE} JOBS=${JOBS} ./build.sh) || die "FCollada build failed"
+echo
+if [ "$with_system_nvtt" = "false" ] && [ "$without_nvtt" = "false" ]; then
+  (cd ${source_libs_dir}/nvtt && MAKE=${MAKE} JOBS=${JOBS} ./build.sh) || die "NVTT build failed"
+fi
+echo
+if [ "$with_system_mozjs" = "false" ]; then
+  (cd ${source_libs_dir}/spidermonkey && MAKE=${MAKE} JOBS=${JOBS} ./build.sh) || die "SpiderMonkey build failed"
+fi
+echo
+
+echo "Copying built files..."
+# Copy built binaries to binaries/system/
+cp ${source_libs_dir}/fcollada/bin/* ../binaries/system/
+if [ "$with_system_nvtt" = "false" ] && [ "$without_nvtt" = "false" ]; then
+  cp ${source_libs_dir}/nvtt/bin/* ../binaries/system/
+fi
+if [ "$with_system_mozjs" = "false" ]; then
+  cp ${source_libs_dir}/spidermonkey/bin/* ../binaries/system/
+fi
+# If a custom source-libs dir was used, includes and static libs should be copied to libraries/source/
+# and all other bundled content should be copied.
+if [ "$source_libs_dir" != "source" ]; then
+  rsync -avzq \
+    --exclude fcollada \
+    --exclude nvtt \
+    --exclude spidermonkey \
+    ${source_libs_dir}/ source
+
+  mkdir -p source/fcollada
+  cp -r ${source_libs_dir}/fcollada/{include,lib} source/fcollada/
+  if [ "$with_system_nvtt" = "false" ] && [ "$without_nvtt" = "false" ]; then
+    mkdir -p source/nvtt
+    cp -r ${source_libs_dir}/nvtt/{include,lib} source/nvtt/
+  fi
+  if [ "$with_system_mozjs" = "false" ]; then
+    mkdir -p source/spidermonkey
+    cp -r ${source_libs_dir}/spidermonkey/{include-unix-debug,include-unix-release,lib} source/spidermonkey/
+  fi
+fi
+echo "Done."

libraries/clean-source-libs.sh
@@ -1,0 +1,21 @@
+#!/bin/sh
+
+# This short script allows one to reset source libraries to a clean state without
+# having to redownload everything.
+# (We don't attempt to clean up every last file here - output in binaries/system/
+# will still be there, etc. This is mostly just to quickly fix problems in the
+# bundled dependencies.)
+
+cd "$(dirname $0)"
+# Now in libraries/ (where we assume this script resides)
+
+echo "Cleaning bundled third-party dependencies..."
+
+if [ -e source/.svn ]; then
+    (cd source && svn revert -R . && svn st --no-ignore | cut -c 9- | xargs rm -rf)
+else
+    rm -rf source
+fi
+
+echo
+echo "Done. Try running build-source-libs.sh again now."

commit a2e0749c01a729257a8e3400b1adc87fb3672b12

Fix regression introduced in 5c1195f66a

build/workspaces/update-workspaces.sh
@@ -11,6 +11,19 @@ die()
   exit 1
 }
 
+OS=${OS:="$(uname -s)"}
+
+# Some of our makefiles depend on GNU make, so we set some sane defaults if MAKE
+# is not set.
+case "$OS" in
+  "FreeBSD" | "OpenBSD" )
+    MAKE=${MAKE:="gmake"}
+    ;;
+  * )
+    MAKE=${MAKE:="make"}
+    ;;
+esac
+
 cd "$(dirname $0)"
 # Now in build/workspaces/ (where we assume this script resides)
 
@@ -36,7 +49,7 @@ done
 
 if [ "$enable_atlas" = "true" ]; then
   premake_args="${premake_args} --atlas"
-  if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$OS" = "Darwin" ]; then
     # Provide path to wx-config on OS X (as wxwidgets doesn't support pkgconfig)
     export WX_CONFIG="${WX_CONFIG:="$(pwd)/../../libraries/macos/wxwidgets/bin/wx-config"}"
   else
@@ -50,7 +63,7 @@ if [ "$enable_atlas" = "true" ]; then
   fi
 fi
 
-if [ "`uname -s`" = "Darwin" ]; then
+if [ "$OS" = "Darwin" ]; then
   # Set minimal SDK version
   export MIN_OSX_VERSION=${MIN_OSX_VERSION:="10.12"}
 fi
@@ -73,7 +86,7 @@ echo
 export HOSTTYPE="$HOSTTYPE"
 # Now run Premake to create the makefiles
 echo "Premake args: ${premake_args}"
-if [ "`uname -s`" != "Darwin" ]; then
+if [ "$OS" != "Darwin" ]; then
   ${premake_command} --file="premake5.lua" --outpath="../workspaces/gcc/" ${premake_args} gmake || die "Premake failed"
 else
   ${premake_command} --file="premake5.lua" --outpath="../workspaces/gcc/" --macosx-version-min="${MIN_OSX_VERSION}" ${premake_args} gmake || die "Premake failed"