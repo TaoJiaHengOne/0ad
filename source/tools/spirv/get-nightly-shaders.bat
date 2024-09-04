8d0af4b1845b1ecb6abc6df88593d2aecd1cf29b
Itms <itms@wildfiregames.com>
2024-09-04 17:19:49 +0200

    Unify Windows scripts for getting nightly binaries
    The new script merges existing scripts for downloading translations and
    SPIR-V shaders, and also exports game binaries from the latest nightly
    build into the git repo.
    
    This allows contributors, especially artists, to obtain a working build
    of the game without learning how to build the game for Windows, refs #1814.

--- source/tools/spirv/get-nightly-shaders.bat
+++
@@ -1,5 +0,0 @@
-rem **Download SPIR-V shaders from the latest nightly build**
-
-for %%m in (mod public) do (
-  svn export --force https://svn.wildfiregames.com/nightly-build/trunk/binaries/data/mods/%%m/shaders/spirv ..\..\..\binaries\data\mods\%%m\shaders\spirv
-)
