TaoJiaHengOne/0ad

commit 4a049c5f3aab41a0f85d3f8704fcd66c27651d50

Use PEP 8 naming conventions for i18n tools

source/tools/i18n/extractors/extractors.py
@@ -24,7 +24,7 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import codecs
-import json as jsonParser
+import json
 import os
 import re
 import sys
@@ -54,8 +54,8 @@ def pathmatch(mask, path):
 
 
 class Extractor:
-    def __init__(self, directoryPath, filemasks, options):
-        self.directoryPath = directoryPath
+    def __init__(self, directory_path, filemasks, options):
+        self.directoryPath = directory_path
         self.options = options
 
         if isinstance(filemasks, dict):
@@ -73,8 +73,8 @@ class Extractor:
         :rtype:     ``iterator``
         """
         empty_string_pattern = re.compile(r"^\s*$")
-        directoryAbsolutePath = os.path.abspath(self.directoryPath)
-        for root, folders, filenames in os.walk(directoryAbsolutePath):
+        directory_absolute_path = os.path.abspath(self.directoryPath)
+        for root, folders, filenames in os.walk(directory_absolute_path):
             for subdir in folders:
                 if subdir.startswith((".", "_")):
                     folders.remove(subdir)
@@ -90,14 +90,14 @@ class Extractor:
                 else:
                     for filemask in self.includeMasks:
                         if pathmatch(filemask, filename):
-                            filepath = os.path.join(directoryAbsolutePath, filename)
+                            filepath = os.path.join(directory_absolute_path, filename)
                             for (
                                 message,
                                 plural,
                                 context,
                                 position,
                                 comments,
-                            ) in self.extractFromFile(filepath):
+                            ) in self.extract_from_file(filepath):
                                 if empty_string_pattern.match(message):
                                     continue
 
@@ -105,7 +105,7 @@ class Extractor:
                                     filename = "\u2068" + filename + "\u2069"
                                 yield message, plural, context, (filename, position), comments
 
-    def extractFromFile(self, filepath):
+    def extract_from_file(self, filepath):
         """Extract messages from a specific file.
 
         :return:    An iterator over ``(message, plural, context, position, comments)`` tuples.
@@ -113,7 +113,7 @@ class Extractor:
         """
 
 
-class javascript(Extractor):
+class JavascriptExtractor(Extractor):
     """Extract messages from JavaScript source code."""
 
     empty_msgid_warning = (
@@ -121,7 +121,7 @@ class javascript(Extractor):
         "returns the header entry with meta information, not the empty string."
     )
 
-    def extractJavascriptFromFile(self, fileObject):
+    def extract_javascript_from_file(self, file_object):
         from babel.messages.jslexer import tokenize, unquote_string
 
         funcname = message_lineno = None
@@ -134,7 +134,7 @@ class javascript(Extractor):
         comment_tags = self.options.get("commentTags", [])
         keywords = self.options.get("keywords", {}).keys()
 
-        for token in tokenize(fileObject.read(), dotted=False):
+        for token in tokenize(file_object.read(), dotted=False):
             if token.type == "operator" and (
                 token.value == "(" or (call_stack != -1 and (token.value in ("[", "{")))
             ):
@@ -236,9 +236,11 @@ class javascript(Extractor):
 
             last_token = token
 
-    def extractFromFile(self, filepath):
-        with codecs.open(filepath, "r", encoding="utf-8-sig") as fileObject:
-            for lineno, funcname, messages, comments in self.extractJavascriptFromFile(fileObject):
+    def extract_from_file(self, filepath):
+        with codecs.open(filepath, "r", encoding="utf-8-sig") as file_object:
+            for lineno, funcname, messages, comments in self.extract_javascript_from_file(
+                file_object
+            ):
                 spec = self.options.get("keywords", {})[funcname] or (1,) if funcname else (1,)
                 if not isinstance(messages, (list, tuple)):
                     messages = [messages]
@@ -276,7 +278,7 @@ class javascript(Extractor):
                 if not messages[first_msg_index]:
                     # An empty string msgid isn't valid, emit a warning
                     where = "%s:%i" % (
-                        hasattr(fileObject, "name") and fileObject.name or "(unknown)",
+                        hasattr(file_object, "name") and file_object.name or "(unknown)",
                         lineno,
                     )
                     print(self.empty_msgid_warning % where, file=sys.stderr)
@@ -291,54 +293,54 @@ class javascript(Extractor):
                 yield message, plural, context, lineno, comments
 
 
-class cpp(javascript):
+class CppExtractor(JavascriptExtractor):
     """Extract messages from C++ source code."""
 
 
-class txt(Extractor):
+class TXTExtractor(Extractor):
     """Extract messages from plain text files."""
 
-    def extractFromFile(self, filepath):
-        with codecs.open(filepath, "r", encoding="utf-8-sig") as fileObject:
+    def extract_from_file(self, filepath):
+        with codecs.open(filepath, "r", encoding="utf-8-sig") as file_object:
             for lineno, line in enumerate(
-                [line.strip("\n\r") for line in fileObject.readlines()], start=1
+                [line.strip("\n\r") for line in file_object.readlines()], start=1
             ):
                 if line:
                     yield line, None, None, lineno, []
 
 
-class json(Extractor):
+class JsonExtractor(Extractor):
     """Extract messages from JSON files."""
 
-    def __init__(self, directoryPath=None, filemasks=None, options=None):
+    def __init__(self, directory_path=None, filemasks=None, options=None):
         if options is None:
             options = {}
         if filemasks is None:
             filemasks = []
-        super().__init__(directoryPath, filemasks, options)
+        super().__init__(directory_path, filemasks, options)
         self.keywords = self.options.get("keywords", {})
         self.context = self.options.get("context", None)
         self.comments = self.options.get("comments", [])
 
-    def setOptions(self, options):
+    def set_options(self, options):
         self.options = options
         self.keywords = self.options.get("keywords", {})
         self.context = self.options.get("context", None)
         self.comments = self.options.get("comments", [])
 
-    def extractFromFile(self, filepath):
-        with codecs.open(filepath, "r", "utf-8") as fileObject:
-            for message, context in self.extractFromString(fileObject.read()):
+    def extract_from_file(self, filepath):
+        with codecs.open(filepath, "r", "utf-8") as file_object:
+            for message, context in self.extract_from_string(file_object.read()):
                 yield message, None, context, None, self.comments
 
-    def extractFromString(self, string):
-        jsonDocument = jsonParser.loads(string)
-        if isinstance(jsonDocument, list):
-            for message, context in self.parseList(jsonDocument):
+    def extract_from_string(self, string):
+        json_document = json.loads(string)
+        if isinstance(json_document, list):
+            for message, context in self.parse_list(json_document):
                 if message:  # Skip empty strings.
                     yield message, context
-        elif isinstance(jsonDocument, dict):
-            for message, context in self.parseDictionary(jsonDocument):
+        elif isinstance(json_document, dict):
+            for message, context in self.parse_dictionary(json_document):
                 if message:  # Skip empty strings.
                     yield message, context
         else:
@@ -347,22 +349,22 @@ class json(Extractor):
                 "You must extend the JSON extractor to support it."
             )
 
-    def parseList(self, itemsList):
-        for listItem in itemsList:
-            if isinstance(listItem, list):
-                for message, context in self.parseList(listItem):
+    def parse_list(self, items_list):
+        for list_item in items_list:
+            if isinstance(list_item, list):
+                for message, context in self.parse_list(list_item):
                     yield message, context
-            elif isinstance(listItem, dict):
-                for message, context in self.parseDictionary(listItem):
+            elif isinstance(list_item, dict):
+                for message, context in self.parse_dictionary(list_item):
                     yield message, context
 
-    def parseDictionary(self, dictionary):
+    def parse_dictionary(self, dictionary):
         for keyword in dictionary:
             if keyword in self.keywords:
                 if isinstance(dictionary[keyword], str):
-                    yield self.extractString(dictionary[keyword], keyword)
+                    yield self.extract_string(dictionary[keyword], keyword)
                 elif isinstance(dictionary[keyword], list):
-                    for message, context in self.extractList(dictionary[keyword], keyword):
+                    for message, context in self.extract_list(dictionary[keyword], keyword):
                         yield message, context
                 elif isinstance(dictionary[keyword], dict):
                     extract = None
@@ -370,22 +372,22 @@ class json(Extractor):
                         "extractFromInnerKeys" in self.keywords[keyword]
                         and self.keywords[keyword]["extractFromInnerKeys"]
                     ):
-                        for message, context in self.extractDictionaryInnerKeys(
+                        for message, context in self.extract_dictionary_inner_keys(
                             dictionary[keyword], keyword
                         ):
                             yield message, context
                     else:
-                        extract = self.extractDictionary(dictionary[keyword], keyword)
+                        extract = self.extract_dictionary(dictionary[keyword], keyword)
                         if extract:
                             yield extract
             elif isinstance(dictionary[keyword], list):
-                for message, context in self.parseList(dictionary[keyword]):
+                for message, context in self.parse_list(dictionary[keyword]):
                     yield message, context
             elif isinstance(dictionary[keyword], dict):
-                for message, context in self.parseDictionary(dictionary[keyword]):
+                for message, context in self.parse_dictionary(dictionary[keyword]):
                     yield message, context
 
-    def extractString(self, string, keyword):
+    def extract_string(self, string, keyword):
         context = None
         if "tagAsContext" in self.keywords[keyword]:
             context = keyword
@@ -395,16 +397,16 @@ class json(Extractor):
             context = self.context
         return string, context
 
-    def extractList(self, itemsList, keyword):
-        for listItem in itemsList:
-            if isinstance(listItem, str):
-                yield self.extractString(listItem, keyword)
-            elif isinstance(listItem, dict):
-                extract = self.extractDictionary(listItem[keyword], keyword)
+    def extract_list(self, items_list, keyword):
+        for list_item in items_list:
+            if isinstance(list_item, str):
+                yield self.extract_string(list_item, keyword)
+            elif isinstance(list_item, dict):
+                extract = self.extract_dictionary(list_item[keyword], keyword)
                 if extract:
                     yield extract
 
-    def extractDictionary(self, dictionary, keyword):
+    def extract_dictionary(self, dictionary, keyword):
         message = dictionary.get("_string", None)
         if message and isinstance(message, str):
             context = None
@@ -419,45 +421,47 @@ class json(Extractor):
             return message, context
         return None
 
-    def extractDictionaryInnerKeys(self, dictionary, keyword):
-        for innerKeyword in dictionary:
-            if isinstance(dictionary[innerKeyword], str):
-                yield self.extractString(dictionary[innerKeyword], keyword)
-            elif isinstance(dictionary[innerKeyword], list):
-                yield from self.extractList(dictionary[innerKeyword], keyword)
-            elif isinstance(dictionary[innerKeyword], dict):
-                extract = self.extractDictionary(dictionary[innerKeyword], keyword)
+    def extract_dictionary_inner_keys(self, dictionary, keyword):
+        for inner_keyword in dictionary:
+            if isinstance(dictionary[inner_keyword], str):
+                yield self.extract_string(dictionary[inner_keyword], keyword)
+            elif isinstance(dictionary[inner_keyword], list):
+                yield from self.extract_list(dictionary[inner_keyword], keyword)
+            elif isinstance(dictionary[inner_keyword], dict):
+                extract = self.extract_dictionary(dictionary[inner_keyword], keyword)
                 if extract:
                     yield extract
 
 
-class xml(Extractor):
+class XmlExtractor(Extractor):
     """Extract messages from XML files."""
 
-    def __init__(self, directoryPath, filemasks, options):
-        super().__init__(directoryPath, filemasks, options)
+    def __init__(self, directory_path, filemasks, options):
+        super().__init__(directory_path, filemasks, options)
         self.keywords = self.options.get("keywords", {})
         self.jsonExtractor = None
 
-    def getJsonExtractor(self):
+    def get_json_extractor(self):
         if not self.jsonExtractor:
-            self.jsonExtractor = json()
+            self.jsonExtractor = JsonExtractor()
         return self.jsonExtractor
 
-    def extractFromFile(self, filepath):
+    def extract_from_file(self, filepath):
         from lxml import etree
 
-        with codecs.open(filepath, "r", encoding="utf-8-sig") as fileObject:
-            xmlDocument = etree.parse(fileObject)
+        with codecs.open(filepath, "r", encoding="utf-8-sig") as file_object:
+            xml_document = etree.parse(file_object)
             for keyword in self.keywords:
-                for element in xmlDocument.iter(keyword):
+                for element in xml_document.iter(keyword):
                     lineno = element.sourceline
                     if element.text is not None:
                         comments = []
                         if "extractJson" in self.keywords[keyword]:
-                            jsonExtractor = self.getJsonExtractor()
-                            jsonExtractor.setOptions(self.keywords[keyword]["extractJson"])
-                            for message, context in jsonExtractor.extractFromString(element.text):
+                            json_extractor = self.get_json_extractor()
+                            json_extractor.set_options(self.keywords[keyword]["extractJson"])
+                            for message, context in json_extractor.extract_from_string(
+                                element.text
+                            ):
                                 yield message, None, context, lineno, comments
                         else:
                             context = None
@@ -474,12 +478,12 @@ class xml(Extractor):
                                 )  # Remove tabs, line breaks and unecessary spaces.
                                 comments.append(comment)
                             if "splitOnWhitespace" in self.keywords[keyword]:
-                                for splitText in element.text.split():
+                                for split_text in element.text.split():
                                     # split on whitespace is used for token lists, there, a
                                     # leading '-' means the token has to be removed, so it's not
                                     # to be processed here either
-                                    if splitText[0] != "-":
-                                        yield str(splitText), None, context, lineno, comments
+                                    if split_text[0] != "-":
+                                        yield str(split_text), None, context, lineno, comments
                             else:
                                 yield str(element.text), None, context, lineno, comments
 
@@ -500,14 +504,14 @@ class FakeSectionHeader:
             return self.fp.readline()
 
 
-class ini(Extractor):
+class IniExtractor(Extractor):
     """Extract messages from INI files."""
 
-    def __init__(self, directoryPath, filemasks, options):
-        super().__init__(directoryPath, filemasks, options)
+    def __init__(self, directory_path, filemasks, options):
+        super().__init__(directory_path, filemasks, options)
         self.keywords = self.options.get("keywords", [])
 
-    def extractFromFile(self, filepath):
+    def extract_from_file(self, filepath):
         import ConfigParser
 
         config = ConfigParser.RawConfigParser()

build/jenkins/pipelines/nightly-build.Jenkinsfile
@@ -133,16 +133,16 @@ pipeline {
 		stage("Update translations") {
 			steps {
 				ws("workspace/nightly-svn") {
-					bat "cd source\\tools\\i18n && python updateTemplates.py"
+					bat "cd source\\tools\\i18n && python update_templates.py"
 					withCredentials([string(credentialsId: 'TX_TOKEN', variable: 'TX_TOKEN')]) {
-						bat "cd source\\tools\\i18n && python pullTranslations.py"
+						bat "cd source\\tools\\i18n && python pull_translations.py"
 					}
-					bat "cd source\\tools\\i18n && python generateDebugTranslation.py --long"
-					bat "cd source\\tools\\i18n && python cleanTranslationFiles.py"
+					bat "cd source\\tools\\i18n && python generate_debug_translation.py --long"
+					bat "cd source\\tools\\i18n && python clean_translation_files.py"
 					script { if (!params.NEW_REPO) {
-						bat "python source\\tools\\i18n\\checkDiff.py --verbose"
+						bat "python source\\tools\\i18n\\check_diff.py --verbose"
 					}}
-					bat "cd source\\tools\\i18n && python creditTranslators.py"
+					bat "cd source\\tools\\i18n && python credit_translators.py"
 				}
 			}
 		}

source/tools/i18n/check_diff.py
@@ -23,12 +23,12 @@ import os
 import subprocess
 from typing import List
 
-from i18n_helper import projectRootDirectory
+from i18n_helper import PROJECT_ROOT_DIRECTORY
 
 
 def get_diff():
     """Return a diff using svn diff."""
-    os.chdir(projectRootDirectory)
+    os.chdir(PROJECT_ROOT_DIRECTORY)
 
     diff_process = subprocess.run(["svn", "diff", "binaries"], capture_output=True, check=False)
     if diff_process.returncode != 0:

source/tools/i18n/check_translations.py
@@ -21,9 +21,9 @@ import os
 import re
 import sys
 
-from i18n_helper import l10nFolderName, projectRootDirectory
+from i18n_helper import L10N_FOLDER_NAME, PROJECT_ROOT_DIRECTORY
 from i18n_helper.catalog import Catalog
-from i18n_helper.globber import getCatalogs
+from i18n_helper.globber import get_catalogs
 
 
 VERBOSE = 0
@@ -36,76 +36,76 @@ class MessageChecker:
         self.regex = re.compile(regex, re.IGNORECASE)
         self.human_name = human_name
 
-    def check(self, inputFilePath, templateMessage, translatedCatalogs):
+    def check(self, input_file_path, template_message, translated_catalogs):
         patterns = set(
             self.regex.findall(
-                templateMessage.id[0] if templateMessage.pluralizable else templateMessage.id
+                template_message.id[0] if template_message.pluralizable else template_message.id
             )
         )
 
         # As a sanity check, verify that the template message is coherent.
         # Note that these tend to be false positives.
         # TODO: the pssible tags are usually comments, we ought be able to find them.
-        if templateMessage.pluralizable:
-            pluralUrls = set(self.regex.findall(templateMessage.id[1]))
-            if pluralUrls.difference(patterns):
+        if template_message.pluralizable:
+            plural_urls = set(self.regex.findall(template_message.id[1]))
+            if plural_urls.difference(patterns):
                 print(
-                    f"{inputFilePath} - Different {self.human_name} in "
+                    f"{input_file_path} - Different {self.human_name} in "
                     f"singular and plural source strings "
-                    f"for '{templateMessage}' in '{inputFilePath}'"
+                    f"for '{template_message}' in '{input_file_path}'"
                 )
 
-        for translationCatalog in translatedCatalogs:
-            translationMessage = translationCatalog.get(
-                templateMessage.id, templateMessage.context
+        for translation_catalog in translated_catalogs:
+            translation_message = translation_catalog.get(
+                template_message.id, template_message.context
             )
-            if not translationMessage:
+            if not translation_message:
                 continue
 
-            translatedPatterns = set(
+            translated_patterns = set(
                 self.regex.findall(
-                    translationMessage.string[0]
-                    if translationMessage.pluralizable
-                    else translationMessage.string
+                    translation_message.string[0]
+                    if translation_message.pluralizable
+                    else translation_message.string
                 )
             )
-            unknown_patterns = translatedPatterns.difference(patterns)
+            unknown_patterns = translated_patterns.difference(patterns)
             if unknown_patterns:
                 print(
-                    f'{inputFilePath} - {translationCatalog.locale}: '
+                    f'{input_file_path} - {translation_catalog.locale}: '
                     f'Found unknown {self.human_name} '
                     f'{", ".join(["`" + x + "`" for x in unknown_patterns])} '
                     f'in the translation which do not match any of the URLs '
                     f'in the template: {", ".join(["`" + x + "`" for x in patterns])}'
                 )
 
-            if templateMessage.pluralizable and translationMessage.pluralizable:
-                for indx, val in enumerate(translationMessage.string):
+            if template_message.pluralizable and translation_message.pluralizable:
+                for indx, val in enumerate(translation_message.string):
                     if indx == 0:
                         continue
-                    translatedPatternsMulti = set(self.regex.findall(val))
-                    unknown_patterns_multi = translatedPatternsMulti.difference(pluralUrls)
+                    translated_patterns_multi = set(self.regex.findall(val))
+                    unknown_patterns_multi = translated_patterns_multi.difference(plural_urls)
                     if unknown_patterns_multi:
                         print(
-                            f'{inputFilePath} - {translationCatalog.locale}: '
+                            f'{input_file_path} - {translation_catalog.locale}: '
                             f'Found unknown {self.human_name} '
                             f'{", ".join(["`" + x + "`" for x in unknown_patterns_multi])} '
                             f'in the pluralised translation which do not '
                             f'match any of the URLs in the template: '
-                            f'{", ".join(["`" + x + "`" for x in pluralUrls])}'
+                            f'{", ".join(["`" + x + "`" for x in plural_urls])}'
                         )
 
 
-def check_translations(inputFilePath):
+def check_translations(input_file_path):
     if VERBOSE:
-        print(f"Checking {inputFilePath}")
-    templateCatalog = Catalog.readFrom(inputFilePath)
+        print(f"Checking {input_file_path}")
+    template_catalog = Catalog.read_from(input_file_path)
 
     # If language codes were specified on the command line, filter by those.
     filters = sys.argv[1:]
 
     # Load existing translation catalogs.
-    existingTranslationCatalogs = getCatalogs(inputFilePath, filters)
+    existing_translation_catalogs = get_catalogs(input_file_path, filters)
 
     spam = MessageChecker("url", r"https?://(?:[a-z0-9-_$@./&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
     sprintf = MessageChecker("sprintf", r"%\([^)]+\)s")
@@ -115,37 +115,37 @@ def check_translations(inputFilePath):
     # Loop through all messages in the .POT catalog for URLs.
     # For each, check for the corresponding key in the .PO catalogs.
     # If found, check that URLS in the .PO keys are the same as those in the .POT key.
-    for templateMessage in templateCatalog:
-        spam.check(inputFilePath, templateMessage, existingTranslationCatalogs)
-        sprintf.check(inputFilePath, templateMessage, existingTranslationCatalogs)
-        tags.check(inputFilePath, templateMessage, existingTranslationCatalogs)
+    for template_message in template_catalog:
+        spam.check(input_file_path, template_message, existing_translation_catalogs)
+        sprintf.check(input_file_path, template_message, existing_translation_catalogs)
+        tags.check(input_file_path, template_message, existing_translation_catalogs)
 
     if VERBOSE:
-        print(f"Done checking {inputFilePath}")
+        print(f"Done checking {input_file_path}")
 
 
 def main():
     print(
-        "\n\tWARNING: Remember to regenerate the POT files with “updateTemplates.py” "
+        "\n\tWARNING: Remember to regenerate the POT files with “update_templates.py” "
         "before you run this script.\n\tPOT files are not in the repository.\n"
     )
-    foundPots = 0
-    for root, _folders, filenames in os.walk(projectRootDirectory):
+    found_pots = 0
+    for root, _folders, filenames in os.walk(PROJECT_ROOT_DIRECTORY):
         for filename in filenames:
             if (
                 len(filename) > 4
                 and filename[-4:] == ".pot"
-                and os.path.basename(root) == l10nFolderName
+                and os.path.basename(root) == L10N_FOLDER_NAME
             ):
-                foundPots += 1
+                found_pots += 1
                 multiprocessing.Process(
                     target=check_translations, args=(os.path.join(root, filename),)
                 ).start()
-    if foundPots == 0:
+    if found_pots == 0:
         print(
             "This script did not work because no '.pot' files were found. "
-            "Please run 'updateTemplates.py' to generate the '.pot' files, "
-            "and run 'pullTranslations.py' to pull the latest translations from Transifex. "
+            "Please run 'update_templates.py' to generate the '.pot' files, "
+            "and run 'pull_translations.py' to pull the latest translations from Transifex. "
             "Then you can run this script to check for spam in translations."
         )
 

source/tools/i18n/clean_translation_files.py
@@ -33,19 +33,19 @@ import os
 import re
 import sys
 
-from i18n_helper import l10nFolderName, projectRootDirectory, transifexClientFolder
+from i18n_helper import L10N_FOLDER_NAME, PROJECT_ROOT_DIRECTORY, TRANSIFEX_CLIENT_FOLDER
 
 
 def main():
-    translatorMatch = re.compile(r"^(#\s+[^,<]*)\s+<.*>(.*)")
-    lastTranslatorMatch = re.compile(r"^(\"Last-Translator:[^,<]*)\s+<.*>(.*)")
+    translator_match = re.compile(r"^(#\s+[^,<]*)\s+<.*>(.*)")
+    last_translator_match = re.compile(r"^(\"Last-Translator:[^,<]*)\s+<.*>(.*)")
 
-    for root, folders, _ in os.walk(projectRootDirectory):
+    for root, folders, _ in os.walk(PROJECT_ROOT_DIRECTORY):
         for folder in folders:
-            if folder != l10nFolderName:
+            if folder != L10N_FOLDER_NAME:
                 continue
 
-            if not os.path.exists(os.path.join(root, folder, transifexClientFolder)):
+            if not os.path.exists(os.path.join(root, folder, TRANSIFEX_CLIENT_FOLDER)):
                 continue
 
             path = os.path.join(root, folder, "*.po")
@@ -59,16 +59,16 @@ def main():
                     if reached:
                         if line == "# \n":
                             line = ""
-                        m = translatorMatch.match(line)
+                        m = translator_match.match(line)
                         if m:
                             if m.group(1) in usernames:
                                 line = ""
                             else:
                                 line = m.group(1) + m.group(2) + "\n"
                                 usernames.append(m.group(1))
-                        m2 = lastTranslatorMatch.match(line)
+                        m2 = last_translator_match.match(line)
                         if m2:
-                            line = re.sub(lastTranslatorMatch, r"\1\2", line)
+                            line = re.sub(last_translator_match, r"\1\2", line)
                     elif line.strip() == "# Translators:":
                         reached = True
                     sys.stdout.write(line)

commit eb499b4c5c16c9705c3e5bc7d0d4f1c927173572

Fix a regression introduced with 4a049c5f3a

source/tools/i18n/extractors/extractors.py
@@ -297,7 +297,7 @@ class CppExtractor(JavascriptExtractor):
     """Extract messages from C++ source code."""
 
 
-class TXTExtractor(Extractor):
+class TxtExtractor(Extractor):
     """Extract messages from plain text files."""
 
     def extract_from_file(self, filepath):