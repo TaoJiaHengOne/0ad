4f93ddf47e5ccc5303ca839d7602aa3c99785196
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2024-09-18 19:47:56 +0200

    Use upstream tarball for cxxtest
    Update source build.sh to use upstream tarball and drop any custom
    patches at the same time.
    
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- libraries/source/cxxtest-4.4/build.sh
+++ libraries/source/cxxtest-4.4/build.sh
@@ -3,25 +3,29 @@ set -e
 
 cd "$(dirname "$0")"
 
-LIB_VERSION=28209
+PV=4.4
+LIB_VERSION=${PV}+wfg0
 
-if [ -e .already-built ] && [ "$(cat .already-built)" = "${LIB_VERSION}" ]; then
-	echo "cxxtest-4.4 is already up to date."
+if [ -e .already-built ] && [ "$(cat .already-built || true)" = "${LIB_VERSION}" ]; then
+	echo "CxxTest is already up to date."
 	exit
 fi
 
 # fetch
-svn co https://svn.wildfiregames.com/public/source-libs/trunk/cxxtest-4.4@${LIB_VERSION} cxxtest-4.4-svn
+if [ ! -e "cxxtest-${PV}.tar.gz" ]; then
+	curl -fLo "cxxtest-${PV}.tar.gz" \
+		"https://github.com/CxxTest/cxxtest/releases/download/${PV}/cxxtest-${PV}.tar.gz"
+fi
 
 # unpack
-rm -Rf cxxtest-4.4-build
-cp -R cxxtest-4.4-svn cxxtest-4.4-build
+rm -Rf "cxxtest-${PV}"
+tar -xf "cxxtest-${PV}.tar.gz"
 
 # nothing to actually build
 # built as part of building tests
 
 # install
 rm -Rf bin cxxtest python
-cp -R cxxtest-4.4-build/bin cxxtest-4.4-build/cxxtest cxxtest-4.4-build/python .
+cp -R "cxxtest-${PV}/bin" "cxxtest-${PV}/cxxtest" "cxxtest-${PV}/python" .
 
 echo "${LIB_VERSION}" >.already-built
