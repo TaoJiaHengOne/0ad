TaoJiaHengOne/0ad

commit b2cdb1e6b44bdd14206139d9472e583f4cf76af6

Improve YAML loading performance using libyaml
This adds optional support for loading the YAML reflection data using
`libyaml`, which is faster than PyYAML's pure Python implementation.

source/tools/spirv/compile.py
@@ -25,6 +25,7 @@ import hashlib
 import itertools
 import json
 import os
+import re
 import shutil
 import subprocess
 import sys
@@ -36,12 +37,19 @@ from pathlib import Path
 import yaml
 
 
+try:
+    from yaml import CSafeLoader as SafeLoader
+except ImportError:
+    from yaml import SafeLoader
+
 STAGE_EXTENSIONS = {
     "vertex": ".vs",
     "fragment": ".fs",
     "geometry": ".gs",
     "compute": ".cs",
 }
+
+YAML_DIRECTIVE_PATTERN = re.compile(b"^%YAML 1.0\n")
 
 
 def execute(command):
@@ -158,8 +166,12 @@ def compile_and_reflect(input_mod_path, dependencies, stage, path, out_path, def
             "Error: {}\n".format(ret, " ".join(command), input_path, output_path, err)
         )
         raise ValueError(err)
+
     # Reflect the result SPIRV.
-    data = yaml.safe_load(out)
+
+    # libyaml doesn't support files with a YAML 1.0 directive, so
+    # let's strip the directive out before loading the data.
+    data = yaml.load(YAML_DIRECTIVE_PATTERN.sub(b"", out), Loader=SafeLoader)
     module = data["module"]
     interface_variables = []
     if data.get("all_interface_variables"):

commit 20cef3bce0ab65f728bacec2fe7cd93de98f47e7

Fix compiling SPIR-V shaders on Windows
b2cdb1e6b4 broke compiling of SPIR-V shaders on Windows, as the output
of `spirv-reflect` contains CRLF as line separators there. This commit
fixes that.

source/tools/spirv/compile.py
@@ -49,7 +49,7 @@ STAGE_EXTENSIONS = {
     "compute": ".cs",
 }
 
-YAML_DIRECTIVE_PATTERN = re.compile(b"^%YAML 1.0\n")
+YAML_DIRECTIVE_PATTERN = re.compile(b"^%YAML 1.0\r?\n")
 
 
 def execute(command):