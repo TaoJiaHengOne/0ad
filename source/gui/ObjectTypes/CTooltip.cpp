ff28ac36a6285d558242b598a1849acfb95ecdca
trompetin17 <trompetin17evony@gmail.com>
2024-10-27 17:04:32 -0500

    Tooltip set like ghost
    Tooltip shouldnt receive any mouse event direct, there is a edge case
    when you have a big tooltip and the object that need that tooltip is in
    the bottom left, at this moment tooltip is in the same position of the
    mouse, current code was returning the tooltip, and the code hide the
    tooltip, and find again there is another object that needs the tooltip
    and show again. To fix this is just than simple to set a tooltip like
    ghost because shouldnt receive any mouse event.

--- source/gui/ObjectTypes/CTooltip.cpp
+++ source/gui/ObjectTypes/CTooltip.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Wildfire Games.
+/* Copyright (C) 2024 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -46,6 +46,7 @@ CTooltip::CTooltip(CGUI& pGUI)
 	  m_UseObject(this, "use_object"),
 	  m_HideObject(this, "hide_object")
 {
+	m_Ghost.GetMutable() = true;
 	// Set up a blank piece of text, to be replaced with a more
 	// interesting message later
 	AddText();
