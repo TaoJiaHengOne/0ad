ce588689ee5f5923df8d16d27de0c4aac24cf88e
Stan <stan@wildfiregames.com>
2024-11-11 14:25:39 +0100

    Move Windows tool executables to win source libs.

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -1,6 +1,6 @@
 @echo off
 rem ** Create Visual Studio Workspaces on Windows **
 
-cd ..\premake
-if not exist ..\workspaces\vs2017\SKIP_PREMAKE_HERE premake5\bin\release\premake5 --outpath="../workspaces/vs2017" --build-shared-glooxwrapper %* vs2017
+cd ..\bin
+if not exist ..\workspaces\vs2017\SKIP_PREMAKE_HERE premake5.exe --file="../premake/premake5.lua" --outpath="../workspaces/vs2017" --build-shared-glooxwrapper %* vs2017
 cd ..\workspaces
--- libraries/get-windows-libs.bat
+++ libraries/get-windows-libs.bat
@@ -2,10 +2,26 @@ rem **Download sources and binaries of libraries**
 
 rem **SVN revision to checkout for windows-libs**
 rem **Update this line when you commit an update to windows-libs**
-set "svnrev=28209"
+set "svnrev=28234"
 
 svn co https://svn.wildfiregames.com/public/windows-libs/trunk@%svnrev% win32
 
-rem **Copy binaries to binaries/system/**
+rem **Copy dependencies' binaries to binaries/system/**
 
-for /d %%l in (win32\*) do (if exist %%l\bin copy /y %%l\bin\* ..\binaries\system\)
+set DIR_LIST=boost enet fcollada fmt freetype gloox iconv icu libcurl libpng libsodium libxml2 microsoft miniupnpc nvtt openal sdl2 spidermonkey vorbis wxwidgets zlib
+for %%d in (%DIR_LIST%) do (
+    if exist win32\%%d\bin\ (
+        copy /y win32\%%d\bin\* ..\binaries\system\
+    )
+)
+
+rem **Copy build tools to build/bin
+
+mkdir ..\build\bin\ 2>nul
+
+set TOOLCHAIN_DIR_LIST=premake-core cxxtest-4.4
+for %%d in (%TOOLCHAIN_DIR_LIST%) do (
+    if exist win32\%%d\bin\ (
+        copy /y win32\%%d\bin\* ..\build\bin\
+    )
+)
