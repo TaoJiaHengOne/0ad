40d3ea33d89b5546697e5085af1e7db523a59d9e
Lancelot de Ferrière <wraitii@gmail.com>
2024-12-22 09:48:35 +0100

    Fix tutorial autostart

--- binaries/data/mods/public/gui/autostart/autostart.js
+++ binaries/data/mods/public/gui/autostart/autostart.js
@@ -1,4 +1,15 @@
 function init(initData)
 {
-	return new AutoStart(initData);
+	// This doesn't use the autostart/ folder as those are intended for CLI commands and the duplication isn't big enough.
+
+	this.settings = new GameSettings().init();
+
+	this.settings.fromInitAttributes(initData.attribs);
+
+	this.settings.launchGame(initData.playerAssignments, initData.storeReplay);
+	
+	Engine.SwitchGuiPage("page_loading.xml", {
+		"attribs": this.settings.finalizedAttributes,
+		"playerAssignments": initData.playerAssignments
+	});
 }
