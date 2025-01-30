bccd3db231ebf3d163f05c1996e48d2448acdbbe
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2025-01-30 21:15:16 +0100

    Add missing header in KeyName
    Reported-by: @svenstaro
    Ref: #7564
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- source/ps/KeyName.cpp
+++ source/ps/KeyName.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 Wildfire Games.
+/* Copyright (C) 2025 Wildfire Games.
  * This file is part of 0 A.D.
  *
  * 0 A.D. is free software: you can redistribute it and/or modify
@@ -25,6 +25,7 @@
 #include "ps/CStr.h"
 
 #include <algorithm>
+#include <cstring>
 #include <unordered_map>
 #include <vector>
 
