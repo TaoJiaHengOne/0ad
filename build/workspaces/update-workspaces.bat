e2511cfbacfa5f78dcac5ccc4013462107ae2c68
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2025-02-19 21:33:28 +0100

    Propagate error for failed update-workspace.bat
    If running premake fails during update workspace then update workspace
    needs to exit with an error as well so the CI will actually report an
    error.
    
    According to Itms testing with powershell passing the original error is
    unreliable, so just exit with one.
    
    Fixes: #7642
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -3,5 +3,5 @@ rem ** Create Visual Studio Workspaces on Windows **
 
 cd /D "%~dp0"
 cd ..\bin
-if not exist ..\workspaces\vs2017\SKIP_PREMAKE_HERE premake5.exe --file="../premake/premake5.lua" --outpath="../workspaces/vs2017" %* vs2017
+if not exist ..\workspaces\vs2017\SKIP_PREMAKE_HERE premake5.exe --file="../premake/premake5.lua" --outpath="../workspaces/vs2017" %* vs2017 || exit /b 1
 cd ..\workspaces
