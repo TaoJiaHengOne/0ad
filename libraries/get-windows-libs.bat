25ef6107e015bf3cfdf1cc108be6ce556ff2cecf
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2025-02-20 09:05:34 +0100

    Make get-windows-libs fail on error
    Don't try to muddle through, if the files expected to be present can't
    be copied it's a hard failure.
    
    Further abort with error code for failed commands so it can be caught by
    the CI.
    
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- libraries/get-windows-libs.bat
+++ libraries/get-windows-libs.bat
@@ -6,24 +6,23 @@ rem **SVN revision to checkout for windows-libs**
 rem **Update this line when you commit an update to windows-libs**
 set "svnrev=28256"
 
-svn co https://svn.wildfiregames.com/public/windows-libs/trunk@%svnrev% win32
+svn co https://svn.wildfiregames.com/public/windows-libs/trunk@%svnrev% win32 || exit /b 1
 
 rem **Copy dependencies' binaries to binaries/system/**
 
-set DIR_LIST=boost enet fcollada fmt freetype gloox iconv icu libcurl libpng libsodium libxml2 microsoft miniupnpc nvtt openal sdl2 spidermonkey vorbis wxwidgets zlib
+rem static libs: boost fmt
+rem wxwidgets isn't provided and needs to be built manually
+set DIR_LIST=enet fcollada freetype gloox iconv icu libcurl libpng libsodium libxml2 microsoft miniupnpc nvtt openal sdl2 spidermonkey vorbis zlib
 for %%d in (%DIR_LIST%) do (
-    if exist win32\%%d\bin\ (
-        copy /y win32\%%d\bin\* ..\binaries\system\
-    )
+    copy /y win32\%%d\bin\* ..\binaries\system\ || exit /b 1
 )
 
 rem **Copy build tools to build/bin
 
-mkdir ..\build\bin\ 2>nul
+if exist ..\build\bin\ rmdir ..\build\bin\ /s /q || exit /b 1
+mkdir ..\build\bin\ || exit /b 1
 
 set TOOLCHAIN_DIR_LIST=premake-core cxxtest-4.4
 for %%d in (%TOOLCHAIN_DIR_LIST%) do (
-    if exist win32\%%d\bin\ (
-        copy /y win32\%%d\bin\* ..\build\bin\
-    )
+    copy /y win32\%%d\bin\* ..\build\bin\ || exit /b 1
 )
