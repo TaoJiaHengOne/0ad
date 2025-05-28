d37811d42cac603e2a1dc0358139c373ea48076d
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2025-05-28 17:18:23 +0200

    Fix eslint rule 'no-invalid-this'
    Manual fixes needed for:
    eslint --no-config-lookup --rule '"no-invalid-this": 1'
    
    Ref: #7812
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- binaries/data/mods/public/gui/autostart/autostart.js
+++ binaries/data/mods/public/gui/autostart/autostart.js
@@ -2,14 +2,14 @@ function init(initData)
 {
 	// This doesn't use the autostart/ folder as those are intended for CLI commands and the duplication isn't big enough.
 
-	this.settings = new GameSettings().init();
+	const settings = new GameSettings().init();
 
-	this.settings.fromInitAttributes(initData.attribs);
+	settings.fromInitAttributes(initData.attribs);
 
-	this.settings.launchGame(initData.playerAssignments, initData.storeReplay);
+	settings.launchGame(initData.playerAssignments, initData.storeReplay);
 
 	Engine.SwitchGuiPage("page_loading.xml", {
-		"attribs": this.settings.finalizedAttributes,
+		"attribs": settings.finalizedAttributes,
 		"playerAssignments": initData.playerAssignments
 	});
 }
